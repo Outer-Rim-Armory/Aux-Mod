8bf0dbe57390b5e031e1bcc381636154c31977e1
BNA_SweMonkey <maxen316@gmail.com>
2023-03-04 19:16:24 +0100

    Removed 3AS LAATs comments

--- BNA_KC_Vehicles/config.cpp
+++ BNA_KC_Vehicles/config.cpp
@@ -992,8 +992,8 @@ class CfgVehicles
 	// class 3as_LAAT_Mk1Lights; // Open Doors, Lights
 	// class 3as_LAAT_Mk2;       // Closed Doors
 	// class 3as_LAAT_Mk2Lights; // Closed Doors, Lights
-	// class W41_LAAT_MK1;
-	// class W41_LAAT_MK2;
+	class W41_LAAT_MK1;
+	class W41_LAAT_MK2;
 	class 3AS_Republic_Transport_01;
 	class 3as_saber_m1;
 	class 3as_ATTE_base;
@@ -1072,7 +1072,6 @@ class CfgVehicles
 	};
 
 	// 3AS LAATs
-	/*
 	class BNA_KC_laati_mk1_3AS: W41_LAAT_MK1
 	{
 		displayName = "[KC] [3AS] Standard LAAT MK1 Transport";
@@ -1152,7 +1151,7 @@ class CfgVehicles
 		ls_impulsor_soundOn = "BNA_KC_ImpulseOn";
 		ls_impulsor_soundOff = "BNA_KC_ImpulseOff";
 	};
-	*/
+	
 	
 	// LAAT C
 	// Base
