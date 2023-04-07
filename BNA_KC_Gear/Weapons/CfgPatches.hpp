7d192cf134ce9094c414d035f7fda181bdf70042
William Grate <skulldude327@gmail.com>
2023-04-07 01:15:45 -0500

    Burn 'Em Redux Flamethrowers
    Burn 'Em Redux versions of the BT X-42 Flamethrower from 3AS.
    
    Burn 'Em Redux added as requirement.

--- BNA_KC_Gear/Weapons/CfgPatches.hpp
+++ BNA_KC_Gear/Weapons/CfgPatches.hpp
@@ -7,16 +7,23 @@ class CfgPatches
         requiredVersion = 1;
         requiredAddons[] =
         {
-            "3AS_Weapons"
+            "3AS_Weapons",
 				// Westar
                 // DC-15S
                 // DC-15C
+                // BT X-42 Flamer
+            "WBK_PhoenixTreal_FlamethrowerMOD",
+                // Flamethrower System
+            "M2"
+                // Base Flamethrower / Ammo
         };
         units[] = {};
         weapons[] =
         {
             // Custom Weapons
             "BNA_KC_Westar_M5_Base",
+            "BNA_KC_Flamer",
+            "BNA_KC_Flamer_Heavy",
 
             // Pre-Defined Weapons
             "BNA_KC_DC15S_Predef",
