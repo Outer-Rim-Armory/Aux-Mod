0ad03950e6f951602a1b84c1453d079aa1f1a671
William Grate <skulldude327@gmail.com>
2023-05-25 16:22:38 -0500

    Updated helmets to use a macro instead

--- BNA_KC_Gear/Infantry/Helmets_Placeable.hpp
+++ BNA_KC_Gear/Infantry/Helmets_Placeable.hpp
@@ -1,3 +1,18 @@
+// Macro to create the helmet holder classes for a given helmet
+#define HELMET_HOLDER(CLASS_NAME, DISPLAY_NAME, HELM_NAME) \
+class CLASS_NAME: BNA_KC_HelmetHolder_P2_CR \
+{ \
+    displayName = DISPLAY_NAME; \
+    class TransportItems \
+    { \
+        class HELM_NAME \
+        { \
+            name = HELM_NAME; \
+            count = 1; \
+        }; \
+    }; \
+};
+
 class ls_blueforHeadgear_base;
 
 // ┌───────────────────┐
@@ -25,297 +40,46 @@ class BNA_KC_HelmetHolder_P2_CR: ls_blueforHeadgear_base
         {
             name = "BNA_KC_Helmet_Phase2_CR";
             count = 1;
-        }
-    }
-};
-class BNA_KC_HelmetHolder_P2_CR_Legacy: BNA_KC_HelmetHolder_P2_CR
-{
-    displayName = "[KC] INF P2 Helm 00 (Recruit, Legacy)";
-    class TransportItems
-    {
-        class BNA_KC_Helmet_Phase2_CR_Legacy
-        {
-            name = "BNA_KC_Helmet_Phase2_CR_Legacy";
-            count = 1;
-        }
-    }
+        };
+    };
 };
 
-class BNA_KC_HelmetHolder_P2_CT: BNA_KC_HelmetHolder_P2_CR
-{
-    displayName = "[KC] INF P2 Helm 01 (Trooper)";
-    class TransportItems
-    {
-        class BNA_KC_Helmet_Phase2_CT
-        {
-            name = "BNA_KC_Helmet_Phase2_CT";
-            count = 1;
-        }
-    }
-};
-class BNA_KC_HelmetHolder_P2_CT_v2: BNA_KC_HelmetHolder_P2_CR
-{
-    displayName = "[KC] INF P2 Helm 01 (Trooper, Version 2)";
-    class TransportItems
-    {
-        class BNA_KC_Helmet_Phase2_CT_v2
-        {
-            name = "BNA_KC_Helmet_Phase2_CT_v2";
-            count = 1;
-        }
-    }
-};
-class BNA_KC_HelmetHolder_P2_CT_Legacy: BNA_KC_HelmetHolder_P2_CR
-{
-    displayName = "[KC] INF P2 Helm 01 (Trooper, Legacy)";
-    class TransportItems
-    {
-        class BNA_KC_Helmet_Phase2_CT_Legacy
-        {
-            name = "BNA_KC_Helmet_Phase2_CT_Legacy";
-            count = 1;
-        }
-    }
-};
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_CR_Legacy,   [KC] INF P2 Helm 00 (Recruit, Legacy),                BNA_KC_Helmet_Phase2_CR_Legacy)
 
-class BNA_KC_HelmetHolder_P2_SCT: BNA_KC_HelmetHolder_P2_CR
-{
-    displayName = "[KC] INF P2 Helm 02 (Senior Trooper)";
-    class TransportItems
-    {
-        class BNA_KC_Helmet_Phase2_SCT
-        {
-            name = "BNA_KC_Helmet_Phase2_SCT";
-            count = 1;
-        }
-    }
-};
-class BNA_KC_HelmetHolder_P2_SCT_Legacy: BNA_KC_HelmetHolder_P2_CR
-{
-    displayName = "[KC] INF P2 Helm 02 (Senior Trooper, Legacy)";
-    class TransportItems
-    {
-        class BNA_KC_Helmet_Phase2_SCT_Legacy
-        {
-            name = "BNA_KC_Helmet_Phase2_SCT_Legacy";
-            count = 1;
-        }
-    }
-};
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_CT,          [KC] INF P2 Helm 01 (Trooper),                        BNA_KC_Helmet_Phase2_CT)
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_CT_v2,       [KC] INF P2 Helm 01 (Trooper, Version 2),             BNA_KC_Helmet_Phase2_CT_v2)
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_CT_Legacy,   [KC] INF P2 Helm 01 (Trooper, Legacy),                BNA_KC_Helmet_Phase2_CT_Legacy)
 
-class BNA_KC_HelmetHolder_P2_VCT: BNA_KC_HelmetHolder_P2_CR
-{
-    displayName = "[KC] INF P2 Helm 03 (Veteran Trooper)";
-    class TransportItems
-    {
-        class BNA_KC_Helmet_Phase2_VCT
-        {
-            name = "BNA_KC_Helmet_Phase2_VCT";
-            count = 1;
-        }
-    }
-};
-class BNA_KC_HelmetHolder_P2_VCT_Legacy: BNA_KC_HelmetHolder_P2_CR
-{
-    displayName = "[KC] INF P2 Helm 03 (Veteran Trooper, Legacy)";
-    class TransportItems
-    {
-        class BNA_KC_Helmet_Phase2_VCT_Legacy
-        {
-            name = "BNA_KC_Helmet_Phase2_VCT_Legacy";
-            count = 1;
-        }
-    }
-};
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_SCT,         [KC] INF P2 Helm 02 (Senior Trooper),                 BNA_KC_Helmet_Phase2_SCT)
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_SCT_Legacy,  [KC] INF P2 Helm 02 (Senior Trooper, Legacy),         BNA_KC_Helmet_Phase2_SCT_Legacy)
 
-class BNA_KC_HelmetHolder_P2_LCPL: BNA_KC_HelmetHolder_P2_CR
-{
-    displayName = "[KC] INF P2 Helm 04 (Lance Corporal)";
-    class TransportItems
-    {
-        class BNA_KC_Helmet_Phase2_LCPL
-        {
-            name = "BNA_KC_Helmet_Phase2_LCPL";
-            count = 1;
-        }
-    }
-};
-class BNA_KC_HelmetHolder_P2_LCPL_Legacy: BNA_KC_HelmetHolder_P2_CR
-{
-    displayName = "[KC] INF P2 Helm 04 (Lance Corporal, Legacy)";
-    class TransportItems
-    {
-        class BNA_KC_Helmet_Phase2_LCPL_Legacy
-        {
-            name = "BNA_KC_Helmet_Phase2_LCPL_Legacy";
-            count = 1;
-        }
-    }
-};
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_VCT,         [KC] INF P2 Helm 03 (Veteran Trooper),                BNA_KC_Helmet_Phase2_VCT)
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_VCT_Legacy,  [KC] INF P2 Helm 03 (Veteran Trooper, Legacy),        BNA_KC_Helmet_Phase2_VCT_Legacy)
 
-class BNA_KC_HelmetHolder_P2_CPL: BNA_KC_HelmetHolder_P2_CR
-{
-    displayName = "[KC] INF P2 Helm 05 (Corporal)";
-    class TransportItems
-    {
-        class BNA_KC_Helmet_Phase2_CPL
-        {
-            name = "BNA_KC_Helmet_Phase2_CPL";
-            count = 1;
-        }
-    }
-};
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_LCPL,        [KC] INF P2 Helm 04 (Lance Corporal Trooper),         BNA_KC_Helmet_Phase2_LCPL)
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_LCPL_Legacy, [KC] INF P2 Helm 04 (Lance Corporal Trooper, Legacy), BNA_KC_Helmet_Phase2_LCPL_Legacy)
 
-class BNA_KC_HelmetHolder_P2_CPL: BNA_KC_HelmetHolder_P2_CR
-{
-    displayName = "[KC] INF P2 Helm 05 (Corporal)";
-    class TransportItems
-    {
-        class BNA_KC_Helmet_Phase2_CPL
-        {
-            name = "BNA_KC_Helmet_Phase2_CPL";
-            count = 1;
-        }
-    }
-};
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_CPL,         [KC] INF P2 Helm 05 (Corporal Trooper),               BNA_KC_Helmet_Phase2_CPL)
 
-class BNA_KC_HelmetHolder_P2_CS: BNA_KC_HelmetHolder_P2_CR
-{
-    displayName = "[KC] INF P2 Helm 06 (Sergeant)";
-    class TransportItems
-    {
-        class BNA_KC_Helmet_Phase2_CS
-        {
-            name = "BNA_KC_Helmet_Phase2_CS";
-            count = 1;
-        }
-    }
-};
-class BNA_KC_HelmetHolder_P2_CS_Legacy: BNA_KC_HelmetHolder_P2_CR
-{
-    displayName = "[KC] INF P2 Helm 06 (Sergeant, Legacy)";
-    class TransportItems
-    {
-        class BNA_KC_Helmet_Phase2_CS_Legacy
-        {
-            name = "BNA_KC_Helmet_Phase2_CS_Legacy";
-            count = 1;
-        }
-    }
-};
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_CS,          [KC] INF P2 Helm 06 (Sergeant),                       BNA_KC_Helmet_Phase2_CS)
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_CS_Legacy,   [KC] INF P2 Helm 06 (Sergeant, Legacy),               BNA_KC_Helmet_Phase2_CS_Legacy)
 
-class BNA_KC_HelmetHolder_P2_CSS: BNA_KC_HelmetHolder_P2_CR
-{
-    displayName = "[KC] INF P2 Helm 07 (Staff Sergeant)";
-    class TransportItems
-    {
-        class BNA_KC_Helmet_Phase2_CSS
-        {
-            name = "BNA_KC_Helmet_Phase2_CSS";
-            count = 1;
-        }
-    }
-};
-class BNA_KC_HelmetHolder_P2_CSS_Legacy: BNA_KC_HelmetHolder_P2_CR
-{
-    displayName = "[KC] INF P2 Helm 07 (Staff Sergeant, Legacy)";
-    class TransportItems
-    {
-        class BNA_KC_Helmet_Phase2_CSS_Legacy
-        {
-            name = "BNA_KC_Helmet_Phase2_CSS_Legacy";
-            count = 1;
-        }
-    }
-};
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_CSS,         [KC] INF P2 Helm 07 (Staff Sergeant),                 BNA_KC_Helmet_Phase2_CSS)
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_CSS_Legacy,  [KC] INF P2 Helm 07 (Staff Sergeant, Legacy),         BNA_KC_Helmet_Phase2_CSS_Legacy)
 
-class BNA_KC_HelmetHolder_P2_CSFC: BNA_KC_HelmetHolder_P2_CR
-{
-    displayName = "[KC] INF P2 Helm 08 (Sergeant First Class)";
-    class TransportItems
-    {
-        class BNA_KC_Helmet_Phase2_CSFC
-        {
-            name = "BNA_KC_Helmet_Phase2_CSFC";
-            count = 1;
-        }
-    }
-};
-class BNA_KC_HelmetHolder_P2_CSFC_Legacy: BNA_KC_HelmetHolder_P2_CR
-{
-    displayName = "[KC] INF P2 Helm 08 (Sergeant First Class, Legacy)";
-    class TransportItems
-    {
-        class BNA_KC_Helmet_Phase2_CSFC_Legacy
-        {
-            name = "BNA_KC_Helmet_Phase2_CSFC_Legacy";
-            count = 1;
-        }
-    }
-};
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_CSFC,        [KC] INF P2 Helm 08 (Sergeant First Class),           BNA_KC_Helmet_Phase2_CSFC)
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_CSFC_Legacy, [KC] INF P2 Helm 08 (Sergeant First Class, Legacy),   BNA_KC_Helmet_Phase2_CSFC_Legacy)
 
-class BNA_KC_HelmetHolder_P2_CMS: BNA_KC_HelmetHolder_P2_CR
-{
-    displayName = "[KC] INF P2 Helm 09 (Master Sergeant)";
-    class TransportItems
-    {
-        class BNA_KC_Helmet_Phase2_CMS
-        {
-            name = "BNA_KC_Helmet_Phase2_CMS";
-            count = 1;
-        }
-    }
-};
-class BNA_KC_HelmetHolder_P2_CMS_Legacy: BNA_KC_HelmetHolder_P2_CR
-{
-    displayName = "[KC] INF P2 Helm 09 (Master Sergeant, Legacy)";
-    class TransportItems
-    {
-        class BNA_KC_Helmet_Phase2_CMS_Legacy
-        {
-            name = "BNA_KC_Helmet_Phase2_CMS_Legacy";
-            count = 1;
-        }
-    }
-};
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_CSFC,        [KC] INF P2 Helm 08 (Sergeant First Class),           BNA_KC_Helmet_Phase2_CSFC)
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_CSFC_Legacy, [KC] INF P2 Helm 08 (Sergeant First Class, Legacy),   BNA_KC_Helmet_Phase2_CSFC_Legacy)
 
-class BNA_KC_HelmetHolder_P2_CSM: BNA_KC_HelmetHolder_P2_CR
-{
-    displayName = "[KC] INF P2 Helm 10 (Sergeant Major)";
-    class TransportItems
-    {
-        class BNA_KC_Helmet_Phase2_CSM
-        {
-            name = "BNA_KC_Helmet_Phase2_CSM";
-            count = 1;
-        }
-    }
-};
-class BNA_KC_HelmetHolder_P2_CSM_Legacy: BNA_KC_HelmetHolder_P2_CR
-{
-    displayName = "[KC] INF P2 Helm 10 (Sergeant Major, Legacy)";
-    class TransportItems
-    {
-        class BNA_KC_Helmet_Phase2_CSM_Legacy
-        {
-            name = "BNA_KC_Helmet_Phase2_CSM_Legacy";
-            count = 1;
-        }
-    }
-};
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_CMS,         [KC] INF P2 Helm 09 (Master Sergeant),                BNA_KC_Helmet_Phase2_CMS)
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_CMS_Legacy,  [KC] INF P2 Helm 09 (Master Sergeant, Legacy),        BNA_KC_Helmet_Phase2_CMS_Legacy)
 
-class BNA_KC_HelmetHolder_P2_Officer: BNA_KC_HelmetHolder_P2_CR
-{
-    displayName = "[KC] INF P2 Helm 12+ (Officer)";
-    class TransportItems
-    {
-        class BNA_KC_Helmet_Phase2_Officer
-        {
-            name = "BNA_KC_Helmet_Phase2_Officer";
-            count = 1;
-        }
-    }
-};
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_CSM,         [KC] INF P2 Helm 10 (Sergeant Major),                 BNA_KC_Helmet_Phase2_CSM)
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_CSM_Legacy,  [KC] INF P2 Helm 10 (Sergeant Major, Legacy),         BNA_KC_Helmet_Phase2_CSM_Legacy)
+
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_Officer,     [KC] INF P2 Helm 12+ (Officer),                       BNA_KC_Helmet_Phase2_Officer)
 
 
 // ┌───────────────────┐
