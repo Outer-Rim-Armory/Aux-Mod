53ca195515d6e8d1c8db7462583c09c36caae048
William Grate <skulldude327@gmail.com>
2023-06-16 15:49:45 -0500

    Moved Flags to Subaddon

---
+++ BNA_KC_Props/Flags/CfgPatches.hpp
@@ -0,0 +1,21 @@
+class CfgPatches
+{
+    class BNA_KC_Props_Flags
+    {
+        addonRootClass = "BNA_KC_Props";
+        author = "SweMonkey and DartRuffian";
+        requiredVersion = 0.1;
+        requiredAddons[] =
+        {
+			"ls_flags"
+                // Base Flag Objects
+        };
+        units[] =
+		{
+			"BNA_KC_Flag_KeeliCompany_Pole",
+            "BNA_KC_Flag_KeeliCompany_Horizontal",
+            "BNA_KC_Flag_KeeliCompany_Vertical"
+		};
+        weapons[] = {};
+    };
+};
