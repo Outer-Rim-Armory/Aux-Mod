630152df8ac895ac4b1b5a0568b615f9fff799ec
William Grate <skulldude327@gmail.com>
2023-06-30 17:35:04 -0500

    Tweaked Impulse/Repulse sound effects

--- BNA_KC_Vehicles/Aviation/Config.cpp
+++ BNA_KC_Vehicles/Aviation/Config.cpp
@@ -269,13 +269,13 @@ class CfgSounds
     class BNA_KC_ImpulseOn
     {
         name = "LAAT Impulse On";
-        sound[] = {"BNA_KC_Vehicles\Aviation\Data\Audio\ImpulseOn.wav", db+160, 1};
+        sound[] = {"BNA_KC_Vehicles\Aviation\Data\Audio\ImpulseOn.wav", 800, 1, 800};
         titles[] = {1, "Impulse Turns On"};
     };
     class BNA_KC_ImpulseOff
     {
         name = "LAAT Impulse Off";
-        sound[] = {"BNA_KC_Vehicles\Aviation\Data\Audio\ImpulseOff.wav", db+160, 1};
+        sound[] = {"BNA_KC_Vehicles\Aviation\Data\Audio\ImpulseOff.wav", 500, 1, 500};
         titles[] = {1, "Impulse Turns Off"};
     };
 };
