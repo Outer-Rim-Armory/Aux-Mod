5dacc54b5f2974055769f63c73ef606ff8c9339b
William Grate <skulldude327@gmail.com>
2023-08-01 19:50:39 -0500

    Smoke particles ignore wind

--- BNA_KC_Gear/Weapons/Config.cpp
+++ BNA_KC_Gear/Weapons/Config.cpp
@@ -876,12 +876,54 @@ class CfgAmmo
         */
     };
 
-    class SmokeShellBlue;
-    class BNA_KC_Grenade_BactaDispenser_Ammo: SmokeShellBlue
+    class SmokeShell;
+    class BNA_KC_Grenade_BactaDispenser_Ammo: SmokeShell
     {
         model = "\3AS\3AS_Equipment\model\3AS_smokegrenade.p3d";
         smokeColor[] = {0.38, 0.54, 0.91, 0.35};
+        effectsSmoke = "BNA_KC_EffectsGroup_BactaSmoke";
         
         aiAmmoUsageFlags = 0;
+    };
+};
+
+
+// Particle Effects Group, used for smoke grenades
+class SmokeShellWhiteEffect;
+class BNA_KC_EffectsGroup_BactaSmoke: SmokeShellWhiteEffect
+{
+    // Set of particle effects, multiple effects for variance
+    class Smoke
+    {
+        type = "BNA_KC_Effects_BactaSmoke"; // CfgCloudlets class
+        simulation = "particles";
+
+        intensity = 1;
+        interval = 1;
+        position[] = {0, 0, 0};
+    };
+    class SmokeUW: Smoke
+    {
+        type = "BNA_KC_Effects_BactaSmokeUW";
+    };
+};
+
+
+class CfgCloudlets
+{
+    class SmokeShellWhite;
+    class BNA_KC_Effects_BactaSmoke: SmokeShellWhite
+    {
+        interval = 0.01;
+        ignoreWind = true;
+    };
+
+    // Underwater effects, easier to just inherit from the base underwater
+    // particles than to change the same properties they do
+    class SmokeShellWhiteUW;
+    class BNA_KC_Effects_BactaSmokeUW: SmokeShellWhiteUW
+    {
+        interval = 0.01;
+        ignoreWind = true;
     };
 };
