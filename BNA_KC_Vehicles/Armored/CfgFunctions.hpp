4f6c70d4d2b582a945c4d655d40869eed2e585f5
DartRuffian <skulldude327@gmail.com>
2023-08-14 11:20:20 -0500

    Converted tabs to spaces

--- BNA_KC_Vehicles/Armored/CfgFunctions.hpp
+++ BNA_KC_Vehicles/Armored/CfgFunctions.hpp
@@ -1,12 +1,12 @@
 class CfgFunctions
 {
-	class BNAKC
-	{
-		class Vehicles
-		{
-			file = "BNA_KC_Vehicles\Armored\Data\Functions";
-			class activateEMP {};
+    class BNAKC
+    {
+        class Vehicles
+        {
+            file = "BNA_KC_Vehicles\Armored\Data\Functions";
+            class activateEMP {};
             class canUseEMP {};
-		};
-	};
+        };
+    };
 };
