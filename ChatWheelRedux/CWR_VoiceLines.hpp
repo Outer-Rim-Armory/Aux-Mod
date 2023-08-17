f267ba8e5fa4df598cb77e748d733c9a2d87b066
DartRuffian <skulldude327@gmail.com>
2023-08-17 13:35:25 -0500

    Renamed voicelines folder to audio

--- ChatWheelRedux/CWR_VoiceLines.hpp
+++ ChatWheelRedux/CWR_VoiceLines.hpp
@@ -4,13 +4,13 @@ class CWR_VoiceLines
     {
         voiceLines[] =
         {
-            "ChatWheelRedux\Data\Voicelines\NeedMedic\ArmorCantTakeIt.wss",
-            "ChatWheelRedux\Data\Voicelines\NeedMedic\ArmorFailing.wss",
-            "ChatWheelRedux\Data\Voicelines\NeedMedic\ArmorsComingApart.wss",
-            "ChatWheelRedux\Data\Voicelines\NeedMedic\NeedAMedic.wss",
-            "ChatWheelRedux\Data\Voicelines\NeedMedic\NotSureIfICanMakeIt.wss",
-            "ChatWheelRedux\Data\Voicelines\NeedMedic\StartingToHurt.wss",
-            "ChatWheelRedux\Data\Voicelines\NeedMedic\TroubleBreathing.wss"
+            "ChatWheelRedux\Data\Audio\NeedMedic\ArmorCantTakeIt.wss",
+            "ChatWheelRedux\Data\Audio\NeedMedic\ArmorFailing.wss",
+            "ChatWheelRedux\Data\Audio\NeedMedic\ArmorsComingApart.wss",
+            "ChatWheelRedux\Data\Audio\NeedMedic\NeedAMedic.wss",
+            "ChatWheelRedux\Data\Audio\NeedMedic\NotSureIfICanMakeIt.wss",
+            "ChatWheelRedux\Data\Audio\NeedMedic\StartingToHurt.wss",
+            "ChatWheelRedux\Data\Audio\NeedMedic\TroubleBreathing.wss"
         };
     };
 };
