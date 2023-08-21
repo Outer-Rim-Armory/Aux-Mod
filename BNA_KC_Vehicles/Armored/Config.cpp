9c8fef2a521ed8c8edf8c3c1c38a9f063b55e521
DartRuffian <skulldude327@gmail.com>
2023-08-20 22:37:38 -0500

    Standard/imperial paints for TX-130

--- BNA_KC_Vehicles/Armored/Config.cpp
+++ BNA_KC_Vehicles/Armored/Config.cpp
@@ -636,6 +636,42 @@ class CfgVehicles
 			"BNA_KC_Vehicles\Armored\Data\Textures\Saber\BNA_KC_Saber_Body.paa",
 			"BNA_KC_Vehicles\Armored\Data\Textures\Saber\BNA_KC_Saber_Weapons.paa"
 		};
+
+        textureList[] = { "Standard", 0, "Imperial", 0, "KeeliCompany", 1 };
+        class TextureSources
+        {
+            class Standard
+            {
+                author = "3rd Army Studios";
+                displayName = "Standard";
+                factions[] = { "BNA_KC_Faction" };
+                textures[] =
+                {
+                    "3as\3as_saber\data\saber_hull_co.paa",
+                    "3as\3as_saber\data\saber_weapons_co.paa"
+                };
+            };
+            class Imperial: Standard
+            {
+                author = "Charger";
+                displayName = "Imperial";
+                textures[] =
+                {
+                    "3AS\3as_saber\data\saber_Imp_hull_co.paa",
+                    "3AS\3as_saber\data\saber_Imp_weapons_co.paa"
+                };
+            };
+            class KeeliCompany: Standard
+            {
+                author = "Dexus";
+                displayName = "Keeli Company";
+                textures[] =
+                {
+                    "BNA_KC_Vehicles\Armored\Data\Textures\Saber\BNA_KC_Saber_Body.paa",
+                    "BNA_KC_Vehicles\Armored\Data\Textures\Saber\BNA_KC_Saber_Weapons.paa"
+                };
+            };
+        };
 	};
 
     class 3as_ATTE_base;
