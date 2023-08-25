c729d0c5fe9ae190878704fef861c3d3139a19d9
DartRuffian <skulldude327@gmail.com>
2023-08-25 16:53:07 -0500

    Created base LAAT class for LAAT/is

--- BNA_KC_Vehicles/Aviation/Config.cpp
+++ BNA_KC_Vehicles/Aviation/Config.cpp
@@ -5,41 +5,38 @@
 
 class CfgVehicles
 {
-    class 3as_laat_Base;
-    class 3as_LAAT_Mk1: 3as_laat_Base
+    class B_Heli_Attack_01_base_F;
+    class 3as_laat_Base: B_Heli_Attack_01_base_F
     {
         class UserActions;
         class pilotCamera;
+        class AnimationSources;
     };
-    // class 3as_LAAT_Mk1Lights;
-    class 3as_LAAT_Mk2: 3as_laat_Base
-    {
-        class UserActions;
-        class pilotCamera;
-    };
-    // class 3as_LAAT_Mk2Lights;
 
-    class BNA_KC_LAATi_MK1: 3as_LAAT_Mk1
+    class BNA_KC_LAATi_Base: 3as_laat_Base
     {
         // Mod Info
         dlc = "BNA_KC";
         author = "SweMonkey and DartRuffian";
 
         // Scope
-        scope = 2;
-        scopeCurator = 2;
+        scope = 1;
+        scopeCurator = 0;
 
         // Editor Attributes
         faction = "BNA_KC_Faction";
         editorSubcategory = "BNA_KC_SubCat_VAviation";
 
-        displayName = "[KC] LAAT/i MK1";
+        displayName = "[KC] LAAT/i (Base)";
+        crew = "BNA_KC_Unit_Phase2_Pilot";
+        side = 1;
 
+        // Armor and fuel
         armor = 200;
         fuelCapacity = 3000;
         fuelConsumptionRate = 0.12;
-        crew = "BNA_KC_Unit_Phase2_Pilot";
 
+        // Textures
         hiddenSelectionsTextures[] =
         {
             "BNA_KC_Vehicles\Aviation\Data\Textures\LAATi\BNA_KC_LAAT_Standard_Body.paa",
@@ -93,6 +90,7 @@ class CfgVehicles
             };
         };
 
+        // Weapons and mags
         weapons[] =
         {
             "ls_laat_gun",  // HE Gun
@@ -142,6 +140,7 @@ class CfgVehicles
             ace_missileguidance_usePilotCameraForTargeting = 1;
         };
 
+        // Special Load
         class UserActions: UserActions
         {
             class SpecialLoadVehicle
@@ -150,8 +149,11 @@ class CfgVehicles
                 // Notably used for 3AS's AT-TE
                 displayName = "Load Vehicle (Custom)";
                 displayNameDefault = "<img size=2 image='\a3\ui_f\data\IGUI\Cfg\Actions\loadVehicle_ca.paa'>";
-                onlyForPlayer = 0;
+                
+                position = "pilotview";
                 radius = 30;
+                onlyForPlayer = 0;
+
                 hideOnUse = 1;
                 priority = 5;
 
@@ -160,166 +162,69 @@ class CfgVehicles
             };
         };
 
-        // Impulse
+        // Impulse Settings
         tas_can_impulse = 0; // Disables 3AS's Impulse System
-		ls_impulsor_soundOn = "BNA_KC_ImpulseOn";
-		ls_impulsor_soundOff = "BNA_KC_ImpulseOff";
+        ls_impulsor_soundOn = "BNA_KC_ImpulseOn";
+        ls_impulsor_soundOff = "BNA_KC_ImpulseOff";
         ls_impulsor_fuelDrain_1 = 0;
         ls_impulsor_fuelDrain_2 = 0;
         // ls_impulsor_boostSpeed_1 = 400; // Impulse speeds, default values listed
-        // ls_impulsor_boostSpeed_2 = 600; 
+        // ls_impulsor_boostSpeed_2 = 600;
+
+        accuracy = 5; // How easy it is for AI to indentify
+        cost = "3e+006"; // How "important" AI thinks it is
+        availableForSupportTypes[] = { "CAS_Heli", "Transport", "Drop" };
+        typicalCargo[] = { "BNA_KC_Unit_Phase2_Pilot" }; // What units are "normally" in the vehicle
+        vehicleClass = "Helicopter"; // What type of vehicle it is
+
+        // Animations
+        class AnimationSources: AnimationSources
+        {
+            class Doors
+            {
+                source = "user";
+                initPhase = 0;
+                animPeriod = 1;
+            };
+            class Lamps
+            {
+                source = "user";
+                initPhase = 0;
+                animPeriod = 1;
+            };
+            class Turrets
+            {
+                source = "user";
+                initPhase = 1;
+                animPeriod = 1;
+            };
+        };
     };
 
-    class BNA_KC_LAATi_MK2: 3as_LAAT_Mk2
+    class BNA_KC_LAATi_MK1: BNA_KC_LAATi_Base
     {
-        // Mod Info
-        dlc = "BNA_KC";
-        author = "SweMonkey and DartRuffian";
-
         // Scope
         scope = 2;
         scopeCurator = 2;
 
-        // Editor Attributes
-        faction = "BNA_KC_Faction";
-        editorSubcategory = "BNA_KC_SubCat_VAviation";
+        displayName = "[KC] LAAT/i MK1";
+    };
+
+    class BNA_KC_LAATi_MK2: 3as_LABNA_KC_LAATi_BaseAT_Mk2
+    {
+        // Scope
+        scope = 2;
+        scopeCurator = 2;
 
         displayName = "[KC] LAAT/i MK2";
 
-        armor = 200;
-        fuelCapacity = 3000;
-        fuelConsumptionRate = 0.12;
-        crew = "BNA_KC_Unit_Phase2_Pilot";
-
-        hiddenSelectionsTextures[] =
+        class AnimationSources: AnimationSources
         {
-            "BNA_KC_Vehicles\Aviation\Data\Textures\LAATi\BNA_KC_LAAT_Standard_Body.paa",
-            "BNA_KC_Vehicles\Aviation\Data\Textures\LAATi\BNA_KC_LAAT_Standard_Wings.paa",
-            "3AS\3as_Laat\LAATI\data\Weapons_CO.paa",
-            "3AS\3as_Laat\LAATI\data\Weapon_Details_CO.paa",
-            "3AS\3as_Laat\LAATI\data\Interior_CO.paa"
-        };
-        textureList[] = { "Standard", 0, "Imperial", 0, "KeeliCompany", 1 };
-        class TextureSources
-        {
-            class Standard
+            class Turrets: Turrets
             {
-                author = "3rd Army Studios";
-                displayName = "Standard";
-                factions[] = { "BNA_KC_Faction" };
-                textures[] =
-                {
-                    "3AS\3as_Laat\LAATI\data\Hull_CO.paa",
-                    "3AS\3as_Laat\LAATI\data\Wings_CO.paa",
-                    "3AS\3as_Laat\LAATI\data\Weapons_CO.paa",
-                    "3AS\3as_Laat\LAATI\data\Weapon_Details_CO.paa",
-                    "3AS\3as_Laat\LAATI\data\Interior_CO.paa"
-                };
-            };
-            class Imperial: Standard
-            {
-                author = "Charger";
-                displayName = "Imperial";
-                textures[] =
-                {
-                    "3AS\3as_Laat\LAATI\data\Hull_Imp_CO.paa",
-                    "3AS\3as_Laat\LAATI\data\wings_Imp_CO.paa",
-                    "3AS\3as_Laat\LAATI\data\weapons_Imp_CO.paa",
-                    "3AS\3as_Laat\LAATI\data\weapon_Details_Imp_CO.paa",
-                    "3AS\3as_Laat\LAATI\data\interior_Imp_CO.paa"
-                };
-            };
-            class KeeliCompany: Standard
-            {
-                author = "Dexus";
-                displayName = "Keeli Company";
-                textures[] =
-                {
-                    "BNA_KC_Vehicles\Aviation\Data\Textures\LAATi\BNA_KC_LAAT_Standard_Body.paa",
-                    "BNA_KC_Vehicles\Aviation\Data\Textures\LAATi\BNA_KC_LAAT_Standard_Wings.paa",
-                    "3AS\3as_Laat\LAATI\data\Weapons_CO.paa",
-                    "3AS\3as_Laat\LAATI\data\Weapon_Details_CO.paa",
-                    "3AS\3as_Laat\LAATI\data\Interior_CO.paa"
-                };
+                animPeriod = 0.001;
             };
         };
-
-        weapons[] =
-        {
-            "ls_laat_gun",  // HE Gun
-            "ls_laat_gun_2", // AP Gun
-            "ls_laat_dar",
-            "3as_LAAT_Missile_AGM",
-            "3as_LAAT_Missile_AA",
-
-            "Laserdesignator_pilotCamera",
-            "ls_weapon_CMFlareLauncher",
-            "SmokeLauncher"
-        };
-        magazines[] =
-        {
-            "200rnd_laat_he_mag",
-            "200rnd_laat_he_mag",
-            "200rnd_laat_apfsds_mag",
-            "200rnd_laat_apfsds_mag",
-
-            "24Rnd_missiles", // Hydra Missiles
-            "3as_LAAT_8Rnd_Missile_AGM",
-            "3as_LAAT_8Rnd_Missile_AGM",
-            "3as_PylonMissile_LAAT_8Rnd_Missile_AA",
-            "3as_PylonMissile_LAAT_8Rnd_Missile_AA",
-
-            "Laserbatteries",
-            "Laserbatteries",
-
-            "ls_mag_300rnd_CMFlareChaff_blue",
-            "ls_mag_300rnd_CMFlareChaff_blue",
-            "ls_mag_300rnd_CMFlareChaff_blue",
-            "SmokeLauncherMag"
-        };
-
-        class pilotCamera: pilotCamera
-        {
-            initTurn = 0;   // Initial states
-            initElev = -10;
-            maxTurn = 180;  // Turn radius
-            minTurn = -180;
-            maxElev = 90;   // Elevation (how far up/down)
-            minElev = -10;
-
-            maxXRotSpeed = 0.3;
-            maxYRotSpeed = 0.3;
-
-            ace_missileguidance_usePilotCameraForTargeting = 1;
-        };
-
-        class UserActions: UserActions
-        {
-            class SpecialLoadVehicle
-            {
-                // Special action used for vehicles that are not fully compatible with ViV (vehicle-in-vehicle)
-                // Notably used for 3AS's AT-TE
-                displayName = "Load Vehicle (Custom)";
-                displayNameDefault = "<img size=2 image='\a3\ui_f\data\IGUI\Cfg\Actions\loadVehicle_ca.paa'>";
-                onlyForPlayer = 0;
-
-                radius = 30;
-                hideOnUse = 1;
-                priority = 5;
-
-                condition = QUOTE(this call BNAKC_fnc_canSpecialLoad;);
-                statement = QUOTE(this call BNAKC_fnc_specialLoad;);
-            };
-        };
-
-        // Impulse
-        tas_can_impulse = 0; // Disables 3AS's Impulse System
-		ls_impulsor_soundOn = "BNA_KC_ImpulseOn";
-		ls_impulsor_soundOff = "BNA_KC_ImpulseOff";
-        ls_impulsor_fuelDrain_1 = 0;
-        ls_impulsor_fuelDrain_2 = 0;
-        // ls_impulsor_boostSpeed_1 = 400; // Impulse speeds, default values listed
-        // ls_impulsor_boostSpeed_2 = 600; 
     };
 
     class lsd_laatc_base;
