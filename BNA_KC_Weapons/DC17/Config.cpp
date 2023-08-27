f76224caf65ff7337d6e446563401def080cb56f
DartRuffian <skulldude327@gmail.com>
2023-08-26 21:30:10 -0500

    Removed ability to equip shield to GR-20

--- BNA_KC_Weapons/DC17/Config.cpp
+++ BNA_KC_Weapons/DC17/Config.cpp
@@ -264,12 +264,13 @@ class CfgWeapons
         };
     };
 
-    class BNA_KC_GR20: BNA_KC_DC17
+    class BNA_KC_GR20: BNA_KC_DC17_Base
     {
         displayName = "[KC] GR-20";
         descriptionShort = "A GR-10 modified to shoot pressurized<br/>bacta canisters instead of plasma.";
         baseWeapon = "BNA_KC_GR20";
 
+        JLTS_canHaveShield = 0;
         JLTS_friedItem = "BNA_KC_GR20_Fried";
 
         magazines[] = {};
