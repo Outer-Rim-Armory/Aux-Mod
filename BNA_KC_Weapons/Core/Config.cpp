5f648f954a3517d1f1090b83a33b9f74c2abe8d8
DartRuffian <skulldude327@gmail.com>
2023-09-01 10:02:25 -0500

    Removed comment

--- BNA_KC_Weapons/Core/Config.cpp
+++ BNA_KC_Weapons/Core/Config.cpp
@@ -176,18 +176,3 @@ class CfgMagazineWells
         };
     };
 };
-/*
-"3Rnd_UGL_FlareWhite_F",
-            "3Rnd_UGL_FlareRed_F",
-            "BNA_KC_3Rnd_UGL_FlareBlue",
-            "3Rnd_UGL_FlareCIR_F",
-            "3Rnd_Smoke_Grenade_shell",
-            "3Rnd_SmokeRed_Grenade_shell",
-            "3Rnd_SmokeGreen_Grenade_shell",
-            "3Rnd_SmokeYellow_Grenade_shell",
-            "3Rnd_SmokePurple_Grenade_shell",
-            "3Rnd_SmokeBlue_Grenade_shell",
-            "3Rnd_SmokeOrange_Grenade_shell",
-            "3Rnd_HE_Grenade_shell",
-            "ACE_HuntIR_M203"
-*/
