af91c52f35d570cc5bb9b4e928b2b8c3c1448c86
DartRuffian <skulldude327@gmail.com>
2023-09-05 16:01:13 -0500

    Mortar uses updated 3AS sounds

--- BNA_KC_Vehicles/Static/Macros.hpp
+++ BNA_KC_Vehicles/Static/Macros.hpp
@@ -1,13 +1,4 @@
-#define MORTAR_SOUND_FIRE() class StandardSound \
-{ \
-    begin1[] = {"swlb_core\data\sounds\vehicles\mortar\weapon\mortar_shot1.wss", 1.5, 1, 250}; \
-    begin2[] = {"swlb_core\data\sounds\vehicles\mortar\weapon\mortar_shot2.wss", 1.5, 1, 250}; \
-    begin3[] = {"swlb_core\data\sounds\vehicles\mortar\weapon\mortar_shot3.wss", 1.5, 1, 250}; \
-    begin4[] = {"swlb_core\data\sounds\vehicles\mortar\weapon\mortar_shot4.wss", 1.5, 1, 250}; \
-    begin5[] = {"swlb_core\data\sounds\vehicles\mortar\weapon\mortar_shot5.wss", 1.5, 1, 250}; \
-    soundBegin[] = {"begin1", 0.15, "begin2", 0.25, "begin3", 0.1, "begin4", 0.3, "begin5", 0.2}; \
-}; \
-reloadSound[] = {"swlb_core\data\sounds\vehicles\mortar\weapon\mortar_reload.wss", 2, 1, 20}; \
+#define MORTAR_SOUND_FIRE() reloadSound[] = {"swlb_core\data\sounds\vehicles\mortar\weapon\mortar_reload.wss", 2, 1, 20}; \
 reloadMagazineSound[] = {"swlb_core\data\sounds\vehicles\mortar\weapon\mortar_reloadMagazine.wss", 1, 1, 20};
 
 
