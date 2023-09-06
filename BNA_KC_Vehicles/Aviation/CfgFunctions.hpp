ec18ce0c42e1fdff8b369befdff7b7be9e9fbebf
DartRuffian <skulldude327@gmail.com>
2023-09-06 10:22:05 -0500

    Tabs to spaces

--- BNA_KC_Vehicles/Aviation/CfgFunctions.hpp
+++ BNA_KC_Vehicles/Aviation/CfgFunctions.hpp
@@ -3,11 +3,11 @@ class CfgFunctions
     class BNAKC
     {
         class Aviation
-		{
-			file = "BNA_KC_Vehicles\Aviation\Data\Functions";
-			class canSpecialLoad {};
-			class specialLoad {};
-		};
+        {
+            file = "BNA_KC_Vehicles\Aviation\Data\Functions";
+            class canSpecialLoad {};
+            class specialLoad {};
+        };
     };
 };
 
@@ -15,11 +15,11 @@ class CfgFunctions
 class Extended_init_EventHandlers
 {
     // Add LS Impulse to 3AS vics
-	class BNA_KC_LAATi_Base
-	{
-		class BNA_KC_Impulse_Init
-		{
-			init = "(_this) spawn ls_vehicle_fnc_ImpulsorMonitor;";
-		};
-	};
+    class BNA_KC_LAATi_Base
+    {
+        class BNA_KC_Impulse_Init
+        {
+            init = "(_this) spawn ls_vehicle_fnc_ImpulsorMonitor;";
+        };
+    };
 };
