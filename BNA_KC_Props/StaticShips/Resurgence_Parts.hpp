8db3e57648ed61248040dde750d3e5766c03864c
DartRuffian <skulldude327@gmail.com>
2023-09-09 18:51:25 -0500

    Hid venator parts

--- BNA_KC_Props/StaticShips/Resurgence_Parts.hpp
+++ BNA_KC_Props/StaticShips/Resurgence_Parts.hpp
@@ -1,174 +1,224 @@
 class ven_body1_1;
 class BNA_KC_Ven_Resurgence_Body1_1: ven_body1_1
 {
-	displayName = "[KC] Resurgence Body 1-1";
-    hiddenSelectionsTextures[] = {BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body1.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Body 1-1";
+    hiddenSelectionsTextures[] = {"BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body1.paa"};
 };
 
 class ven_body1_2;
 class BNA_KC_Ven_Resurgence_Body1_2: ven_body1_2
 {
-	displayName = "[KC] Resurgence Body 1-2";
-    hiddenSelectionsTextures[] = {BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body1.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Body 1-2";
+    hiddenSelectionsTextures[] = {"BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body1.paa"};
 };
 
 class ven_body2_1;
 class BNA_KC_Ven_Resurgence_Body2_1: ven_body2_1
 {
-	displayName = "[KC] Resurgence Body 2-1";
-    hiddenSelectionsTextures[] = {BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body2.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Body 2-1";
+    hiddenSelectionsTextures[] = {"BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body2.paa"};
 };
 
 class ven_body2_2;
 class BNA_KC_Ven_Resurgence_Body2_2: ven_body2_2
 {
-	displayName = "[KC] Resurgence Body 2-2";
-    hiddenSelectionsTextures[] = {BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body2.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Body 2-2";
+    hiddenSelectionsTextures[] = {"BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body2.paa"};
 };
 
 class ven_body2_3;
 class BNA_KC_Ven_Resurgence_Body2_3: ven_body2_3
 {
-	displayName = "[KC] Resurgence Body 2-3";
-    hiddenSelectionsTextures[] = {BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body2.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Body 2-3";
+    hiddenSelectionsTextures[] = {"BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body2.paa"};
 };
 
 class ven_body3;
 class BNA_KC_Ven_Resurgence_Body3: ven_body3
 {
-	displayName = "[KC] Resurgence Body 3";
-    hiddenSelectionsTextures[] = {BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body3.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Body 3";
+    hiddenSelectionsTextures[] = {"BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body3.paa"};
 };
 
 class ven_body4;
 class BNA_KC_Ven_Resurgence_Body4: ven_body4
 {
-	displayName = "[KC] Resurgence Body 4";
-    hiddenSelectionsTextures[] = {BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body4.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Body 4";
+    hiddenSelectionsTextures[] = {"BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body4.paa"};
 };
 
 class ven_body5;
 class BNA_KC_Ven_Resurgence_Body5: ven_body5
 {
-	displayName = "[KC] Resurgence Body 5";
-    hiddenSelectionsTextures[] = {BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body5.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Body 5";
+    hiddenSelectionsTextures[] = {"BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body5.paa"};
 };
 
 class ven_body6;
 class BNA_KC_Ven_Resurgence_Body6: ven_body6
 {
-	displayName = "[KC] Resurgence Body 6";
-    hiddenSelectionsTextures[] = {BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body6.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Body 6";
+    hiddenSelectionsTextures[] = {"BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body6.paa"};
 };
 
 class ven_body7;
 class BNA_KC_Ven_Resurgence_Body7: ven_body7
 {
-	displayName = "[KC] Resurgence Body 7";
-    hiddenSelectionsTextures[] = {BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body7.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Body 7";
+    hiddenSelectionsTextures[] = {"BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body7.paa"};
 };
 
 class ven_body8;
 class BNA_KC_Ven_Resurgence_Body8: ven_body8
 {
-	displayName = "[KC] Resurgence Body 8";
-    hiddenSelectionsTextures[] = {BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body8.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Body 8";
+    hiddenSelectionsTextures[] = {"BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body8.paa"};
 };
 
 class ven_body9;
 class BNA_KC_Ven_Resurgence_Body9: ven_body9
 {
-	displayName = "[KC] Resurgence Body 9";
-    hiddenSelectionsTextures[] = {BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body9.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Body 9";
+    hiddenSelectionsTextures[] = {"BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body9.paa"};
 };
 
 class ven_body10;
 class BNA_KC_Ven_Resurgence_Body10: ven_body10
 {
-	displayName = "[KC] Resurgence Body 10";
-    hiddenSelectionsTextures[] = {BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body10.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Body 10";
+    hiddenSelectionsTextures[] = {"BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Body10.paa"};
 };
 
 class ven_bridge;
 class BNA_KC_Ven_Resurgence_Bridge: ven_bridge
 {
-	displayName = "[KC] Resurgence Bridge";
-    hiddenSelectionsTextures[] = {BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Bridge.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Bridge";
+    hiddenSelectionsTextures[] = {"BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Bridge.paa"};
 };
 
 class ven_door;
 class BNA_KC_Ven_Resurgence_Door: ven_door
 {
-	displayName = "[KC] Resurgence Door (Closed)";
-    hiddenSelectionsTextures[] = {BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Door.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Door (Closed)";
+    hiddenSelectionsTextures[] = {"BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Door.paa"};
 };
 
 class ven_door2;
 class BNA_KC_Ven_Resurgence_Door2: ven_door2
 {
-	displayName = "[KC] Resurgence Door (Open)";
-    hiddenSelectionsTextures[] = {"", BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Door.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Door (Open)";
+    hiddenSelectionsTextures[] = {"", "BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Door.paa"};
 };
 
 class ven_engine;
 class BNA_KC_Ven_Resurgence_Engine: ven_engine
 {
-	displayName = "[KC] Resurgence Engine";
-    hiddenSelectionsTextures[] = {BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Engine.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Engine";
+    hiddenSelectionsTextures[] = {"BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Engine.paa"};
 };
 
 class ven_interior1;
 class BNA_KC_Ven_Resurgence_Interior1: ven_interior1
 {
-	displayName = "[KC] Resurgence Interior 1";
-    hiddenSelectionsTextures[] = {BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Interior1.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Interior 1";
+    hiddenSelectionsTextures[] = {"BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Interior1.paa"};
 };
 
 class ven_interior2;
 class BNA_KC_Ven_Resurgence_Interior2: ven_interior2
 {
-	displayName = "[KC] Resurgence Interior 2";
-    hiddenSelectionsTextures[] = {BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Interior2.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Interior 2";
+    hiddenSelectionsTextures[] = {"BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Interior2.paa"};
 };
 
 class ven_interior3;
 class BNA_KC_Ven_Resurgence_Interior3: ven_interior3
 {
-	displayName = "[KC] Resurgence Interior 3";
-    hiddenSelectionsTextures[] = {BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Interior3.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Interior 3";
+    hiddenSelectionsTextures[] = {"BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Interior3.paa"};
 };
 
 class ven_interior4;
 class BNA_KC_Ven_Resurgence_Interior4: ven_interior4
 {
-	displayName = "[KC] Resurgence Interior 4";
-    hiddenSelectionsTextures[] = {BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Interior4.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Interior 4";
+    hiddenSelectionsTextures[] = {"BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Interior4.paa"};
 };
 
 class ven_interior5;
 class BNA_KC_Ven_Resurgence_Interior5: ven_interior5
 {
-	displayName = "[KC] Resurgence Interior 5";
-    hiddenSelectionsTextures[] = {BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Interior5.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Interior 5";
+    hiddenSelectionsTextures[] = {"BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Interior5.paa"};
 };
 
 class ven_interior6;
 class BNA_KC_Ven_Resurgence_Interior6: ven_interior6
 {
-	displayName = "[KC] Resurgence Interior 6";
-    hiddenSelectionsTextures[] = {BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Interior6.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Interior 6";
+    hiddenSelectionsTextures[] = {"BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Interior6.paa"};
 };
 
 class ven_interior7;
 class BNA_KC_Ven_Resurgence_Interior7: ven_interior7
 {
-	displayName = "[KC] Resurgence Interior 7";
-    hiddenSelectionsTextures[] = {BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Interior7.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Interior 7";
+    hiddenSelectionsTextures[] = {"BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Interior7.paa"};
 };
 
 class ven_interior8;
 class BNA_KC_Ven_Resurgence_Interior8: ven_interior8
 {
-	displayName = "[KC] Resurgence Interior 8";
-    hiddenSelectionsTextures[] = {BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Interior8.paa};
+    scope = 1;
+    scopeCurator = 0;
+    displayName = "[KC] Resurgence Interior 8";
+    hiddenSelectionsTextures[] = {"BNA_KC_Props\StaticShips\Data\Resurgence\BNA_KC_Venator_Interior8.paa"};
 };
