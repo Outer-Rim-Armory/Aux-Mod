e1e9558e12616513224980bd8333fe2766bbc5c8
DartRuffian <skulldude327@gmail.com>
2023-09-17 17:10:30 -0500

    Increased mag size to 20

--- BNA_KC_Weapons/DC15X/Config.cpp
+++ BNA_KC_Weapons/DC15X/Config.cpp
@@ -141,8 +141,9 @@ class CfgMagazines
     {
         displayName = "[12th Fleet] DC-15X Energy Cell";
         displayNameShort = "High Energy";
-        descriptionShort = "Energy Cell Pack<br/>Rounds: 15<br/>Used In: DC-15X";
+        descriptionShort = "Energy Cell Pack<br/>Rounds: 20<br/>Used In: DC-15X";
         ammo = "12thFleet_Ammo_Sniper_Blue";
+        count = 20;
 
         JLTS_hasEMPProtection = 1;
     };
