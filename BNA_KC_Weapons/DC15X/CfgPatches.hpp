9b1f6027fd605199208c31501b3f88ad170b172a
DartRuffian <skulldude327@gmail.com>
2023-09-17 19:42:39 -0500

    Fixed CfgPatches

--- BNA_KC_Weapons/DC15X/CfgPatches.hpp
+++ BNA_KC_Weapons/DC15X/CfgPatches.hpp
@@ -9,14 +9,14 @@ class CfgPatches
         {
             "JLTS_weapons_DC15X",
                 // DC-15X
-            //"JLTS_weapons_DC17SA", // Looked odd when on the DC-15X
+            "JLTS_weapons_DC17SA",
                 // Flashlight
-            //"A3_Weapons_F_Acc",
+            "A3_Weapons_F_Acc",
                 // Laser pointer
-            //"ace_laserpointer",
+            "ace_laserpointer",
                 // Green laser pointer
             "A3_Weapons_F_Mark_Acc"
-                // Bipods
+                // Bipod
         };
         units[] = {};
         weapons[] =
