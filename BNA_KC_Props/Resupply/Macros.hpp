3d7fa553aee2d12546140c524753fe1d8b6935a7
DartRuffian <skulldude327@gmail.com>
2023-09-18 15:55:38 -0500

    Weapon / item macros

--- BNA_KC_Props/Resupply/Macros.hpp
+++ BNA_KC_Props/Resupply/Macros.hpp
@@ -1,5 +1,17 @@
+#define WEAP_XX(WEAP, COUNT) class _xx_##MAG \
+{ \
+    weapon = #WEAP; \
+    count = COUNT; \
+};
+
 #define MAG_XX(MAG, COUNT) class _xx_##MAG \
 { \
     magazine = #MAG; \
+    count = COUNT; \
+};
+
+#define ITEM_XX(ITEM, COUNT) class _xx_##MAG \
+{ \
+    name = #ITEM; \
     count = COUNT; \
 };
