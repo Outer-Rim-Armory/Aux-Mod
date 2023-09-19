250562fe261e8fec475694f1c21aea2ea1012c14
DartRuffian <skulldude327@gmail.com>
2023-09-19 14:01:42 -0500

    Commando Base

---
+++ BNA_KC_Gear/Commandos/CfgPatches.hpp
@@ -0,0 +1,27 @@
+class CfgPatches
+{
+    class BNA_KC_Gear_Commandos
+    {
+        addonRootClass= "BNA_KC_Gear";
+        author = "SweMonkey and DartRuffian";
+        requiredVersion = 1;
+        requiredAddons[] =
+        {
+            "BNA_KC_Gear_SpecialForces",
+                // SF Unit, Uniform, etc. as base
+            "ls_armor_bluefor",
+                // Commando Helmet
+                // Commando Uniform
+            "SWLB_clones_spec_backpacks"
+                // Commando Backpacks
+        };
+        units[] =
+        {
+
+        };
+        weapons[] =
+        {
+
+        };
+    };
+};
---
+++ BNA_KC_Gear/Commandos/Config.cpp
@@ -0,0 +1,46 @@
+#include "CfgPatches.hpp"
+
+class CfgWeapons
+{
+    // ┌───────────────────┐
+    // │      Helmets      │
+    // └───────────────────┘
+
+
+    // ┌────────────────────┐
+    // │      Uniforms      │
+    // └────────────────────┘
+
+
+    // ┌───────────────────┐
+    // │       Vests       │
+    // └───────────────────┘
+};
+
+
+class CfgVehicles
+{
+    // ┌───────────────────┐
+    // │       Units       │
+    // └───────────────────┘
+
+
+    // ┌───────────────────┐
+    // │     Backpacks     │
+    // └───────────────────┘
+};
+
+
+class CfgEditorSubcategories
+{
+    class BNA_KC_Subcat_Commandos
+    {
+        dlc = "BNA_KC";
+        author = "SweMonkey and DartRuffian";
+
+        scope = 2;
+        scopeCurator = 2;
+
+        displayName = "Commandos";
+    };
+};
