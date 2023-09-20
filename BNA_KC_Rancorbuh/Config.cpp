b2df607d9dd5729f8c5918bc8b620e57fd808286
DartRuffian <skulldude327@gmail.com>
2023-09-19 22:23:50 -0500

    Faction and category for zombies

--- BNA_KC_Rancorbuh/Config.cpp
+++ BNA_KC_Rancorbuh/Config.cpp
@@ -10,6 +10,9 @@ class CfgVehicles
         uniformClass = "BNA_KC_Uniform_Rancor";
         nakedUniform = "BNA_KC_Uniform_Rancor";
         model = "BNA_KC_Rancorbuh\Model\Rancor.p3d";
+
+        faction = "BNA_KC_Faction_Zombies";
+        editorSubcategory = "BNA_KC_SubCat_Creatures";
     };
 };
 
@@ -41,5 +44,33 @@ class Extended_InitPost_EventHandlers
         {
             init = "_unit = _this select 0; if (local _unit) then {_unit execVM '\WBK_Zombies\AI\WBK_AI_Smasher.sqf';};";
         };
+    };
+};
+
+
+class CfgFactionClasses
+{
+    class BNA_KC_Faction_Zombies
+    {
+        dlc = "BNA_KC";
+        author = "SweMonkey and DartRuffian";
+        scope = 2;
+        scopeCurator = 2;
+        displayName = "[KC] Zombies and Creatures";
+    };
+};
+
+
+class CfgEditorSubcategories
+{
+    class BNA_KC_SubCat_Creatures
+    {
+        dlc = "BNA_KC";
+        author = "SweMonkey and DartRuffian";
+
+        scope = 2;
+        scopeCurator = 2;
+
+        displayName = "Creatures";
     };
 };
