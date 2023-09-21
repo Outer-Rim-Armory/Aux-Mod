cdb38856e7d3b29d34f852383b8281840ba7c623
DartRuffian <skulldude327@gmail.com>
2023-09-20 21:07:51 -0500

    DC-15A Sound Sets

--- BNA_KC_Weapons/Core/Config.cpp
+++ BNA_KC_Weapons/Core/Config.cpp
@@ -229,3 +229,35 @@ class CfgMagazineWells
         };
     };
 };
+
+
+class CfgSoundShaders
+{
+    class BNA_KC_SoundShader_Weapon_Base
+    {
+        range = 1800;
+        volume = 1;
+        samples[] = {};
+    };
+};
+
+class CfgSoundSets
+{
+    class BNA_KC_SoundSet_Weapon_Base
+    {
+        soundShaders[] = {"BNA_KC_SoundShader_Weapon_Base"};
+        soundShadersLimit = 1;
+        sound3DProcessingType = "WeaponMediumShot3DProcessingType";
+
+        volumeCurve = "InverseSquare2Curve";
+        volumeFactor = 1;
+
+        distanceFilter = "weaponShotDistanceFreqAttenuationFilter";
+        obstructionFactor = 0.3;
+        occlusionFactor = 0.5;
+
+        spatial = 1;
+        doppler = 0;
+        loop = 0;
+    };
+};
--- BNA_KC_Weapons/DC15A/Config.cpp
+++ BNA_KC_Weapons/DC15A/Config.cpp
@@ -92,30 +92,20 @@ class CfgWeapons
         {
             class StandardSound: StandardSound
             {
-                begin1[] =
-                {
-                    "BNA_KC_Weapons\DC15A\Data\Audio\DC15A_Fire1.wss",
-                    1,
-                    1,
-                    1800
-                };
-                soundBegin[] = {begin1, 1};
-                soundBeginWater[] = {begin1, 1};
+                soundBegin[] = {};
+                soundBeginWater[] = {};
+                soundSetShot[] = {"BNA_KC_SoundSet_DC15A_Single"};
+                soundSetShotWater[] = {"BNA_KC_SoundSet_DC15A_Single"};
             };
         };
         class FullAuto: FullAuto
         {
             class StandardSound: StandardSound
             {
-                begin1[] =
-                {
-                    "BNA_KC_Weapons\DC15A\Data\Audio\DC15A_Fire1.wss",
-                    1,
-                    1,
-                    1800
-                };
-                soundBegin[] = {begin1, 1};
-                soundBeginWater[] = {begin1, 1};
+                soundBegin[] = {};
+                soundBeginWater[] = {};
+                soundSetShot[] = {"BNA_KC_SoundSet_DC15A_FullAuto"};
+                soundSetShotWater[] = {"BNA_KC_SoundSet_DC15A_FullAuto"};
             };
         };
         class Stun: Stun
@@ -235,5 +225,40 @@ class CfgMagazines
         ammo = "Aux_12thFleet_Ammo_Rifle_Blue";
 
         JLTS_hasEMPProtection = 1;
+    };
+};
+
+class CfgSoundShaders
+{
+    class BNA_KC_SoundShader_Weapon_Base;
+    class BNA_KC_SoundShader_DC15A_Single: BNA_KC_SoundShader_Weapon_Base
+    {
+        samples[] =
+        {
+            {"BNA_KC_Weapons\DC15A\Data\Audio\DC15A_Fire1.wss", 1, 1},
+            {"BNA_KC_Weapons\DC15A\Data\Audio\DC15A_Fire2.wss", 1, 1},
+            {"BNA_KC_Weapons\DC15A\Data\Audio\DC15A_Fire3.wss", 1, 1},
+            {"BNA_KC_Weapons\DC15A\Data\Audio\DC15A_Fire4.wss", 1, 1}
+        };
+    };
+    class BNA_KC_SoundShader_DC15A_FullAuto: BNA_KC_SoundShader_DC15A_Single
+    {
+        samples[] =
+        {
+            {"BNA_KC_Weapons\DC15A\Data\Audio\DC15A_Fire1.wss", 1, 1}
+        };
+    };
+};
+
+class CfgSoundSets
+{
+    class BNA_KC_SoundSet_Weapon_Base;
+    class BNA_KC_SoundSet_DC15A_Single: BNA_KC_SoundSet_Weapon_Base
+    {
+        soundShaders[] = {"BNA_KC_SoundShader_DC15A_Single"};
+    };
+    class BNA_KC_SoundSet_DC15A_FullAuto: BNA_KC_SoundSet_DC15A_Single
+    {
+        soundShaders[] = {"BNA_KC_SoundShader_DC15A_FullAuto"};
     };
 };
