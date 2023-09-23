66a548694b760fcae74830bdf5067075f1de31f5
DartRuffian <skulldude327@gmail.com>
2023-09-22 23:31:12 -0500

    Increased melee unit health

--- BNA_KC_OPFOR/TechnoUnion/Config.cpp
+++ BNA_KC_OPFOR/TechnoUnion/Config.cpp
@@ -262,7 +262,7 @@ class Extended_Init_EventHandlers
     {
         class Melee_Init
         {
-            init = "_unit = _this select 0; if (local _unit) then {[_unit] execVM 'WebKnight_StarWars_Mechanic\AI_Ims_Rush.sqf';};";
+            init = "_unit = _this select 0; if (local _unit) then {[_unit] execVM 'WebKnight_StarWars_Mechanic\AI_Ims_Rush.sqf';}; _unit setVariable ['IMS_CustomHealthDamage', 20, true];"; // Default health is 2
         };
     };
 };
