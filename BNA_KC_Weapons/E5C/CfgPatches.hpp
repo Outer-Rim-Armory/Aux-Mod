a21b627d01c222cdaa283a47ebfc56063f6b6cc7
DartRuffian <skulldude327@gmail.com>
2023-09-22 22:16:58 -0500

    Base E-5C

---
+++ BNA_KC_Weapons/E5C/CfgPatches.hpp
@@ -0,0 +1,28 @@
+class CfgPatches
+{
+    class BNA_KC_Weapons_E5C
+    {
+        author = "SweMonkey and DartRuffian";
+        addonRootClass = "BNA_KC_Weapons"
+        requiredVersion = 0.1;
+        requiredAddons[] =
+        {
+            "BNA_KC_Weapons_Core",
+                // Ammo
+            "JLTS_weapons_E5C"
+                // E-5C
+        };
+        units[] = {};
+        weapons[] =
+        {
+            "BNA_KC_E5C_Base",
+            "BNA_KC_E5C",
+            "BNA_KC_E5C_Fried"
+        };
+        magazines[] =
+        {
+            "Aux_12thFleet_Mag_E5C"
+        };
+        ammo[] = {};
+    };
+};
---
+++ BNA_KC_Weapons/E5C/Config.cpp
@@ -0,0 +1,58 @@
+#include "CfgPatches.hpp"
+
+
+class CfgWeapons
+{
+    class JLTS_E5C;
+    class BNA_KC_E5C_Base: JLTS_E5C
+    {
+        displayName = "[KC] E-5C (Base)";
+        baseWeapon = "BNA_KC_E5C_Base";
+
+        muzzles[] = {"this"};
+        magazines[] = {"Aux_12thFleet_Mag_E5C"};
+        magazineWell[] = {};
+    };
+
+    class BNA_KC_E5C: BNA_KC_E5C_Base
+    {
+        // Scope
+        scope = 2;
+        scopeArsenal = 2;
+
+        displayName = "[KC] E-5C";
+        JLTS_friedItem = "BNA_KC_E5C_Fried";
+        baseWeapon = "BNA_KC_E5C";
+    };
+    class BNA_KC_E5C_Fried: BNA_KC_E5C
+    {
+        // Scope
+        scope = 1;
+        scopeArsenal = 0;
+
+        baseWeapon = "BNA_KC_E5C_Fried";
+
+        displayName = "[KC] E-5C (Fried)";
+        descriptionShort = "The circuits of the weapon have<br/>been fried by an EMP blast.";
+        picture = "\MRC\JLTS\weapons\E5C\data\ui\E5C_fried_ui_ca.paa";
+
+        JLTS_isFried = 1;
+        magazines[] = {};
+    };
+};
+
+
+class CfgMagazines
+{
+    class JLTS_E5C_mag;
+    class Aux_12thFleet_Mag_E5C: JLTS_E5C_mag
+    {
+        author = "DartRuffian and SweMonkey";
+        displayName = "[12th Fleet] E-5C Energy Cell";
+        displayNameShort = "Standard Energy";
+        descriptionShort = "Energy Cell Pack<br/>Rounds: 150<br/>Used In: E-5C";
+        ammo = "Aux_12thFleet_Ammo_Carbine_Red";
+
+        JLTS_hasEMPProtection = 1;
+    };
+};
