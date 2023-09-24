97b442ca7f6b5cc320c8b7d83115473ee0683ee0
DartRuffian <skulldude327@gmail.com>
2023-09-24 17:29:09 -0500

    Akali LAAT

--- BNA_KC_Vehicles/Aviation/Config.cpp
+++ BNA_KC_Vehicles/Aviation/Config.cpp
@@ -53,7 +53,7 @@ class CfgVehicles
             "3AS\3as_Laat\LAATI\data\Weapon_Details_CO.paa",
             "3AS\3as_Laat\LAATI\data\Interior_CO.paa"
         };
-        textureList[] = { "Standard", 0, "Imperial", 0, "KeeliCompany", 1 };
+        textureList[] = {"Standard", 0, "Imperial", 0, "KeeliCompany", 1, "Akali", 0};
         class TextureSources
         {
             class Standard
@@ -94,6 +94,19 @@ class CfgVehicles
                     "3AS\3as_Laat\LAATI\data\Weapons_CO.paa",
                     "3AS\3as_Laat\LAATI\data\Weapon_Details_CO.paa",
                     "3AS\3as_Laat\LAATI\data\Interior_CO.paa"
+                };
+            };
+            class Akali: Standard
+            {
+                author = "Rev";
+                displayName = "Akali";
+                textures[] =
+                {
+                    "BNA_KC_Vehicles\Aviation\Data\Textures\LAATi\Akali\Akali_Body.paa",
+                    "BNA_KC_Vehicles\Aviation\Data\Textures\LAATi\Akali\Akali_Wings.paa",
+                    "BNA_KC_Vehicles\Aviation\Data\Textures\LAATi\Akali\Akali_Weapons.paa",
+                    "BNA_KC_Vehicles\Aviation\Data\Textures\LAATi\Akali\Akali_Weapon_Details.paa",
+                    "BNA_KC_Vehicles\Aviation\Data\Textures\LAATi\Akali\Akali_Interior.paa"
                 };
             };
         };
