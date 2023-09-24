ce3d241ecfdaa94b8aef08d384366445e6443938
DartRuffian <skulldude327@gmail.com>
2023-09-23 19:55:29 -0500

    Anti-Armor Squad

--- BNA_KC_OPFOR/TechnoUnion/CfgGroups.hpp
+++ BNA_KC_OPFOR/TechnoUnion/CfgGroups.hpp
@@ -53,6 +53,49 @@ class CfgGroups
                         position[] = {-3, -3, 0};
                     };
                 };
+
+                class AntiArmorSquad
+                {
+                    faction = "BNA_KC_OPFOR_TU";
+                    name = "Anti-Armor Squad";
+                    icon = "\A3\ui_f\data\map\markers\nato\o_inf.paa";
+                    side = 0;
+
+                    class Object0
+                    {
+                        side = 0;
+                        vehicle = "BNA_KC_OPFOR_Unit_TU_SL";
+                        description = "Squad Lead";
+                        rank = "SERGEANT";
+                        position[] = {0, 0, 0};
+                        dir = 0;
+                    };
+                    class Object1: Object0
+                    {
+                        vehicle = "BNA_KC_OPFOR_Unit_TU_AT";
+                        description = "Rifleman (AT)";
+                        rank = "Private";
+                        position[] = {1, -1, 0};
+                    };
+                    class Object2: Object1
+                    {
+                        vehicle = "BNA_KC_OPFOR_Unit_TU_Rifleman";
+                        description = "Rifleman";
+                        position[] = {-1, -1, 0};
+                    };
+                    class Object3: Object2
+                    {
+                        position[] = {2, -2, 0};
+                    };
+                    class Object4: Object2
+                    {
+                        position[] = {-2, -2, 0};
+                    };
+                    class Object5: Object1
+                    {
+                        position[] = {3, -3, 0};
+                    };
+                };
             };
         };
     };
