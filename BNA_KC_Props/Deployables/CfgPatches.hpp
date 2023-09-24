07af8daf785869c5d2b65ef251db51be4112b0dd
DartRuffian <skulldude327@gmail.com>
2023-09-24 12:02:11 -0500

    Deployables preview

--- BNA_KC_Props/Deployables/CfgPatches.hpp
+++ BNA_KC_Props/Deployables/CfgPatches.hpp
@@ -9,6 +9,8 @@ class CfgPatches
         {
             "BNA_KC_Props_Resupply",
                 // Resupply object used as base
+            "BNA_KC_Props_Gonks",
+                // Editor Previews
             "cba_common",
                 // Base inventory item
             "RD501_patch_fx7_droid"
