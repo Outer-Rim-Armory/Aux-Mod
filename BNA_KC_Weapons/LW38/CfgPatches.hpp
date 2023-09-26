5fac7a8f8a77fe7e447f6d87360165e12acbe53b
DartRuffian <skulldude327@gmail.com>
2023-09-25 22:33:24 -0500

    Base LW-38 and fried version

---
+++ BNA_KC_Weapons/LW38/CfgPatches.hpp
@@ -0,0 +1,28 @@
+class CfgPatches
+{
+    class BNA_KC_Weapons_LW38
+    {
+        author = "SweMonkey and DartRuffian";
+        addonRootClass = "BNA_KC_Weapons"
+        requiredVersion = 0.1;
+        requiredAddons[] =
+        {
+            "LF_Weapon_Unit_lw38m",
+                // LW-38 model / magazine
+            "BNA_KC_Weapons_DC15A"
+                // DC-15A
+        };
+        units[] = {};
+        weapons[] =
+        {
+            "BNA_KC_LW38_Base",
+            "BNA_KC_LW38",
+            "BNA_KC_LW38_Fried",
+        };
+        magazines[] =
+        {
+            "BNA_KC_Mag_LW38"
+        };
+        ammo[] = {};
+    };
+};
---
+++ BNA_KC_Weapons/LW38/Config.cpp
@@ -0,0 +1,64 @@
+#include "CfgPatches.hpp"
+
+
+class CfgWeapons
+{
+    class BNA_KC_DC15A;
+    class BNA_KC_LW38_Base: BNA_KC_DC15A
+    {
+        // Scope
+        scope = 1;
+        scopeArsenal = 0;
+
+        displayName = "[KC] LW-38 (Base)";
+        baseWeapon = "BNA_KC_LW38_Base";
+
+        modes[] = {"Single", "FullAuto"};
+        muzzles[] = {"this"};
+        magazines[] = {"BNA_KC_Mag_LW38"};
+
+        model = "LF_Weapon_Unit\lw38m\lw38m.p3d";
+        hiddenSelections[] = {};
+        hiddenSelectionsTextures[] = {};
+        handAnim[] = {"OFP2_ManSkeleton", "LF_Weapon_Unit\lw38m\anims\lw38mhand.rtm"};
+        picture = "\LF_Weapon_Unit\lw38m\ui\lw38m.paa";
+        // picture = "\BNA_KC_Weapons\LW38M\Data\Textures\UI\LW38M_UI.paa";
+    };
+
+    class BNA_KC_LW38: BNA_KC_LW38_Base
+    {
+        // Scope
+        scope = 2;
+        scopeArsenal = 2;
+
+        displayName = "[KC] LW-38";
+        baseWeapon = "BNA_KC_LW38";
+        JLTS_friedItem = "BNA_KC_LW38_Fried";
+    };
+
+    class BNA_KC_LW38_Fried: BNA_KC_LW38
+    {
+        // Scope
+        scope = 1;
+        scopeArsenal = 0;
+
+        displayName = "[KC] LW-38 (Fried)";
+        descriptionShort = "The circuits of the weapon have<br/>been fried by an EMP blast.";
+        // picture = "\BNA_KC_Weapons\LW38\Data\Textures\UI\LW38_Fried_UI.paa";
+        baseWeapon = "BNA_KC_LW38_Fried";
+
+        JLTS_isFried = 1;
+        magazines[] = {};
+    };
+};
+
+
+class CfgMagazines
+{
+    class Aux12thFleet_Mag_DC15A;
+    class BNA_KC_Mag_LW38: Aux12thFleet_Mag_DC15A
+    {
+        displayName = "[KC] LW-38 Energy Cell";
+        descriptionShort = "Energy Cell Pack<br/>Rounds: 60<br/>Used In: LW-38";
+    };
+};
