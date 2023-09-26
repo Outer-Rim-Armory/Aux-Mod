b9dfa8b8b4c5fd2151eed1d54ff310b1a1a5b589
DartRuffian <skulldude327@gmail.com>
2023-09-26 11:49:21 -0500

    Base unit and rifleman

--- BNA_KC_OPFOR/Galtorran/CfgPatches.hpp
+++ BNA_KC_OPFOR/Galtorran/CfgPatches.hpp
@@ -7,15 +7,24 @@ class CfgPatches
         requiredVersion = 1;
         requiredAddons[] =
         {
-
+            "BNA_KC_OPFOR",
+                // Base uniform, unit, etc.
+            "ls_armor_greenfor",
+                // Mandalorian uniform textures
+            "OPTRE_UNSC_Units_Army",
+                // Snow Helmet
+            "SC_MDF",
+                // MDF White Vests
+            "OPTRE_Weapons_Backpacks"
+                // Backpacks
         };
         units[] =
         {
-
+            "BNA_KC_Galtorran_Unit_Base"
         };
         weapons[] =
         {
-
+            "BNA_KC_Galtorran_Uniform"
         };
 
         skipWhenMissingDependencies = 1;
--- BNA_KC_OPFOR/Galtorran/Config.cpp
+++ BNA_KC_OPFOR/Galtorran/Config.cpp
@@ -7,7 +7,24 @@ class CfgWeapons
     // ┌────────────────────┐
     // │      Uniforms      │
     // └────────────────────┘
+    class ls_redforUniform_base;
+    class BNA_KC_OPFOR_Uniform_Base: ls_redforUniform_base
+    {
+        class ItemInfo;
+    };
+    class BNA_KC_Galtorran_Uniform: BNA_KC_OPFOR_Uniform_Base
+    {
+        // Scope
+        scope = 2;
+        scopeArsenal = 2;
 
+        displayName = "[KC GM] Gal'torran Uniform";
+
+        class ItemInfo: ItemInfo
+        {
+            uniformClass = "BNA_KC_Galtorran_Unit_Base";
+        };
+    };
     // ┌───────────────────┐
     // │       Vests       │
     // └───────────────────┘
@@ -19,6 +36,37 @@ class CfgVehicles
     // ┌───────────────────┐
     // │       Units       │
     // └───────────────────┘
+    class BNA_KC_OPFOR_Unit_Base;
+    class BNA_KC_Galtorran_Unit_Base: BNA_KC_OPFOR_Unit_Base
+    {
+        faction = "BNA_KC_OPFOR_Galtorran";
+        editorSubcategory = "BNA_KC_SubCat_Galtorran_Infantry";
+
+        weapons[] = {};
+        respawnWeapons[] = {};
+        magazines[] = {};
+        respawnMagazines[] = {};
+        items[] = {};
+        respawnItems[] = {};
+
+        model = "\A3\Characters_F_Beta\INDEP\ia_soldier_01.p3d";
+        uniformClass = "BNA_KC_Galtorran_Uniform";
+        hiddenSelections[] = {"camo", "insignia"};
+        hiddenSelectionsTextures[] = {"\ls_armor_greenfor\uniform\mandalorian\undersuit\data\uniform_undersuit_co.paa"};
+    };
+
+    class BNA_KC_Galtorran_Unit_Rifleman: BNA_KC_Galtorran_Unit_Base
+    {
+        // Scope
+        scope = 2;
+        scopeCurator = 2;
+
+        displayName = "Rifleman";
+
+        linkedItems[] = {"OPTRE_UNSC_CH252_Helmet2_Vacuum_SNO", "SC_MDF_TorsoLight_White", BASE_LINKED_ITEMS};
+        respawnLinkedItems[] = {"OPTRE_UNSC_CH252_Helmet2_Vacuum_SNO", "SC_MDF_TorsoLight_White", BASE_LINKED_ITEMS};
+        backpack = "OPTRE_ILCS_Rucksack_Black";
+    };
 
     // ┌───────────────────┐
     // │     Backpacks     │
