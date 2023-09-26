4bb79831a0f1fa1fabbe1c8d2384cba3a1034c26
DartRuffian <skulldude327@gmail.com>
2023-09-26 11:33:25 -0500

    Faction and subcategory

---
+++ BNA_KC_OPFOR/Galtorran/CfgPatches.hpp
@@ -0,0 +1,23 @@
+class CfgPatches
+{
+    class BNA_KC_OPFOR_Galtorran
+    {
+        addonRootClass= "BNA_KC_OPFOR";
+        author = "SweMonkey and DartRuffian";
+        requiredVersion = 1;
+        requiredAddons[] =
+        {
+
+        };
+        units[] =
+        {
+
+        };
+        weapons[] =
+        {
+
+        };
+
+        skipWhenMissingDependencies = 1;
+    };
+};
---
+++ BNA_KC_OPFOR/Galtorran/Config.cpp
@@ -0,0 +1,56 @@
+#include "CfgPatches.hpp"
+#include "\BNA_KC_OPFOR\Macros.hpp"
+
+
+class CfgWeapons
+{
+    // ┌────────────────────┐
+    // │      Uniforms      │
+    // └────────────────────┘
+
+    // ┌───────────────────┐
+    // │       Vests       │
+    // └───────────────────┘
+};
+
+
+class CfgVehicles
+{
+    // ┌───────────────────┐
+    // │       Units       │
+    // └───────────────────┘
+
+    // ┌───────────────────┐
+    // │     Backpacks     │
+    // └───────────────────┘
+};
+
+
+class CfgFactionClasses
+{
+    class BNA_KC_OPFOR_Galtorran
+    {
+        dlc = "BNA_KC";
+        author = "SweMonkey and DartRuffian";
+
+        scope = 2;
+        scopeCurator = 2;
+
+        displayName = "[KC] Gal'torran Military";
+    };
+};
+
+
+class CfgEditorSubcategories
+{
+    class BNA_KC_SubCat_Galtorran_Infantry
+    {
+        dlc = "BNA_KC";
+        author = "SweMonkey and DartRuffian";
+
+        scope = 2;
+        scopeCurator = 2;
+
+        displayName = "Infantry";
+    };
+};
