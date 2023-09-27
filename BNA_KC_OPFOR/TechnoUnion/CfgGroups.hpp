c16389125e7ff792186ba2e7fe43a95a27a85e37
DartRuffian <skulldude327@gmail.com>
2023-09-27 12:13:14 -0500

    Simplified groups

--- BNA_KC_OPFOR/TechnoUnion/CfgGroups.hpp
+++ BNA_KC_OPFOR/TechnoUnion/CfgGroups.hpp
@@ -52,51 +52,25 @@ class CfgGroups
                     };
                 };
 
-                class AntiArmorSquad
+                class AntiArmorSquad: RifleSquad
                 {
-                    faction = "BNA_KC_OPFOR_TU";
                     name = "Anti-Armor Squad";
-                    icon = "\A3\ui_f\data\map\markers\nato\o_inf.paa";
-                    side = 0;
-
-                    class Object0
-                    {
-                        side = 0;
-                        vehicle = "BNA_KC_TU_Unit_SL";
-                        rank = "SERGEANT";
-                        position[] = {0, 0, 0};
-                        dir = 0;
-                    };
-                    class Object1: Object0
-                    {
-                        vehicle = "BNA_KC_TU_Unit_Rifleman";
-                        rank = "Private";
-                        position[] = {5, -5, 0};
-                    };
-                    class Object2: Object1
-                    {
-                        position[] = {-5, -5, 0};
-                    };
-                    class Object3: Object1
+                    class Object0: Object0 {};
+                    class Object1: Object1 {};
+                    class Object2: Object2 {};
+                    class Object3: Object3
                     {
                         vehicle = "BNA_KC_TU_Unit_AT";
-                        position[] = {10, -10, 0};
                     };
-                    class Object4: Object3
+                    class Object4: Object4
                     {
-                        position[] = {-10, -10, 0};
+                        vehicle = "BNA_KC_TU_Unit_AT";
                     };
-                    class Object5: Object1
-                    {
-                        position[] = {15, -15, 0};
-                    };
-                    class Object6: Object1
-                    {
-                        position[] = {-15, -15, 0};
-                    };
+                    class Object5: Object5 {};
+                    class Object6: Object6 {};
                 };
 
-                class AntiAirSquad: AntiArmorSquad
+                class AntiAirSquad: RifleSquad
                 {
                     name = "Anti-Air Squad";
                     class Object0: Object0 {};
@@ -114,48 +88,28 @@ class CfgGroups
                     class Object6: Object6 {};
                 };
 
-                class AssaultSquad
+                class AssaultSquad: RifleSquad
                 {
-                    faction = "BNA_KC_OPFOR_TU";
                     name = "Assault Squad";
-                    icon = "\A3\ui_f\data\map\markers\nato\o_inf.paa";
-                    side = 0;
 
-                    class Object0
-                    {
-                        side = 0;
-                        vehicle = "BNA_KC_TU_Unit_SL";
-                        rank = "SERGEANT";
-                        position[] = {0, 0, 0};
-                        dir = 0;
-                    };
-                    class Object1: Object0
-                    {
-                        vehicle = "BNA_KC_TU_Unit_Rifleman";
-                        rank = "Private";
-                        position[] = {5, -5, 0};
-                    };
-                    class Object2: Object1
-                    {
-                        position[] = {-5, -5, 0};
-                    };
-                    class Object3: Object1
+                    class Object0: Object0 {};
+                    class Object1: Object1 {};
+                    class Object2: Object2 {};
+                    class Object3: Object3
                     {
                         vehicle = "BNA_KC_TU_Unit_AssaultMedium";
-                        position[] = {10, -10, 0};
                     };
-                    class Object4: Object3
+                    class Object4: Object4
                     {
-                        position[] = {-10, -10, 0};
+                        vehicle = "BNA_KC_TU_Unit_AssaultMedium";
                     };
-                    class Object5: Object1
+                    class Object5: Object5
                     {
                         vehicle = "BNA_KC_TU_Unit_AssaultHeavy";
-                        position[] = {15, -15, 0};
                     };
-                    class Object6: Object5
+                    class Object6: Object6
                     {
-                        position[] = {-15, -15, 0};
+                        vehicle = "BNA_KC_TU_Unit_AssaultHeavy";
                     };
                 };
             };
