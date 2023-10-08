7709612542777c3baa5b3b180808043093b32650
DartRuffian <skulldude327@gmail.com>
2023-10-08 00:40:14 -0500

    Fixed parenthesis

--- BNA_KC_Vehicles/Air/LAATi/Config.cpp
+++ BNA_KC_Vehicles/Air/LAATi/Config.cpp
@@ -194,13 +194,13 @@ class CfgVehicles
             class RampOpen: Impulse
             {
                 displayName = "Ramp Open";
-                condition = QUOTE(ace_player == currentPilot this) and (this animationPhase 'ramp' == 0);
+                condition = QUOTE(ace_player == currentPilot this and this animationPhase 'ramp' == 0);
                 statement = QUOTE(this animateSource ['ramp', 1, 1]);
             };
             class RampClose: RampOpen
             {
                 displayName = "Ramp Close";
-                condition = QUOTE(ace_player == currentPilot this) and (this animationPhase 'ramp' == 1);
+                condition = QUOTE(ace_player == currentPilot this and this animationPhase 'ramp' == 1);
                 statement = QUOTE(this animateSource ['ramp', 0, 1]);
             };
 
