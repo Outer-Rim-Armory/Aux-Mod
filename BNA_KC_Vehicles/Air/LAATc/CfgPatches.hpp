a537994a2939dc49be7f6bed84a4ee83f4d0b110
DartRuffian <skulldude327@gmail.com>
2023-10-08 00:08:52 -0500

    Moved LAAT/c

---
+++ BNA_KC_Vehicles/Air/LAATc/CfgPatches.hpp
@@ -0,0 +1,23 @@
+class CfgPatches
+{
+    class BNA_KC_Vehicles_LAATc
+    {
+        addonRootClass= "BNA_KC_Vehicles";
+        author = "SweMonkey and DartRuffian";
+        requiredVersion = 0.1;
+        requiredAddons[] =
+        {
+            "lsd_vehicles_heli",
+                // LAAT/c
+            "BNA_KC_Gear_Aviation",
+                // Pilot unit
+            "ls_functions"
+                // Impulse script
+        };
+        units[] =
+        {
+            "BNA_KC_LAATc"
+        };
+        weapons[] = {};
+    };
+};
---
+++ BNA_KC_Vehicles/Air/LAATc/Config.cpp
@@ -0,0 +1,122 @@
+#include "CfgPatches.hpp"
+#include "..\..\Common\Macros.hpp"
+
+
+class CfgVehicles
+{
+    class lsd_laatc_base;
+    class lsd_heli_laatc: lsd_laatc_base
+    {
+        class ACE_SelfActions;
+        class UserActions;
+    };
+    class BNA_KC_LAATc: lsd_heli_laatc
+    {
+        // Mod Info
+        dlc = "BNA_KC";
+        author = "SweMonkey and DartRuffian";
+
+        // Scope
+        scope = 2;
+        scopeCurator = 2;
+
+        // Editor Attributes
+        faction = "BNA_KC_Faction";
+        editorSubcategory = "BNA_KC_SubCat_VAviation";
+        editorPreview = "\BNA_KC_Vehicles\Aviation\Data\Textures\Previews\BNA_KC_LAATc.jpg";
+
+        displayName = "[KC] LAAT/c";
+        crew = "BNA_KC_Unit_Phase2_Pilot";
+
+        hiddenSelectionsTextures[] =
+        {
+            "BNA_KC_Vehicles\Aviation\Data\Textures\LAATc\BNA_KC_LAATC_Auxiliary.paa",
+            "BNA_KC_Vehicles\Aviation\Data\Textures\LAATc\BNA_KC_LAATC_Cockpit.paa",
+            "lsd_vehicles_heli\laatc\data\glass_ca.paa",
+            "BNA_KC_Vehicles\Aviation\Data\Textures\LAATc\BNA_KC_LAATC_Hull.paa",
+            "BNA_KC_Vehicles\Aviation\Data\Textures\LAATc\BNA_KC_LAATC_Wings.paa"
+        };
+        textureList[] = {"Standard", 0, "KeeliCompany", 1};
+        class TextureSources
+        {
+            class Standard
+            {
+                author = "Legion Studios";
+                displayName = "Standard";
+                factions[] = {"BNA_KC_Faction"};
+                textures[] =
+                {
+                    "lsd_vehicles_heli\laatc\data\auxiliary_co.paa",
+                    "lsd_vehicles_heli\laatc\data\standard\cockpit_co.paa",
+                    "lsd_vehicles_heli\laatc\data\glass_ca.paa",
+                    "lsd_vehicles_heli\laatc\data\standard\hull_co.paa",
+                    "lsd_vehicles_heli\laatc\data\standard\wings_co.paa"
+                };
+            };
+            class KeeliCompany: Standard
+            {
+                author = "Dexus";
+                displayName = "Keeli Company";
+                textures[] =
+                {
+                    "BNA_KC_Vehicles\Aviation\Data\Textures\LAATc\BNA_KC_LAATC_Auxiliary.paa",
+                    "BNA_KC_Vehicles\Aviation\Data\Textures\LAATc\BNA_KC_LAATC_Cockpit.paa",
+                    "lsd_vehicles_heli\laatc\data\glass_ca.paa",
+                    "BNA_KC_Vehicles\Aviation\Data\Textures\LAATc\BNA_KC_LAATC_Hull.paa",
+                    "BNA_KC_Vehicles\Aviation\Data\Textures\LAATc\BNA_KC_LAATC_Wings.paa"
+                };
+            };
+        };
+
+        class UserActions: UserActions
+        {
+            class Impulse
+            {
+                displayName = "Impulse";
+                position = "pilotview";
+                radius = 5;
+
+                shortcut = "User19";
+                onlyforplayer = 0;
+                hideOnUse = 1;
+
+                condition = QUOTE(isEngineOn this and ace_player == currentPilot this and !isTouchingGround this);
+                statement = QUOTE(this call ls_vehicle_fnc_ImpulseJoystick);
+            };
+            class Repulse: Impulse
+            {
+                displayName = "Repulse";
+                shortcut = "User20";
+                statement = QUOTE(this call ls_vehicle_fnc_RepulseJoystick);
+            };
+
+            class SpecialLoadVehicle
+            {
+                displayName = "Load Vehicle (Custom)";
+                displayNameDefault = "<img size=2 image='\a3\ui_f\data\IGUI\Cfg\Actions\loadVehicle_ca.paa'>";
+
+                position = "pilotview";
+                radius = 30;
+                onlyForPlayer = 0;
+
+                hideOnUse = 1;
+                priority = 5;
+
+                condition = QUOTE(this call BNAKC_fnc_canSpecialLoad);
+                statement = QUOTE(this call BNAKC_fnc_specialLoad);
+            };
+        };
+
+        class ACE_SelfActions: ACE_SelfActions
+        {
+            HUD_CHANGER
+        };
+
+        ls_impulsor_soundOn = "BNA_KC_ImpulseOn";
+        ls_impulsor_soundOff = "BNA_KC_ImpulseOff";
+        ls_impulsor_fuelDrain_1 = 0;
+        ls_impulsor_fuelDrain_2 = 0;
+        // ls_impulsor_boostSpeed_1 = 400; // Impulse speeds, default values listed
+        // ls_impulsor_boostSpeed_2 = 600;
+    };
+};
