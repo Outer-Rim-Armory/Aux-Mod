de7ffce94c2a8dded51071d99be78b0c6d6fa7a3
DartRuffian <skulldude327@gmail.com>
2023-10-08 18:09:46 -0500

    Moved OLV-20 config

---
+++ BNA_KC_Vehicles/Armored/OLV20/CfgPatches.hpp
@@ -0,0 +1,23 @@
+class CfgPatches
+{
+    class BNA_KC_Vehicles_OLV20
+    {
+        addonRootClass= "BNA_KC_Vehicles";
+        author = "SweMonkey and DartRuffian";
+        requiredVersion = 0.1;
+        requiredAddons[] =
+        {
+            "A3_Armor_F_Beta_APC_Wheeled_02",
+                // "OLV-20" APC
+            "BNA_KC_Vehicles_Weapons",
+                // Weapons
+            "BNA_KC_Vehicles_Sounds"
+                // Sounds
+        };
+        units[] =
+        {
+            "BNA_KC_APC_OLV20"
+        };
+        weapons[] = {};
+    };
+};
