36e3a2ad2d4a2321bee13ac708b5528501f1c548
DartRuffian <skulldude327@gmail.com>
2023-10-08 17:44:17 -0500

    Moved Badger config

---
+++ BNA_KC_Vehicles/Armored/Badger/CfgPatches.hpp
@@ -0,0 +1,23 @@
+class CfgPatches
+{
+    class BNA_KC_Vehicles_Badger
+    {
+        addonRootClass= "BNA_KC_Vehicles";
+        author = "SweMonkey and DartRuffian";
+        requiredVersion = 0.1;
+        requiredAddons[] =
+        {
+            "A3_Armor_F_Beta_APC_Wheeled_01",
+                // Badger
+            "BNA_KC_Vehicles_Weapons",
+                // Weapons
+            "BNA_KC_Vehicles_Sounds"
+                // Sounds
+        };
+        units[] =
+        {
+            "BNA_KC_APC_Badger"
+        };
+        weapons[] = {};
+    };
+};
