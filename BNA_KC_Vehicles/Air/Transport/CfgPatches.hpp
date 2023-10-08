609c3ca3edd43ca529b3491e81f140291833fa91
DartRuffian <skulldude327@gmail.com>
2023-10-08 00:51:17 -0500

    Republic Transport

---
+++ BNA_KC_Vehicles/Air/Transport/CfgPatches.hpp
@@ -0,0 +1,14 @@
+class CfgPatches
+{
+    class BNA_KC_Vehicles_RepublicTransport
+    {
+        addonRootClass= "BNA_KC_Vehicles";
+        author = "SweMonkey and DartRuffian";
+        requiredVersion = 0.1;
+        requiredAddons[] =
+        {
+            "BNA_KC_RepubTransport"
+        };
+        weapons[] = {};
+    };
+};
