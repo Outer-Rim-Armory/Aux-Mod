1fa185d7a8ce0970afad09928d23a5dcb2b93cd7
DartRuffian <skulldude327@gmail.com>
2023-10-08 01:00:31 -0500

    Moved galaxy configs

---
+++ BNA_KC_Vehicles/Air/Galaxy/CfgPatches.hpp
@@ -0,0 +1,14 @@
+class CfgPatches
+{
+    class BNA_KC_Vehicles_Galaxy
+    {
+        addonRootClass= "BNA_KC_Vehicles";
+        author = "SweMonkey and DartRuffian";
+        requiredVersion = 0.1;
+        requiredAddons[] =
+        {
+
+        };
+        weapons[] = {};
+    };
+};
