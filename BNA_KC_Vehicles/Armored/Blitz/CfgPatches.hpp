7fdfc951209be3d75288b5a97c9a4708fc7aac0d
DartRuffian <skulldude327@gmail.com>
2023-10-08 10:09:50 -0500

    Moved Blitz

---
+++ BNA_KC_Vehicles/Armored/Blitz/CfgPatches.hpp
@@ -0,0 +1,19 @@
+class CfgPatches
+{
+    class BNA_KC_Vehicles_Blitz
+    {
+        addonRootClass= "BNA_KC_Vehicles";
+        author = "SweMonkey and DartRuffian";
+        requiredVersion = 0.1;
+        requiredAddons[] =
+        {
+            "A3_Armor_F_EPC_MBT_01"
+                // Slammer
+        };
+        units[] =
+        {
+            "BNA_KC_MBT_Slammer"
+        };
+        weapons[] = {};
+    };
+};
