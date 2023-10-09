6ca8fc681fd65653e492d5f88282541a4136a8aa
DartRuffian <skulldude327@gmail.com>
2023-10-09 09:08:38 -0500

    Moved Glavenus configs

---
+++ BNA_KC_Vehicles/Light/Glavenus/CfgPatches.hpp
@@ -0,0 +1,25 @@
+class CfgPatches
+{
+    class BNA_KC_Vehicles_Glavenus
+    {
+        addonRootClass= "BNA_KC_Vehicles";
+        author = "SweMonkey and DartRuffian";
+        requiredVersion = 0.1;
+        requiredAddons[] =
+        {
+            "BNA_KC_Vehicles",
+                // Core Config
+            "A3_Soft_F_MRAP_02",
+                // Karetel
+            "BNA_KC_Vehicles_Weapons",
+                // Co-Ax
+            "BNA_KC_Gear_Infantry"
+                // Unit
+        };
+        units[] =
+        {
+
+        };
+        weapons[] = {};
+    };
+};
