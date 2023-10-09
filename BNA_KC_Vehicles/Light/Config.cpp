ccdee3579f50ff75016168bdf59d8ac63dd4aa77
DartRuffian <skulldude327@gmail.com>
2023-10-09 14:05:50 -0500

    Moved event handler

--- BNA_KC_Vehicles/Light/Config.cpp
+++ BNA_KC_Vehicles/Light/Config.cpp
@@ -12,15 +12,3 @@ class CfgVehicles
 {
 
 };
-
-
-class Extended_Init_EventHandlers
-{
-    class BNA_KC_ATRT
-    {
-        class BNA_KC_ATRT_init
-        {
-            init = "(_this select 0) call BNAKC_fnc_initATRT;";
-        };
-    };
-};
