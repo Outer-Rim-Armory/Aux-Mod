e1bb50d96ca8ade6d4267f379ac98ad5bd79f70a
DartRuffian <skulldude327@gmail.com>
2023-10-19 23:05:28 -0500

    Tyrant plasma helmets

--- BNA_KC_Props/PlaceableHelmets/CfgPatches.hpp
+++ BNA_KC_Props/PlaceableHelmets/CfgPatches.hpp
@@ -154,6 +154,7 @@ class CfgPatches
             "BNA_KC_HelmetHolder_P2_Sytha",
             "BNA_KC_HelmetHolder_P2_Tugz",
             "BNA_KC_HelmetHolder_P2_Tyrant",
+            "BNA_KC_HelmetHolder_P2_Tyrant_Plasma",
             "BNA_KC_HelmetHolder_P2_Vortex",
             "BNA_KC_HelmetHolder_P2_Woods",
             // Snow
@@ -183,6 +184,7 @@ class CfgPatches
             "BNA_KC_HelmetHolder_ARC_Drake_Bacta",
             "BNA_KC_HelmetHolder_ARC_Hagrid",
             "BNA_KC_HelmetHolder_ARC_Tyrant",
+            "BNA_KC_HelmetHolder_ARC_Tyrant_Plasma",
             "BNA_KC_HelmetHolder_ARF_Patriot",
             "BNA_KC_HelmetHolder_ARF_Sin",
             "BNA_KC_HelmetHolder_BARC_Rat",
