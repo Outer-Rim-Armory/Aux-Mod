1600bc4a022c92ff3192b8e17a6a2b55a61f57dc
DartRuffian <skulldude327@gmail.com>
2023-10-20 23:23:37 -0500

    Added backslashes

--- BNA_KC_Gear/Commandos/Config.cpp
+++ BNA_KC_Gear/Commandos/Config.cpp
@@ -16,11 +16,11 @@ class CfgWeapons
 
         model = "\ls_armor_bluefor\helmet\sob\commando\lsd_sob_commando_helmet.p3d";
         hiddenSelections[] = {"illum", "camo1"};
-        hiddenSelectionsMaterials[] = {"a3\characters_f_bootcamp\common\data\vrarmoremmisive.rvmat"};
+        hiddenSelectionsMaterials[] = {"\a3\characters_f_bootcamp\common\data\vrarmoremmisive.rvmat"};
         hiddenSelectionsTextures[] =
         {
-            "ls_armor_bluefor\helmet\sob\commando\data\helmet_co.paa",
-            "ls_armor_bluefor\helmet\sob\commando\data\helmet_co.paa"
+            "\ls_armor_bluefor\helmet\sob\commando\data\helmet_co.paa",
+            "\ls_armor_bluefor\helmet\sob\commando\data\helmet_co.paa"
         };
         picture = "\SWLB_clones_spec\data\ui\icon_SWLB_clone_commando_helmet_k1_ca.paa";
 
@@ -72,12 +72,12 @@ class CfgVehicles
         editorSubcategory = "BNA_KC_Subcat_Commandos";
 
         uniformClass = "DAC_Uniform_Commando_Base";
-        model = "ls_armor_bluefor\uniform\sob\commando\ls_sob_commando_uniform.p3d";
+        model = "\ls_armor_bluefor\uniform\sob\commando\ls_sob_commando_uniform.p3d";
         hiddenSelections[] = {"camo1", "camo2"};
         hiddenSelectionsTextures[] =
         {
-            "ls_armor_bluefor\uniform\sob\commando\data\camo1_co.paa",
-            "ls_armor_bluefor\uniform\sob\commando\data\camo2_co.paa"
+            "\ls_armor_bluefor\uniform\sob\commando\data\camo1_co.paa",
+            "\ls_armor_bluefor\uniform\sob\commando\data\camo2_co.paa"
         };
 
         linkedItems[] =
