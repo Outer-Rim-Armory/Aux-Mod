3d0336264e0a6403a19360863896e0c68884d5c8
DartRuffian <skulldude327@gmail.com>
2023-10-21 14:52:54 -0500

    Added fireLight

--- BNA_KC_Weapons/DC17M/Config.cpp
+++ BNA_KC_Weapons/DC17M/Config.cpp
@@ -42,6 +42,10 @@ class CfgWeapons
         };
 
         canShootInWater = 1;
+        fireLightDiffuse[] = {0, 0, 1};
+        fireLightDuration = 0.05;
+        fireLightIntensity = 0.2;
+        flashSize = 0.5;
 
         // JLTS emp system
         JLTS_hasElectronics = 1;
