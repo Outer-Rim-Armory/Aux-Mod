c3362af71418ef67372b5213194932a0440b5b50
DartRuffian <skulldude327@gmail.com>
2023-10-23 15:41:26 -0500

    Airborne addon

---
+++ BNA_KC_Gear/Airborne/CfgPatches.hpp
@@ -0,0 +1,20 @@
+class CfgPatches
+{
+    class BNA_KC_Gear_Airborne
+    {
+        addonRootClass= "BNA_KC_Gear";
+        author = "SweMonkey and DartRuffian";
+        requiredVersion = 1;
+        requiredAddons[] =
+        {
+            "BNA_KC_Gear_Infantry"
+                // Base helmet, uniform, etc.
+        };
+        units[] =
+        {
+        };
+        weapons[] =
+        {
+        };
+    };
+};
---
+++ BNA_KC_Gear/Airborne/Config.cpp
@@ -0,0 +1,36 @@
+#include "CfgPatches.hpp"
+
+
+class CfgWeapons
+{
+    // ┌────────────────────┐
+    // │      Uniforms      │
+    // └────────────────────┘
+
+    // ┌───────────────────┐
+    // │       Vests       │
+    // └───────────────────┘
+};
+
+
+class CfgVehicles
+{
+    // ┌───────────────────┐
+    // │       Units       │
+    // └───────────────────┘
+};
+
+
+class CfgEditorSubcategories
+{
+    class BNA_KC_SubCat_Airborne
+    {
+        dlc = "BNA_KC";
+        author = "SweMonkey and DartRuffian";
+
+        scope = 2;
+        scopeCurator = 2;
+
+        displayName = "Airborne";
+    };
+};
