7ea22467d34f70e85956d526c27ac70de921749c
DartRuffian <skulldude327@gmail.com>
2023-10-24 20:05:58 -0500

    Added colors/camos

--- BNA_KC_Vehicles/Armored/AAT/Config.cpp
+++ BNA_KC_Vehicles/Armored/AAT/Config.cpp
@@ -59,6 +59,81 @@ class CfgVehicles
 
         hiddenSelectionsTextures[] = {"\3AS\3AS_AAT\data\CIS_AAT_CO.paa"};
 
+        textureList[] =
+        {
+            "CIS", 1,
+            "TradeFederation", 0,
+            "Green", 0,
+            "Red", 0,
+            "Desert", 0,
+            "Geonosis", 0,
+            "Woodland", 0,
+            "Tropical", 0,
+            "Arid", 0,
+            "Winter", 0,
+            "Aqua", 0
+        };
+        class TextureSources
+        {
+            class CIS
+            {
+                displayName = "CIS";
+                author = "$STR_3AS_Studio";
+                factions[] = {"BNA_KC_OPFOR_CIS"};
+                textures[] = {"\3AS\3AS_AAT\data\CIS_AAT_CO.paa"};
+            };
+            class TradeFederation: CIS
+            {
+                displayName = "Trade Federation";
+                textures[] = {"\3AS\3AS_AAT\data\Tan_AAT_CO.paa"};
+            };
+            class Green: CIS
+            {
+                displayName = "Green";
+                textures[] = {"\3AS\3AS_AAT\data\Green_AAT_CO.paa"};
+            };
+            class Red: CIS
+            {
+                displayName = "Red";
+                textures[] = {"\3AS\3AS_AAT\data\Red_AAT_CO.paa"};
+            };
+            class Desert
+            {
+                displayName = "Camo - Desert";
+                textures[] = {"\3AS\3AS_AAT\data\Desert_AAT_CO.paa"};
+            };
+            class Geonosis
+            {
+                displayName = "Camo - Geonosis";
+                textures[] = {"\3AS\3AS_AAT\data\Geonosis_AAT_CO.paa"};
+            };
+            class Woodland
+            {
+                displayName = "Camo - Woodland";
+                textures[] = {"\3AS\3AS_AAT\data\Woodland_AAT_CO.paa"};
+            };
+            class Tropical
+            {
+                displayName = "Camo - Tropical";
+                textures[] = {"\3AS\3AS_AAT\data\Tropical_AAT_CO.paa"};
+            };
+            class Arid
+            {
+                displayName = "Camo - Arid";
+                textures[] = {"\3AS\3AS_AAT\data\Arid_AAT_CO.paa"};
+            };
+            class Winter
+            {
+                displayName = "Camo - Winter";
+                textures[] = {"\3AS\3AS_AAT\data\Winter_AAT_CO.paa"};
+            };
+            class Aqua
+            {
+                displayName = "Camo - Aqua";
+                textures[] = {"\3AS\3AS_AAT\data\Aqua_AAT_CO.paa"};
+            };
+        };
+
         class Turrets: Turrets
         {
             class MainTurret: MainTurret
