e945b1210ca1e33e09f17d1652143aead20f51ad
DartRuffian <skulldude327@gmail.com>
2023-10-27 10:48:46 -0500

    Swapped default textures

--- BNA_KC_Vehicles/Armored/Plesioth/Config.cpp
+++ BNA_KC_Vehicles/Armored/Plesioth/Config.cpp
@@ -42,36 +42,21 @@ class CfgVehicles
 
         hiddenSelectionsTextures[] =
         {
-            "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\scorp_lopo_co.paa",
-            "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\scorp_tur_wood_co.paa",
-            "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\det_3_co.paa",
+            "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\camo\Scorp_base_desert_co.paa",
+            "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\camo\Scorp_tur_desert_co.paa",
+            "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\camo\det_3_desert_co.paa",
             "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\scorp_decals_ca.paa",
-            "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\scorp_net_wood_ca.paa",
-            "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\scorp_net_wood_ca.paa",
-            "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\mine_roller_co.paa"
+            "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\camo\Scorp_net_desert_ca.paa",
+            "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\camo\Scorp_net_desert_ca.paa",
+            "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\camo\mine_roller_desert_co.paa"
         };
 
-        textureList[] = {"EPF", 1, "TechnoUnion", 0};
+        textureList[] = {"TechnoUnion", 1, "EPF", 0};
         class TextureSources
         {
-            class EPF
+            class TechnoUnion
             {
                 author = "Article 2 Studios";
-                displayName = "El President Forces";
-                factions[] = {"BNA_KC_OPFOR_EPF"};
-                textures[] =
-                {
-                    "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\scorp_lopo_co.paa",
-                    "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\scorp_tur_wood_co.paa",
-                    "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\det_3_co.paa",
-                    "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\scorp_decals_ca.paa",
-                    "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\scorp_net_wood_ca.paa",
-                    "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\scorp_net_wood_ca.paa",
-                    "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\mine_roller_co.paa"
-                };
-            };
-            class TechnoUnion: EPF
-            {
                 displayName = "Techno Union";
                 factions[] = {"BNA_KC_OPFOR_TU"};
                 textures[] =
@@ -83,6 +68,21 @@ class CfgVehicles
                     "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\camo\Scorp_net_desert_ca.paa",
                     "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\camo\Scorp_net_desert_ca.paa",
                     "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\camo\mine_roller_desert_co.paa"
+                };
+            };
+            class EPF: TechnoUnion
+            {
+                displayName = "El President Forces";
+                factions[] = {"BNA_KC_OPFOR_EPF"};
+                textures[] =
+                {
+                    "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\scorp_lopo_co.paa",
+                    "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\scorp_tur_wood_co.paa",
+                    "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\det_3_co.paa",
+                    "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\scorp_decals_ca.paa",
+                    "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\scorp_net_wood_ca.paa",
+                    "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\scorp_net_wood_ca.paa",
+                    "\OPTRE_Vehicles_Tracked\Scorpion\data\texture\mine_roller_co.paa"
                 };
             };
         };
