25a4bed0dae51d560d8e48968c1ab83824e3ca55
DartRuffian <skulldude327@gmail.com>
2023-10-26 20:37:46 -0500

    Base Configs

---
+++ BNA_KC_OPFOR/EPF/CfgPatches.hpp
@@ -0,0 +1,32 @@
+class CfgPatches
+{
+    class BNA_KC_OPFOR_EPF
+    {
+        addonRootClass= "BNA_KC_OPFOR";
+        author = "SweMonkey and DartRuffian";
+        requiredVersion = 1;
+        requiredAddons[] =
+        {
+            "BNA_KC_OPFOR",
+                // Base OPFOR unit, uniform, etc.
+            "sc_equipment",
+                // Uniform textures
+                // Vests
+                // Backpacks
+            "BNA_KC_Weapons_E5",
+                // E-5
+            "BNA_KC_Weapons_E5C",
+                // E-5C
+            "BNA_KC_Weapons_E60R"
+                // E-60R
+        };
+        units[] =
+        {
+        };
+        weapons[] =
+        {
+        };
+
+        skipWhenMissingDependencies = 1;
+    };
+};
---
+++ BNA_KC_OPFOR/EPF/Config.cpp
@@ -0,0 +1,13 @@
+#include "CfgPatches.hpp"
+#include "CfgGroups.hpp"
+#include "\BNA_KC_OPFOR\Macros.hpp"
+
+
+class CfgWeapons
+{
+};
+
+
+class CfgVehicles
+{
+};
