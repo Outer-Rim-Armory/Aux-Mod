a6c8f8899a79ab765a13842ad5e2bb4c95bc1e23
DartRuffian <skulldude327@gmail.com>
2023-10-29 17:18:19 -0500

    Base Scout Tank

---
+++ BNA_KC_Vehicles/Light/AST/Config.cpp
@@ -0,0 +1,20 @@
+#include "CfgPatches.hpp"
+#include "..\..\Common\Macros.hpp"
+
+
+class CfgVehicles
+{
+    class lsd_car_ast;
+    class BNA_KC_AST_Base: lsd_car_ast
+    {
+        // Mod Info
+        dlc = "BNA_KC";
+        author = "SweMonkey and DartRuffian";
+
+        // Scope
+        scope = 2;
+        scopeCurator = 2;
+
+        displayName = "Armored Scout Tank";
+    };
+};
