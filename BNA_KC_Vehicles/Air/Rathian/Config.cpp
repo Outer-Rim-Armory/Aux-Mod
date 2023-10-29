0324eb75da2efe39e71d368172e929a51407978e
DartRuffian <skulldude327@gmail.com>
2023-10-29 17:55:01 -0500

    Texture Sources

--- BNA_KC_Vehicles/Air/Rathian/Config.cpp
+++ BNA_KC_Vehicles/Air/Rathian/Config.cpp
@@ -20,5 +20,25 @@ class CfgVehicles
         // editorPreview = "";
 
         displayName = "Rathian";
+
+        hiddenSelectionsTextures[] = {"\OPTRE_Vehicles_Air\hornet\data\hornet_hull_desert_co.paa"};
+
+        textureList[] = {"TechnoUnion", 1, "EPF", 0};
+        class TextureSources
+        {
+            class TechnoUnion
+            {
+                author = "Article 2 Studios";
+                displayName = "Techno Union";
+                factions[] = {"BNA_KC_OPFOR_TU"};
+                textures[] = {"\OPTRE_Vehicles_Air\hornet\data\hornet_hull_desert_co.paa"};
+            };
+            class EPF: TechnoUnion
+            {
+                displayName = "El President Forces";
+                factions[] = {"BNA_KC_OPFOR_EPF"};
+                textures[] = {"\OPTRE_Vehicles_Air\hornet\data\hornet_hull_standard_co.paa"};
+            };
+        };
     };
 };
