8d15478651f222a8bcb3f1cdbbbe4633b1f7890c
DartRuffian <skulldude327@gmail.com>
2023-10-31 22:41:44 -0500

    Moved comment

--- BNA_KC_Core/Config.cpp
+++ BNA_KC_Core/Config.cpp
@@ -1,3 +1,18 @@
+/*
+This is the root config for the mod.
+
+Scope Values and their Meaning:
+scope =
+    0: Class is unavailable (and used for inheritance only)
+    1: Class is unavailable in the editor (can be accessed with a macro)
+    2: Class is available in the editor
+scopeCurator =
+    0: Class is unavailable in Zeus.
+    2: Class is available in Zeus.
+scopeArsenal =
+    0: Class is unavailable in the Arsenal.
+    2: Class is available in the Arsenal.
+*/
 #include "CfgPatches.hpp"
 
 
--- BNA_KC_Gear/Config.cpp
+++ BNA_KC_Gear/Config.cpp
@@ -1,23 +1,3 @@
-/*
-This is the root config for the mod.
-Mod contents are split into "sub-addons" based on detachment.
-Infantry, Armored, Aviation, etc.
-
-
-Scope Values and their Meaning:
-scope =
-    0: Class is unavailable (and used for inheritance only)
-    1: Class is unavailable in the editor (can be accessed with a macro)
-    2: Class is available in the editor
-scopeCurator =
-    0: Class is unavailable in Zeus.
-    2: Class is available in Zeus.
-scopeArsenal =
-    0: Class is unavailable in the Arsenal.
-    2: Class is available in the Arsenal.
-*/
-
-
 class CfgPatches
 {
     class BNA_KC_Gear
