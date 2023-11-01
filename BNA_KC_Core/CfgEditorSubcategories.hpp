95b0308ac4b06d4949e15ae36bedc5768237819f
DartRuffian <skulldude327@gmail.com>
2023-11-01 00:26:34 -0500

    Fixed subcategories not being in root config

--- BNA_KC_Core/CfgEditorSubcategories.hpp
+++ BNA_KC_Core/CfgEditorSubcategories.hpp
@@ -1,45 +1,48 @@
-// Units
-class BNA_KC_SubCat_Infantry
+class CfgEditorSubcategories
 {
-    dlc = "BNA_KC";
-    author = "SweMonkey and DartRuffian";
-    scope = 2;
-    scopeCurator = 2;
-    displayName = "Infantry";
-};
-class BNA_KC_SubCat_SpecialForces: BNA_KC_SubCat_Infantry
-{
-    displayName = "Special Forces";
-};
+    // Units
+    class BNA_KC_SubCat_Infantry
+    {
+        dlc = "BNA_KC";
+        author = "SweMonkey and DartRuffian";
+        scope = 2;
+        scopeCurator = 2;
+        displayName = "Infantry";
+    };
+    class BNA_KC_SubCat_SpecialForces: BNA_KC_SubCat_Infantry
+    {
+        displayName = "Special Forces";
+    };
 
-// Ground Vehicles
-class BNA_KC_SubCat_Tanks: BNA_KC_SubCat_Infantry
-{
-    displayName = "Tanks";
-};
-class BNA_KC_SubCat_Cars: BNA_KC_SubCat_Infantry
-{
-    displayName = "Cars";
-};
-class BNA_KC_SubCat_Speeders: BNA_KC_SubCat_Infantry
-{
-    displayName = "Speeders";
-};
-class BNA_KC_SubCat_Drones: BNA_KC_SubCat_Infantry
-{
-    displayName = "Drones";
-};
-class BNA_KC_SubCat_Turrets: BNA_KC_SubCat_Infantry
-{
-    displayName = "Turrets";
-};
+    // Ground Vehicles
+    class BNA_KC_SubCat_Tanks: BNA_KC_SubCat_Infantry
+    {
+        displayName = "Tanks";
+    };
+    class BNA_KC_SubCat_Cars: BNA_KC_SubCat_Infantry
+    {
+        displayName = "Cars";
+    };
+    class BNA_KC_SubCat_Speeders: BNA_KC_SubCat_Infantry
+    {
+        displayName = "Speeders";
+    };
+    class BNA_KC_SubCat_Drones: BNA_KC_SubCat_Infantry
+    {
+        displayName = "Drones";
+    };
+    class BNA_KC_SubCat_Turrets: BNA_KC_SubCat_Infantry
+    {
+        displayName = "Turrets";
+    };
 
-// Air Vehicles
-class BNA_KC_SubCat_Planes: BNA_KC_SubCat_Infantry
-{
-    displayName = "Planes";
-};
-class BNA_KC_SubCat_Helicopters: BNA_KC_SubCat_Infantry
-{
-    displayName = "Helicopters";
+    // Air Vehicles
+    class BNA_KC_SubCat_Planes: BNA_KC_SubCat_Infantry
+    {
+        displayName = "Planes";
+    };
+    class BNA_KC_SubCat_Helicopters: BNA_KC_SubCat_Infantry
+    {
+        displayName = "Helicopters";
+    };
 };
