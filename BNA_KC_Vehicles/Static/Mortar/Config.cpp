3a4df7cf2740498945a9bf263cc1f8ab3b5dd34d
DartRuffian <skulldude327@gmail.com>
2023-11-01 01:17:32 -0500

    Removed displayName prefix

--- BNA_KC_Vehicles/Static/Mortar/Config.cpp
+++ BNA_KC_Vehicles/Static/Mortar/Config.cpp
@@ -21,7 +21,7 @@ class CfgWeapons
         scope = 2;
         scopeArsenal = 2;
 
-        displayName = "[KC] Model 190 Mortar System";
+        displayName = "Model 190 Mortar System";
         picture = "\ls_weapons\_ui\rps6_ca.paa"; // Temp picture
         drySound[] = {};
 
