8590950ac3f6f0d36d04a9f15993228001376413
DartRuffian <skulldude327@gmail.com>
2023-10-31 22:20:22 -0500

    Moved logo

--- BNA_KC_Core/Config.cpp
+++ BNA_KC_Core/Config.cpp
@@ -6,9 +6,9 @@ class CfgMods
     class Mod_Base;
     class BNA_KC: Mod_Base
     {
-        logo = "\BNA_KC_Core\KC_Logo.paa";
-        logoOver = "\BNA_KC_Core\KC_Logo.paa";
-        picture = "\BNA_KC_Core\KC_Logo.paa";
+        logo = "\BNA_KC_Core\Data\UI\KC_Logo.paa";
+        logoOver = "\BNA_KC_Core\Data\UI\KC_Logo.paa";
+        picture = "\BNA_KC_Core\Data\UI\KC_Logo.paa";
         name = "Keeli Company Aux Mod";
         dlcColor[] = {0, 0, 0, 1};
     };
