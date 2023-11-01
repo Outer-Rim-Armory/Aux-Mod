ea47e030efe5ea63095b1ebbf70ec2a3e8cdbb7d
DartRuffian <skulldude327@gmail.com>
2023-10-31 22:19:50 -0500

    Inventory macros

---
+++ BNA_KC_Core/Macros.hpp
@@ -0,0 +1,32 @@
+// Inventory Macros
+// Inventory
+#define ITEM_2(a) a, a
+#define ITEM_3(a) a, a, a
+#define ITEM_4(a) a, a, a, a
+#define ITEM_5(a) a, a, a, a, a
+#define ITEM_6(a) a, a, a, a, a, a
+#define ITEM_7(a) a, a, a, a, a, a, a
+#define ITEM_8(a) a, a, a, a, a, a, a, a
+#define ITEM_9(a) a, a, a, a, a, a, a, a, a
+#define ITEM_10(a) a, a, a, a, a, a, a, a, a, a
+#define ITEM_11(a) a, a, a, a, a, a, a, a, a, a, a
+#define ITEM_12(a) a, a, a, a, a, a, a, a, a, a, a, a
+
+// Transport
+#define WEAP_XX(WEAP, COUNT) class _xx_##WEAP \
+{ \
+    weapon = #WEAP; \
+    count = COUNT; \
+};
+
+#define MAG_XX(MAG, COUNT) class _xx_##MAG \
+{ \
+    magazine = #MAG; \
+    count = COUNT; \
+};
+
+#define ITEM_XX(ITEM, COUNT) class _xx_##ITEM \
+{ \
+    name = #ITEM; \
+    count = COUNT; \
+};
