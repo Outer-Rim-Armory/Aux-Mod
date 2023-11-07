db05cd6ce54d9d6f907a0d4a9515d2319ea5b570
DartRuffian <skulldude327@gmail.com>
2023-11-06 18:15:54 -0600

    Removed double "class"

--- BNA_KC_ZeusAudioModule/config.cpp
+++ BNA_KC_ZeusAudioModule/config.cpp
@@ -230,21 +230,21 @@ class CfgSFX
         BNA_KC_Ambient_Let_There_Be_Carnage[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\Let_There_Be_Carnage.ogg",db+50,1,500,1,2,3,4};
         empty[] = {"",0,0,0,0,0,0,0};
     };
-	class class BNA_KC_Ambient_EPF_Radio_1
+	class BNA_KC_Ambient_EPF_Radio_1
 	{
         name = "[KC] Holonet News";
         sounds[] = {BNA_KC_Ambient_EPF_Radio_1_SFX};
         BNA_KC_Ambient_EPF_Radio_1[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\EPF_Radio_1.ogg",db+80,1,100,1,2,3,4};
         empty[] = {"",0,0,0,0,0,0,0};
     };
-	class class BNA_KC_Ambient_EPF_Radio_2
+	class BNA_KC_Ambient_EPF_Radio_2
 	{
         name = "[KC] Holonet News 2";
         sounds[] = {BNA_KC_Ambient_EPF_Radio_2_SFX};
         BNA_KC_Ambient_EPF_Radio_2[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\EPF_Radio_2.ogg",db+80,1,100,1,2,3,4};
         empty[] = {"",0,0,0,0,0,0,0};
     };
-	class class BNA_KC_Ambient_EPF_Radio_3
+	class BNA_KC_Ambient_EPF_Radio_3
 	{
         name = "[KC] Holonet News 3";
         sounds[] = {BNA_KC_Ambient_EPF_Radio_3_SFX};
