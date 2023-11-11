3e181c6b92918abc3a8939e39395d8e4733fadc3
DartRuffian <skulldude327@gmail.com>
2023-11-11 12:15:32 -0600

    Removed displayName tags from placeable helmets

--- BNA_KC_Props/PlaceableHelmets/Configs/AVI_Helmets.hpp
+++ BNA_KC_Props/PlaceableHelmets/Configs/AVI_Helmets.hpp
@@ -1,3 +1,3 @@
-HELMET_HOLDER(BNA_KC_HelmetHolder_P1_Pilot, [KC] AVI P1 Helm (Pilot), BNA_KC_Helmet_Phase1_Pilot, "P1")
+HELMET_HOLDER(BNA_KC_HelmetHolder_P1_Pilot, AVI P1 Helm (Pilot), BNA_KC_Helmet_Phase1_Pilot, "P1")
 
-HELMET_HOLDER(BNA_KC_HelmetHolder_P2_Pilot, [KC] AVI P2 Helm (Pilot), BNA_KC_Helmet_Phase2_Pilot, "P2")
+HELMET_HOLDER(BNA_KC_HelmetHolder_P2_Pilot, AVI P2 Helm (Pilot), BNA_KC_Helmet_Phase2_Pilot, "P2")
