4d00428cfc596c879cc164b461096ee628e13f5a
DartRuffian <skulldude327@gmail.com>
2023-12-05 01:06:04 -0600

    Updated authors

--- mod.cpp
+++ mod.cpp
@@ -1,6 +1,6 @@
-author = "SweMonkey, DartRuffian, and Dexus";
+author = "Keeli Company Aux Team";
 name = "Keeli Company Aux Mod";
-picture = "kc_logo.paa"; 
+picture = "kc_logo.paa";
 logo = "kc_logo.paa";
 overview = "";
 version = 1.0;
