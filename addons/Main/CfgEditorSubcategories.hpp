494d66625344d953bd3e2beec5fdca46ffa822c0
DartRuffian <skulldude327@gmail.com>
2023-12-07 11:23:54 -0600

    Subcategories

---
+++ addons/Main/CfgEditorSubcategories.hpp
@@ -0,0 +1,60 @@
+class CfgEditorSubcategories
+{
+    // Units
+    class BNA_KC_SubCat_Infantry
+    {
+        displayName = "Infantry";
+    };
+    class BNA_KC_SubCat_Crewmen
+    {
+        displayName = "Crewmen";
+    };
+    class BNA_KC_SubCat_SpecialForces
+    {
+        displayName = "Special Forces";
+    };
+    class BNA_KC_SubCat_Special
+    {
+        displayName = "Special"; // Used for things like faction officers
+    };
+
+    // Ground Vehicles
+    class BNA_KC_SubCat_Tanks
+    {
+        displayName = "Tanks";
+    };
+    class BNA_KC_SubCat_APCs
+    {
+        displayName = "APCs";
+    };
+    class BNA_KC_SubCat_Walkers
+    {
+        displayName = "Walkers";
+    };
+    class BNA_KC_SubCat_Cars
+    {
+        displayName = "Cars";
+    };
+    class BNA_KC_SubCat_Speeders
+    {
+        displayName = "Speeders";
+    };
+    class BNA_KC_SubCat_Drones
+    {
+        displayName = "Drones";
+    };
+    class BNA_KC_SubCat_Turrets
+    {
+        displayName = "Turrets";
+    };
+
+    // Air Vehicles
+    class BNA_KC_SubCat_Planes
+    {
+        displayName = "Planes";
+    };
+    class BNA_KC_SubCat_Helicopters
+    {
+        displayName = "Helicopters";
+    };
+};
