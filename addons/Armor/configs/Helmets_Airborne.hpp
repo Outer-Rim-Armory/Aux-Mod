b29d3388d6165009d8befe1d41b814a496ff4075
DartRuffian <skulldude327@gmail.com>
2023-12-08 12:26:34 -0600

    Added base (plain) airborne helmet

--- addons/Armor/configs/Helmets_Airborne.hpp
+++ addons/Armor/configs/Helmets_Airborne.hpp
@@ -1,8 +1,8 @@
-class BNA_KC_Helmet_Airborne_CT: BNA_KC_Helmet_Base
+class BNA_KC_Helmet_Airborne_Base: BNA_KC_Helmet_Base
 {
     SCOPE_PUBLIC;
 
-    displayName = "[KC] AB Helm 01 (Trooper)";
+    displayName = "[KC] AB Helm (Base)";
 
     model = "\lsd_armor_bluefor\helmet\gar\airborne\lsd_gar_airborne_helmet.p3d";
     hiddenSelections[] = {"camo1", "visor"};
@@ -13,17 +13,26 @@ class BNA_KC_Helmet_Airborne_CT: BNA_KC_Helmet_Base
     };
     hiddenSelectionsTextures[] =
     {
-        QPATHTOF(data\helmets\airborne\CT_camo1_co.paa)
+        "\lsd_armor_bluefor\helmet\gar\airborne\data\helmet_co.paa",
+        "\lsd_armor_bluefor\helmet\gar\airborne\data\visor_co.paa"
     };
     picture = "\SWLB_clones\data\ui\icon_SWLB_clone_ab_helmet_ca.paa";
+
+    subItems[] = {"BNA_KC_NVG_Chip"};
 
     class ItemInfo: ItemInfo
     {
         hiddenSelections[] = {"camo1", "visor"};
         uniformModel = "\lsd_armor_bluefor\helmet\gar\airborne\lsd_gar_airborne_helmet.p3d";
     };
-
-    subItems[] = {"BNA_KC_NVG_Chip"};
+};
+class BNA_KC_Helmet_Airborne_CT: BNA_KC_Helmet_Airborne_Base
+{
+    displayName = "[KC] AB Helm 01 (Trooper)";
+    hiddenSelectionsTextures[] =
+    {
+        QPATHTOF(data\helmets\airborne\CT_camo1_co.paa)
+    };
 };
 
 class BNA_KC_Helmet_Airborne_CT_v2: BNA_KC_Helmet_Airborne_CT
