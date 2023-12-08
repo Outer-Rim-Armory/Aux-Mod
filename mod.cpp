0b2a37b69cb73eb7f05d190b92d74f7121f947df
DartRuffian <skulldude327@gmail.com>
2023-12-08 16:09:21 -0600

    Updated icon file name

--- mod.cpp
+++ mod.cpp
@@ -1,6 +1,7 @@
 author = "Keeli Company Aux Team";
 name = "Keeli Company Aux Mod";
-picture = "kc_logo.paa";
-logo = "kc_logo.paa";
+picture = "logo_kc_ca.paa";
+logo = "logo_kc_ca.paa";
+logoOver = "logo_kc_ca.paa";
 overview = "";
 version = 1.0;
