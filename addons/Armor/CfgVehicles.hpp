6b1747b2fd265016249f05cd0879893746fabe49
DartRuffian <skulldude327@gmail.com>
2023-12-09 22:00:07 -0600

    Organized includes

--- addons/Armor/CfgVehicles.hpp
+++ addons/Armor/CfgVehicles.hpp
@@ -98,10 +98,10 @@ class CfgVehicles
     };
 
     #include "configs/Units_P1.hpp"
-    #include "configs/Units_P2.hpp"
     #include "configs/Units_P1_Pilot.hpp"
-    #include "configs/Units_P2_Pilot.hpp"
     #include "configs/Units_P1_Tanker.hpp"
+    #include "configs/Units_P2.hpp"
+    #include "configs/Units_P2_Pilot.hpp"
     #include "configs/Units_P2_Tanker.hpp"
     #include "configs/Units_ARC.hpp"
     #include "configs/Units_ARF.hpp"
--- addons/Armor/CfgWeapons.hpp
+++ addons/Armor/CfgWeapons.hpp
@@ -13,17 +13,17 @@ class CfgWeapons
     };
 
     #include "configs/Helmets_P1.hpp"
-    #include "configs/Helmets_P2.hpp"
-    #include "configs/Helmets_ARC.hpp"
-    #include "configs/Helmets_ARF.hpp"
-    #include "configs/Helmets_BARC.hpp"
     #include "configs/Helmets_P1_Pilot.hpp"
-    #include "configs/Helmets_P2_Pilot.hpp"
     #include "configs/Helmets_P1_SpecOp.hpp"
+    #include "configs/Helmets_P2.hpp"
+    #include "configs/Helmets_P2_Pilot.hpp"
     #include "configs/Helmets_P2_SpecOp.hpp"
     #include "configs/Helmets_Airborne.hpp"
     #include "configs/Helmets_Engineer.hpp"
     #include "configs/Helmets_Insulated.hpp"
+    #include "configs/Helmets_ARC.hpp"
+    #include "configs/Helmets_ARF.hpp"
+    #include "configs/Helmets_BARC.hpp"
 
     class ls_blueforUniform_base;
     class ls_gar_phase2_uniform: ls_blueforUniform_base
