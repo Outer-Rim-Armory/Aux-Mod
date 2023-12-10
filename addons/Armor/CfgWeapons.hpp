a6ec4c358655e159af48d624eab98222754e7db2
DartRuffian <skulldude327@gmail.com>
2023-12-09 21:53:41 -0600

    Fixed missing includes

--- addons/Armor/CfgWeapons.hpp
+++ addons/Armor/CfgWeapons.hpp
@@ -15,6 +15,8 @@ class CfgWeapons
     #include "configs/Helmets_P1.hpp"
     #include "configs/Helmets_P2.hpp"
     #include "configs/Helmets_ARC.hpp"
+    #include "configs/Helmets_ARF.hpp"
+    #include "configs/Helmets_BARC.hpp"
     #include "configs/Helmets_P1_Pilot.hpp"
     #include "configs/Helmets_P2_Pilot.hpp"
     #include "configs/Helmets_P1_SpecOp.hpp"
