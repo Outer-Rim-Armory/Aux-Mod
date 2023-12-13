dbea5f1f38528fbd0be7ade36d4d85c01170644b
DartRuffian <skulldude327@gmail.com>
2023-12-13 11:30:16 -0600

    Z-6 uses MG ammo

--- BNA_KC_Weapons/Z6/CfgMagazines.hpp
+++ BNA_KC_Weapons/Z6/CfgMagazines.hpp
@@ -7,7 +7,7 @@ class CfgMagazines
         displayName = "[12th Fleet] Z-6 Energy Cell";
         displayNameShort = "Standard Energy";
         descriptionShort = "Energy Cell Pack<br/>Rounds: 300<br/>Used In: Z-6";
-        ammo = "Aux12thFleet_Ammo_Carbine_Blue";
+        ammo = "Aux12thFleet_Ammo_MachineGun_Blue";
 
         JLTS_hasEMPProtection = 1;
     };
