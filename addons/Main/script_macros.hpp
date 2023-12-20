74df1466450645ebb2c43b5d8152bf8384e200c8
DartRuffian <skulldude327@gmail.com>
2023-12-20 13:50:32 -0600

    CLASS and QCLASS macros

--- addons/Main/script_macros.hpp
+++ addons/Main/script_macros.hpp
@@ -10,6 +10,9 @@
     #define PREP(fncName) [QPATHTOF(functions\DOUBLES(fnc,fncName).sqf), QFUNC(fncName)] call CBA_fnc_compileFunction
 #endif
 
+#define CLASS(var1) DOUBLES(PREFIX,var1)
+#define QCLASS(var1) QUOTE(CLASS(var1))
+
 #define GETVAR_SYS(var1,var2) getVariable [ARR_2(QUOTE(var1),var2)]
 #define SETVAR_SYS(var1,var2) setVariable [ARR_2(QUOTE(var1),var2)]
 #define SETPVAR_SYS(var1,var2) setVariable [ARR_3(QUOTE(var1),var2,true)]
