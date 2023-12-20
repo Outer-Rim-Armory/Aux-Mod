3b28a7323c6c093cd9c5904cc1fe9452addc220e
DartRuffian <skulldude327@gmail.com>
2023-12-20 13:50:14 -0600

    Get/Setvar macros

--- addons/Main/script_macros.hpp
+++ addons/Main/script_macros.hpp
@@ -10,6 +10,28 @@
     #define PREP(fncName) [QPATHTOF(functions\DOUBLES(fnc,fncName).sqf), QFUNC(fncName)] call CBA_fnc_compileFunction
 #endif
 
+#define GETVAR_SYS(var1,var2) getVariable [ARR_2(QUOTE(var1),var2)]
+#define SETVAR_SYS(var1,var2) setVariable [ARR_2(QUOTE(var1),var2)]
+#define SETPVAR_SYS(var1,var2) setVariable [ARR_3(QUOTE(var1),var2,true)]
+
+#undef GETVAR
+#define GETVAR(var1,var2,var3) (var1 GETVAR_SYS(var2,var3))
+#define GETMVAR(var1,var2) (missionNamespace GETVAR_SYS(var1,var2))
+#define GETUVAR(var1,var2) (uiNamespace GETVAR_SYS(var1,var2))
+#define GETPRVAR(var1,var2) (profileNamespace GETVAR_SYS(var1,var2))
+#define GETPAVAR(var1,var2) (parsingNamespace GETVAR_SYS(var1,var2))
+
+#undef SETVAR
+#define SETVAR(var1,var2,var3) var1 SETVAR_SYS(var2,var3)
+#define SETPVAR(var1,var2,var3) var1 SETPVAR_SYS(var2,var3)
+#define SETMVAR(var1,var2) missionNamespace SETVAR_SYS(var1,var2)
+#define SETUVAR(var1,var2) uiNamespace SETVAR_SYS(var1,var2)
+#define SETPRVAR(var1,var2) profileNamespace SETVAR_SYS(var1,var2)
+#define SETPAVAR(var1,var2) parsingNamespace SETVAR_SYS(var1,var2)
+
+#define GETGVAR(var1,var2) GETMVAR(GVAR(var1),var2)
+#define GETEGVAR(var1,var2,var3) GETMVAR(EGVAR(var1,var2),var3)
+
 #define EDITOR_PREVIEW(CLASS) QPATHTOF(data\previews\CLASS.jpg)
 #define EEDITOR_PREVIEW(COMPONENT,CLASS) QUOTE(PATHTOF_SYS(PREFIX,COMPONENT,data\previews\CLASS.jpg))
 
