c86b2c28786569a4e821bd3d3094941ff2a14449
DartRuffian <skulldude327@gmail.com>
2023-12-21 18:34:07 -0600

    Customs -> CustomArmor

--- addons/CustomArmor/script_component.hpp
+++ addons/CustomArmor/script_component.hpp
@@ -1,4 +1,5 @@
-#define COMPONENT Customs
+#define COMPONENT CustomArmor
+#define COMPONENT_BEAUTIFIED Custom Armor
 
 #include "\BNA\BNA_KC\addons\Core\script_mod.hpp"
 #include "\BNA\BNA_KC\addons\Core\script_macros.hpp"
