90ab7bc335ae1a24d5f4c46c3330a41b6d738cd9
DartRuffian <skulldude327@gmail.com>
2023-12-22 15:53:38 -0600

    Fixed Rat helmet

--- addons/CustomArmor/configs/Helmets_BARC.hpp
+++ addons/CustomArmor/configs/Helmets_BARC.hpp
@@ -4,7 +4,7 @@ class CLASS(Helmet_BARC_Rat): CLASS(Helmet_BARC_Base)
     displayName = "[KC] SF BARC Helm ('Rat')";
     hiddenSelectionsTextures[] =
     {
-        "\BNA_KC_Gear\Customs\Data\Textures\Helmets\SpecialForces\BARC\Helmet_BARC_Rat.paa",
+        QPATHTOF(data\helmets\barc\Rat_camo1_co.paa),
         "\ls_armor_bluefor\helmet\gar\barc\data\visor_co.paa"
     };
 
