fad9d32ed9f0aacd782b02071aa02a812c4cf6a3
DartRuffian <skulldude327@gmail.com>
2023-12-23 16:10:51 -0600

    Fixed name

--- addons/weapons/dc15a/CfgSoundSets.hpp
+++ addons/weapons/dc15a/CfgSoundSets.hpp
@@ -3,6 +3,6 @@ class CfgSoundSets
     class CLASS(SoundSet_WeaponShot_Base);
     class CLASS(SoundSet_DC15AShot): CLASS(SoundSet_WeaponShot_Base)
     {
-        soundShaders[] = {QCLASS(SoundShader_DC15A)};
+        soundShaders[] = {QCLASS(SoundShader_DC15AShot)};
     };
 };
