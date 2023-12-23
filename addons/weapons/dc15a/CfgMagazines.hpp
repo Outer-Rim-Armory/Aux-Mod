12c42bffd2e64d20a1c74aec61040311b3a0f33e
DartRuffian <skulldude327@gmail.com>
2023-12-23 14:50:59 -0600

    Fixed model/picture for 15A magazine

--- addons/weapons/dc15a/CfgMagazines.hpp
+++ addons/weapons/dc15a/CfgMagazines.hpp
@@ -8,6 +8,9 @@ class CfgMagazines
         displayNameShort = "Standard Energy";
         descriptionShort = "Energy Cell Pack<br/>Rounds: 60<br/>Used In: DC-15A";
 
+        model = "\MRC\JLTS\weapons\DC15A\DC15A_mag.p3d";
+        picture = "\MRC\JLTS\weapons\DC15A\data\ui\DC15A_mag_ui_ca.paa";
+
         ammo = QCLASS(Bullet_PlasmaRifle_Blue);
         count = 80;
     };
