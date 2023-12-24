670b92e378941fff91d93f22806ad31124168a24
DartRuffian <skulldude327@gmail.com>
2023-12-24 12:06:01 -0600

    Sound Shader/Set

--- addons/weapons/dc15l/CfgSoundSets.hpp
+++ addons/weapons/dc15l/CfgSoundSets.hpp
@@ -1,8 +1,8 @@
 class CfgSoundSets
 {
     class CLASS(SoundSet_WeaponShot_Base);
-    class CLASS(SoundSet_WeaponNameShot): CLASS(SoundSet_WeaponShot_Base)
+    class CLASS(SoundSet_DC15LShot): CLASS(SoundSet_WeaponShot_Base)
     {
-        soundShaders[] = {QCLASS(SoundShader_WeaponNameShot)};
+        soundShaders[] = {QCLASS(SoundShader_DC15LShot)};
     };
 };
