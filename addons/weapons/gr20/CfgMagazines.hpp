a379b1ed936b32518abe2ecbbdd9f90e68ce71f2
DartRuffian <skulldude327@gmail.com>
2023-12-24 11:24:03 -0600

    Legacy magazine

--- addons/weapons/gr20/CfgMagazines.hpp
+++ addons/weapons/gr20/CfgMagazines.hpp
@@ -14,4 +14,10 @@ class CfgMagazines
         ammo = QCLASS(Bullet_BactaSmoke); // Most likely a temporary name
         count = 1;
     };
+
+    class CLASS(Mag_GR20): CLASS(Mag_1rnd_GR20)
+    {
+        SCOPE_HIDDEN;
+        descriptionShort = "Pressurized Bacta Gas<br/>Rounds: 1<br/>Used In: GR-20<br/>Nicknamed the 'Bacta Bomb' by Republic Medics<br/>LEGACY CLASS<br/>This class has been deprecated and will be removed in the future.";
+    };
 };
