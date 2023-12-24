9298b932dfafec867080d9b98693ca112cbb2579
DartRuffian <skulldude327@gmail.com>
2023-12-24 11:47:42 -0600

    DC-15C uses DC-15S sounds

--- addons/weapons/dc15c/CfgSoundSets.hpp
+++
@@ -1,8 +0,0 @@
-class CfgSoundSets
-{
-    class CLASS(SoundSet_WeaponShot_Base);
-    class CLASS(SoundSet_WeaponNameShot): CLASS(SoundSet_WeaponShot_Base)
-    {
-        soundShaders[] = {QCLASS(SoundShader_WeaponNameShot)};
-    };
-};
