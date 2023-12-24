f0f905e89dd186950c65227212873971b5cbe83c
DartRuffian <skulldude327@gmail.com>
2023-12-24 16:21:30 -0600

    Magazine

--- addons/weapons/rps6/CfgMagazines.hpp
+++ addons/weapons/rps6/CfgMagazines.hpp
@@ -1,17 +1,23 @@
 class CfgMagazines
 {
     class CLASS(Mag_Base);
-    class CLASS(Mag_1rnd_WeaponName): CLASS(Mag_Base)
+    class CLASS(Mag_1rnd_RPS6_Disposable): CLASS(Mag_Base)
     {
         SCOPE_PUBLIC;
-        displayName = "[KC] WeaponName Energy Cell";
-        displayNameShort = "Standard Energy";
-        descriptionShort = "Energy Cell Pack<br/>Rounds: 1<br/>Used In: WeaponName";
+        displayName = "[KC] RPS-6 HEAT Disposable Rocket";
+        displayNameShort = "HEAT";
+        descriptionShort = "High Explosive Anti-Tank Rocket<br/>Rounds: 1<br/>Used In: RPS-6";
 
-        // model = "";
-        // picture = "";
+        model = "\ls_weapons\tertiary\rps6\ls_magazine_rps6";
+        picture = "\ls_weapons\_ui\RPS6_mag_ca.paa";
 
-        ammo = QCLASS(Bullet_PlasmaRifle_Blue);
+        modelSpecial = "\ls_weapons\tertiary\rps6\ls_magazine_rps6";
+        modelSpecialIsProxy = TRUE;
+
+        ammo = QCLASS(Rocket_RPS6);
         count = 1;
+
+        allowedSlots[] = {BACKPACK_SLOT};
+        mass = MASS_MAGAZINE;
     };
 };
