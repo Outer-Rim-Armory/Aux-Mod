29a6a0567a00310ea4c07fc7f62dbd526fb45724
DartRuffian <skulldude327@gmail.com>
2023-12-23 19:21:05 -0600

    Attachment swapping on reload

--- addons/weapons/CfgEventHandlers.hpp
+++ addons/weapons/CfgEventHandlers.hpp
@@ -13,3 +13,11 @@ class Extended_PreInit_EventHandlers
         init = QUOTE(call COMPILE_SCRIPT(XEH_preInit));
     };
 };
+
+class Extended_Reloaded_EventHandlers
+{
+    class CAManBase
+    {
+        GVAR(attachmentSwap) = QUOTE(_this call FUNC(attachmentSwap));
+    };
+};
