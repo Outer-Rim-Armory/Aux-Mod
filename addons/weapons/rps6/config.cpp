20a3376ae25dc4da8bdbf9a827d408bfcfe5c9f3
DartRuffian <skulldude327@gmail.com>
2023-12-24 16:06:45 -0600

    Base config

---
+++ addons/weapons/rps6/config.cpp
@@ -0,0 +1,27 @@
+#include "script_component.hpp"
+#include "CfgWeapons.hpp"
+#include "CfgMagazines.hpp"
+#include "CfgSoundShaders.hpp"
+#include "CfgSoundSets.hpp"
+
+
+class CfgPatches
+{
+    class SUBADDON
+    {
+        author = "SweMonkey and DartRuffian";
+        name = COMPONENT_NAME;
+        addonRootClass = QUOTE(ADDON);
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] =
+        {
+            QUOTE(ADDON),
+            "ls_weapons"
+        };
+        units[] = {};
+        weapons[] =
+        {
+        };
+        VERSION_CONFIG;
+    };
+};
---
+++ addons/weapons/rps6/script_component.hpp
@@ -0,0 +1,4 @@
+#define SUBCOMPONENT rps6
+#define SUBCOMPONENT_BEAUTIFIED RPS-6
+
+#include "..\script_component.hpp"
