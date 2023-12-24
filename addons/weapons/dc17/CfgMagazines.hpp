ad3a7e6b14ccf8644ddc1ae8a4ee261e471bd3e1
DartRuffian <skulldude327@gmail.com>
2023-12-23 23:46:52 -0600

    Magazine

--- addons/weapons/dc17/CfgMagazines.hpp
+++ addons/weapons/dc17/CfgMagazines.hpp
@@ -1,17 +1,17 @@
 class CfgMagazines
 {
     class CLASS(Mag_Base);
-    class CLASS(Mag_1rnd_DC17): CLASS(Mag_Base)
+    class CLASS(Mag_20rnd_DC17): CLASS(Mag_Base)
     {
         SCOPE_PUBLIC;
         displayName = "[KC] DC-17 Energy Cell";
-        displayNameShort = "Standard Energy";
-        descriptionShort = "Energy Cell Pack<br/>Rounds: 1<br/>Used In: DC-17";
+        displayNameShort = "Low Energy";
+        descriptionShort = "Energy Cell Pack<br/>Rounds: 20<br/>Used In: DC-17";
 
-        // model = "";
-        // picture = "";
+        model = "\MRC\JLTS\weapons\DC17SA\DC17SA_mag.p3d";
+        picture = "\MRC\JLTS\weapons\DC17SA\data\ui\DC17SA_mag_ui_ca.paa";
 
-        ammo = QCLASS(Bullet_PlasmaRifle_Blue);
-        count = 1;
+        ammo = QCLASS(Bullet_PlasmaPistol_Blue);
+        count = 20;
     };
 };
