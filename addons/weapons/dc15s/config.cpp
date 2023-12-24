c549479f8e4e99f3ee4ca2e00b502738fe31a3e2
DartRuffian <skulldude327@gmail.com>
2023-12-23 19:44:19 -0600

    Base config

---
+++ addons/weapons/dc15s/config.cpp
@@ -0,0 +1,29 @@
+#include "script_component.hpp"
+
+
+class CfgPatches
+{
+    class SUBADDON
+    {
+        author = "SweMonkey and DartRuffian";
+        addonRootClass = QUOTE(ADDON);
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] =
+        {
+            QUOTE(ADDON),
+            "JLTS_weapons_DC15S",
+            "Aux501_Patch_Accessories",
+            "JLTS_weapons_DC17SA",
+            "ace_laserpointer"
+        };
+        units[] = {};
+        weapons[] =
+        {
+            QCLASS(DC15S_Base),
+            QCLASS(DC15S),
+            QCLASS(DC15S_Fried),
+            QCLASS(DC15S_RiotShield),
+            QCLASS(DC15S_RiotShield_Fried)
+        };
+    };
+};
