6dd34451e10b2c12006f281659dc409b178dba45
DartRuffian <skulldude327@gmail.com>
2023-12-24 12:09:45 -0600

    Magazine

--- addons/weapons/dc15l/CfgMagazines.hpp
+++ addons/weapons/dc15l/CfgMagazines.hpp
@@ -1,17 +1,23 @@
 class CfgMagazines
 {
     class CLASS(Mag_Base);
-    class CLASS(Mag_1rnd_WeaponName): CLASS(Mag_Base)
+    class CLASS(Mag_240rnd_DC15L): CLASS(Mag_Base)
     {
         SCOPE_PUBLIC;
-        displayName = "[KC] WeaponName Energy Cell";
+        displayName = "[KC] DC-15L Energy Cell";
         displayNameShort = "Standard Energy";
-        descriptionShort = "Energy Cell Pack<br/>Rounds: 1<br/>Used In: WeaponName";
+        descriptionShort = "Energy Cell Pack<br/>Rounds: 240<br/>Used In: DC-15L";
 
-        // model = "";
-        // picture = "";
+        model = "\A3\weapons_F\ammo\mag_univ.p3d";
+        picture = "\3AS\3AS_Weapons\Data\Textures\Energy_Cell_Arsenal.paa";
 
-        ammo = QCLASS(Bullet_PlasmaRifle_Blue);
-        count = 1;
+        ammo = QCLASS(Bullet_PlasmaAutoRifle_Blue);
+        count = 240;
+    };
+
+    class Aux12thFleet_Mag_DC15L: CLASS(Mag_240rnd_DC15L)
+    {
+        SCOPE_HIDDEN;
+        descriptionShort = "Energy Cell Pack<br/>Rounds: 240<br/>Used In: DC-15L<br/>LEGACY CLASS<br/>This class has been deprecated and will be removed in the future.";
     };
 };
