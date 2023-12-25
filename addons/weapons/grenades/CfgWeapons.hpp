f5b310c0c166e897750617325763885fe71a8bb8
DartRuffian <skulldude327@gmail.com>
2023-12-25 11:31:52 -0600

    Grenade muzzles

--- addons/weapons/grenades/CfgWeapons.hpp
+++ addons/weapons/grenades/CfgWeapons.hpp
@@ -1,3 +1,20 @@
 class CfgWeapons
 {
+    class GrenadeLauncher;
+    class Throw: GrenadeLauncher
+    {
+        muzzles[] += {QCLASS(Muzzle_GrenadeEMP), QCLASS(Muzzle_GrenadeBacta)};
+
+        class ThrowMuzzle;
+        class CLASS(Muzzle_GrenadeEMP): ThrowMuzzle
+        {
+            displayName = "[KC] Droid Popper";
+            magazines[] = {QCLASS(Grenade_EMP)};
+        };
+        class CLASS(Muzzle_GrenadeBacta): ThrowMuzzle
+        {
+            displayName = "[KC] Bacta Grenade";
+            magazines[] = {QCLASS(Grenade_Bacta)};
+        };
+    };
 };
