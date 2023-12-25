17b3cf16194fe683d18830f5284e845805ca9dcc
DartRuffian <skulldude327@gmail.com>
2023-12-24 21:29:22 -0600

    Magazines

--- addons/weapons/rps7/CfgMagazines.hpp
+++ addons/weapons/rps7/CfgMagazines.hpp
@@ -1,17 +1,32 @@
 class CfgMagazines
 {
     class CLASS(Mag_Base);
-    class CLASS(Mag_1rnd_WeaponName): CLASS(Mag_Base)
+    class CLASS(Mag_1rnd_RPS7_HE): CLASS(Mag_Base)
     {
         SCOPE_PUBLIC;
-        displayName = "[KC] WeaponName Energy Cell";
-        displayNameShort = "Standard Energy";
-        descriptionShort = "Energy Cell Pack<br/>Rounds: 1<br/>Used In: WeaponName";
+        displayName = "[KC] RPS-7 HE Rocket";
+        displayNameShort = "High Explosive";
+        descriptionShort = "High Explosive Rocket<br/>Rounds: 1<br/>Used In: RPS-7";
 
-        // model = "";
-        // picture = "";
+        model = "\A3\weapons_f\launchers\RPG32\tbg32v_rocket_item.p3d";
+        picture = "\A3\Weapons_F\launchers\RPG32\data\UI\gear_tbg32v_rocket_ca.paa";
 
-        ammo = QCLASS(Bullet_PlasmaRifle_Blue);
+        ammo = QCLASS(Rocket_RPS7_HE);
         count = 1;
+
+        allowedSlots[] = {TYPE_BACKPACK};
+        mass = MASS_MAGAZINE;
+    };
+
+    class CLASS(Mag_1rnd_RPS7_AT): CLASS(Mag_1rnd_RPS7_HE)
+    {
+        displayName = "[KC] RPS-7 AT Rocket";
+        displayNameShort = "Anti-Tank";
+        descriptionShort = "Anti-Tank Rocket<br/>Rounds: 1<br/>Used In: RPS-7";
+
+        model = "\A3\weapons_f\launchers\RPG32\tbg32v_rocket_item.p3d";
+        picture = "\A3\Weapons_F\launchers\RPG32\data\UI\gear_tbg32v_rocket_ca.paa";
+
+        ammo = QCLASS(Rocket_RPS7_AT);
     };
 };
