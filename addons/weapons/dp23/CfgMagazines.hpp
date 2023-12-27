d8498bbb591a3d8575e002dfa736d2cdc6dce32b
DartRuffian <skulldude327@gmail.com>
2023-12-26 21:25:44 -0600

    Magazines

--- addons/weapons/dp23/CfgMagazines.hpp
+++ addons/weapons/dp23/CfgMagazines.hpp
@@ -1,17 +1,38 @@
 class CfgMagazines
 {
     class CLASS(Mag_Base);
-    class CLASS(Mag_1rnd_WeaponName): CLASS(Mag_Base)
+    class CLASS(Mag_30rnd_DP23): CLASS(Mag_Base)
     {
         SCOPE_PUBLIC;
-        displayName = "[KC] WeaponName Energy Cell";
-        displayNameShort = "Standard Energy";
-        descriptionShort = "Energy Cell Pack<br/>Rounds: 1<br/>Used In: WeaponName";
+        displayName = "[KC] DP-23 Energy Cell";
+        displayNameShort = "Low Energy";
+        descriptionShort = "Scatter Energy Cell Pack<br/>Rounds: 30<br/>Used In: DP-23";
 
-        // model = "";
-        // picture = "";
+        model = "\MRC\JLTS\weapons\DP23\DP23_mag.p3d";
+        picture = "\MRC\JLTS\weapons\DP23\data\ui\DP23_mag_ui_ca.paa";
 
-        ammo = QCLASS(Bullet_PlasmaRifle_Blue);
-        count = 1;
+        ammo = QCLASS(Bullet_PlasmaScatter_Blue);
+        count = 30;
+    };
+
+    class CLASS(Mag_15rnd_DP23): CLASS(Mag_30rnd_DP23)
+    {
+        displayName = "[KC] DP-23 HP Energy Cell";
+        displayNameShort = "Medium Energy";
+        descriptionShort = "Scatter Energy Cell Pack<br/>Rounds: 15<br/>Used In: DP-23";
+
+        // ammo = QCLASS(Bullet_PlasmaScatterHP_Blue);
+        count = 15;
+    };
+
+    class Aux12thFleet_Mag_DP23: CLASS(Mag_30rnd_DP23)
+    {
+        SCOPE_HIDDEN;
+        descriptionShort = "Scatter Energy Cell Pack<br/>Rounds: 30<br/>Used In: DP-23<br/>LEGACY CLASS<br/>This class has been deprecated and will be removed in the future.";
+    };
+    class Aux12thFleet_Mag_DP23_HP: CLASS(Mag_15rnd_DP23)
+    {
+        SCOPE_HIDDEN;
+        descriptionShort = "Scatter Energy Cell Pack<br/>Rounds: 15<br/>Used In: DP-23<br/>LEGACY CLASS<br/>This class has been deprecated and will be removed in the future.";
     };
 };
