7ef96af8f0930541d0357014afa873d7f2f1c51a
DartRuffian <skulldude327@gmail.com>
2023-12-27 17:33:54 -0600

    Magazines

--- addons/weapons/e5/CfgMagazines.hpp
+++ addons/weapons/e5/CfgMagazines.hpp
@@ -1,17 +1,23 @@
 class CfgMagazines
 {
     class CLASS(Mag_Base);
-    class CLASS(Mag_1rnd_WeaponName): CLASS(Mag_Base)
+    class CLASS(Mag_100rnd_E5): CLASS(Mag_Base)
     {
         SCOPE_PUBLIC;
-        displayName = "[KC] WeaponName Energy Cell";
+        displayName = "[KC] E-5 Energy Cell";
         displayNameShort = "Standard Energy";
-        descriptionShort = "Energy Cell Pack<br/>Rounds: 1<br/>Used In: WeaponName";
+        descriptionShort = "Energy Cell Pack<br/>Rounds: 100<br/>Used In: E-5";
 
-        // model = "";
-        // picture = "";
+        model = "\MRC\JLTS\weapons\E5\E5_mag.p3d";
+        picture = "\MRC\JLTS\weapons\E5\data\ui\E5_mag_ui_ca.paa";
 
-        ammo = QCLASS(Bullet_PlasmaRifle_Blue);
-        count = 1;
+        ammo = QCLASS(Bullet_PlasmaCarbine_Red);
+        count = 100;
+    };
+
+    class Aux12thFleet_Mag_E5: CLASS(Mag_100rnd_E5)
+    {
+        SCOPE_HIDDEN;
+        descriptionShort = "Energy Cell Pack<br/>Rounds: 100<br/>Used In: E-5<br/>LEGACY CLASS<br/>This class has been deprecated and will be removed in the future.";
     };
 };
