a85f57042c7508bbe4bc3332a1ee1ee2c81a9174
DartRuffian <skulldude327@gmail.com>
2023-12-26 21:09:05 -0600

    Base config

---
+++ addons/weapons/dp23/CfgSoundSets.hpp
@@ -0,0 +1,8 @@
+class CfgSoundSets
+{
+    class CLASS(SoundSet_WeaponShot_Base);
+    class CLASS(SoundSet_WeaponNameShot): CLASS(SoundSet_WeaponShot_Base)
+    {
+        soundShaders[] = {QCLASS(SoundShader_WeaponNameShot)};
+    };
+};
