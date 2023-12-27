e4907b459930286ba33d176205d3d8122526772b
DartRuffian <skulldude327@gmail.com>
2023-12-27 14:29:18 -0600

    Sound shader/set

--- addons/weapons/westar/CfgSoundShaders.hpp
+++ addons/weapons/westar/CfgSoundShaders.hpp
@@ -1,14 +1,14 @@
 class CfgSoundShaders
 {
     class CLASS(SoundShader_WeaponShot_Base);
-    class CLASS(SoundShader_WeaponNameShot): CLASS(SoundShader_WeaponShot_Base)
+    class CLASS(SoundShader_WestarM5Shot): CLASS(SoundShader_WeaponShot_Base)
     {
         samples[] =
         {
-            {QPATHTOF(subAddonName\data\audio\Fire1.wss), 1, 1},
-            {QPATHTOF(subAddonName\data\audio\Fire2.wss), 1, 1},
-            {QPATHTOF(subAddonName\data\audio\Fire3.wss), 1, 1},
-            {QPATHTOF(subAddonName\data\audio\Fire4.wss), 1, 1}
+            {"\3AS\3AS_Main\Sounds\Westar\Westar_Shot_1.ogg", 1, 1},
+            {"\3AS\3AS_Main\Sounds\Westar\Westar_Shot_2.ogg", 1, 1},
+            {"\3AS\3AS_Main\Sounds\Westar\Westar_Shot_3.ogg", 1, 1},
+            {"\3AS\3AS_Main\Sounds\Westar\Westar_Shot_4.ogg", 1, 1}
         };
     };
 };
