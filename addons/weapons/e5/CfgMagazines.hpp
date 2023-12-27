1bad65ed8549fa90b799c16a0256d03998927d42
DartRuffian <skulldude327@gmail.com>
2023-12-27 17:59:23 -0600

    Removed legacy E-5 mag

--- addons/weapons/e5/CfgMagazines.hpp
+++ addons/weapons/e5/CfgMagazines.hpp
@@ -14,10 +14,4 @@ class CfgMagazines
         ammo = QCLASS(Bullet_PlasmaCarbine_Red);
         count = 100;
     };
-
-    class Aux12thFleet_Mag_E5: CLASS(Mag_100rnd_E5)
-    {
-        SCOPE_HIDDEN;
-        descriptionShort = "Energy Cell Pack<br/>Rounds: 100<br/>Used In: E-5<br/>LEGACY CLASS<br/>This class has been deprecated and will be removed in the future.";
-    };
 };
