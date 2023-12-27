c6aef7350b56f9e28d2e235f769dcede64db47eb
DartRuffian <skulldude327@gmail.com>
2023-12-27 14:28:27 -0600

    Magazine

--- addons/weapons/westar/CfgMagazines.hpp
+++ addons/weapons/westar/CfgMagazines.hpp
@@ -1,17 +1,17 @@
 class CfgMagazines
 {
     class CLASS(Mag_Base);
-    class CLASS(Mag_1rnd_WeaponName): CLASS(Mag_Base)
+    class CLASS(Mag_60rnd_WestarM5): CLASS(Mag_Base)
     {
         SCOPE_PUBLIC;
-        displayName = "[KC] WeaponName Energy Cell";
+        displayName = "[KC] Westar-M5 Energy Cell";
         displayNameShort = "Standard Energy";
-        descriptionShort = "Energy Cell Pack<br/>Rounds: 1<br/>Used In: WeaponName";
+        descriptionShort = "Energy Cell Pack<br/>Rounds: 60<br/>Used In: Westar-M5";
 
-        // model = "";
-        // picture = "";
+        model = "\SWLW_clones\smgs\westar_m5\WestarM5_mag.p3d";
+        picture = "\SWLW_clones\smgs\westar_m5\data\ui\WestarM5_mag_ui.paa";
 
         ammo = QCLASS(Bullet_PlasmaRifle_Blue);
-        count = 1;
+        count = 60;
     };
 };
