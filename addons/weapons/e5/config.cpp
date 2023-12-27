c7a40a744b532bc666532ad04a9f8d4c06985d3a
DartRuffian <skulldude327@gmail.com>
2023-12-27 17:19:49 -0600

    Base config

---
+++ addons/weapons/e5/config.cpp
@@ -0,0 +1,31 @@
+#include "script_component.hpp"
+#include "CfgWeapons.hpp"
+#include "CfgMagazines.hpp"
+#include "CfgSoundShaders.hpp"
+#include "CfgSoundSets.hpp"
+
+
+class CfgPatches
+{
+    class SUBADDON
+    {
+        author = "Keeli Company Aux Team";
+        name = COMPONENT_NAME;
+        addonRootClass = QUOTE(ADDON);
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] =
+        {
+            QUOTE(ADDON),
+            QCLASS(weapons_dc15s),
+            "JLTS_weapons_E5",
+            "Aux501_Patch_Accessories",
+            "JLTS_weapons_DC17SA",
+            "ace_laserpointer"
+        };
+        units[] = {};
+        weapons[] =
+        {
+        };
+        VERSION_CONFIG;
+    };
+};
