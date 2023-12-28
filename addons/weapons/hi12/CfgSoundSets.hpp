f1171ebb841597c5b3338778ee9576c75ea0b24f
DartRuffian <skulldude327@gmail.com>
2023-12-27 22:25:59 -0600

    Sound Shader/Set

--- addons/weapons/hi12/CfgSoundSets.hpp
+++ addons/weapons/hi12/CfgSoundSets.hpp
@@ -1,8 +1,8 @@
 class CfgSoundSets
 {
     class CLASS(SoundSet_WeaponShot_Base);
-    class CLASS(SoundSet_WeaponNameShot): CLASS(SoundSet_WeaponShot_Base)
+    class CLASS(SoundSet_HI12Shot): CLASS(SoundSet_WeaponShot_Base)
     {
-        soundShaders[] = {QCLASS(SoundShader_WeaponNameShot)};
+        soundShaders[] = {QCLASS(SoundShader_HI12Shot)};
     };
 };
--- addons/weapons/hi12/CfgSoundShaders.hpp
+++ addons/weapons/hi12/CfgSoundShaders.hpp
@@ -1,14 +1,11 @@
 class CfgSoundShaders
 {
     class CLASS(SoundShader_WeaponShot_Base);
-    class CLASS(SoundShader_WeaponNameShot): CLASS(SoundShader_WeaponShot_Base)
+    class CLASS(SoundShader_HI12Shot): CLASS(SoundShader_WeaponShot_Base)
     {
         samples[] =
         {
-            {QPATHTOF(subAddonName\data\audio\Fire1.wss), 1, 1},
-            {QPATHTOF(subAddonName\data\audio\Fire2.wss), 1, 1},
-            {QPATHTOF(subAddonName\data\audio\Fire3.wss), 1, 1},
-            {QPATHTOF(subAddonName\data\audio\Fire4.wss), 1, 1}
+            {QPATHTOF(hi12\data\audio\Fire1.wss), 1, 1}
         };
     };
 };
