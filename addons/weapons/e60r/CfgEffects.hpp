182e508735571820aa9f50b95d600d119f466223
DartRuffian <skulldude327@gmail.com>
2023-12-27 21:03:57 -0600

    Fixed typo

--- addons/weapons/e60r/CfgEffects.hpp
+++ addons/weapons/e60r/CfgEffects.hpp
@@ -3,21 +3,21 @@ class CLASS(Effect_E60_Fly)
     class Sparks
     {
         simulation = "particles";
-        type = QLCASS(Cloudlet_E60R_Sparks);
+        type = QCLASS(Cloudlet_E60R_Sparks);
         position[] = {0, 0, 0};
         qualityLevel = 2;
     };
     class Smoke
     {
         simulation = "particles";
-        type = QLCASS(Cloudlet_E60R_Smoke);
+        type = QCLASS(Cloudlet_E60R_Smoke);
         position[] = {0, 0, 0};
         qualityLevel = -1;
     };
     class Light
     {
         simulation = "light";
-        type = QLCASS(Light_E60R);
+        type = QCLASS(Light_E60R);
         position[] = {0, 0, 0};
     };
 };
