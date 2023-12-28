01891a355e6fcb1573bb4612ecafbc69be758c20
DartRuffian <skulldude327@gmail.com>
2023-12-27 22:24:34 -0600

    Magazine

--- addons/weapons/hi12/CfgMagazines.hpp
+++ addons/weapons/hi12/CfgMagazines.hpp
@@ -1,17 +1,20 @@
 class CfgMagazines
 {
     class CLASS(Mag_Base);
-    class CLASS(Mag_1rnd_WeaponName): CLASS(Mag_Base)
+    class CLASS(Mag_30rnd_HI12): CLASS(Mag_Base)
     {
         SCOPE_PUBLIC;
-        displayName = "[KC] WeaponName Energy Cell";
+        displayName = "[KC] HI-12 Energy Cell";
         displayNameShort = "Standard Energy";
-        descriptionShort = "Energy Cell Pack<br/>Rounds: 1<br/>Used In: WeaponName";
+        descriptionShort = "Energy Cell Pack<br/>Rounds: 30<br/>Used In: HI-12";
 
-        // model = "";
-        // picture = "";
+        model = "\LF_Weapon_Unit\hi12\hi12_mag.p3d";
+        picture = "\LF_Weapon_Unit\DC17SA\ui\dc17ammo_co.paa";
+
+        initSpeed = 900;
+        mass = 10;
 
         ammo = QCLASS(Bullet_PlasmaRifle_Blue);
-        count = 1;
+        count = 30;
     };
 };
