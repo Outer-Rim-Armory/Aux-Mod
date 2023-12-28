f673c7d63ad76c445031f358d95b7165dc17deb0
DartRuffian <skulldude327@gmail.com>
2023-12-27 23:26:30 -0600

    Sound shader/set

--- addons/weapons/valken38x/CfgSoundSets.hpp
+++ addons/weapons/valken38x/CfgSoundSets.hpp
@@ -1,8 +1,8 @@
 class CfgSoundSets
 {
     class CLASS(SoundSet_WeaponShot_Base);
-    class CLASS(SoundSet_WeaponNameShot): CLASS(SoundSet_WeaponShot_Base)
+    class CLASS(SoundSet_Valken38xShot): CLASS(SoundSet_WeaponShot_Base)
     {
-        soundShaders[] = {QCLASS(SoundShader_WeaponNameShot)};
+        soundShaders[] = {QCLASS(SoundShader_Valken38xShot)};
     };
 };
