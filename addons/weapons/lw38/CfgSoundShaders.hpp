33fb3a3f94c564daf73c65c6d1aa4f37287ce8e5
DartRuffian <skulldude327@gmail.com>
2023-12-27 22:55:41 -0600

    Sound Shader/Set

--- addons/weapons/lw38/CfgSoundShaders.hpp
+++ addons/weapons/lw38/CfgSoundShaders.hpp
@@ -1,14 +1,11 @@
 class CfgSoundShaders
 {
     class CLASS(SoundShader_WeaponShot_Base);
-    class CLASS(SoundShader_WeaponNameShot): CLASS(SoundShader_WeaponShot_Base)
+    class CLASS(SoundShader_LW38Shot): CLASS(SoundShader_WeaponShot_Base)
     {
         samples[] =
         {
-            {QPATHTOF(subAddonName\data\audio\Fire1.wss), 1, 1},
-            {QPATHTOF(subAddonName\data\audio\Fire2.wss), 1, 1},
-            {QPATHTOF(subAddonName\data\audio\Fire3.wss), 1, 1},
-            {QPATHTOF(subAddonName\data\audio\Fire4.wss), 1, 1}
+            {QPATHTOF(lw38\data\audio\Fire1.wss), 1, 1}
         };
     };
 };
