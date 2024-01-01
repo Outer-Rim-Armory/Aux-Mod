add342c9493c6dfb25207cf63e441c7070139627
DartRuffian <skulldude327@gmail.com>
2024-01-01 15:00:49 -0600

    Magazine

--- addons/weapons/z6/CfgMagazines.hpp
+++ addons/weapons/z6/CfgMagazines.hpp
@@ -1,14 +1,25 @@
 class CfgMagazines
 {
-    class JLTS_Z6_mag;
-    class Aux12thFleet_Mag_Z6: JLTS_Z6_mag
+    class CLASS(Mag_Base);
+    class CLASS(Mag_300rnd_Z6): CLASS(Mag_Base)
     {
-        author = "DartRuffian and SweMonkey";
-        displayName = "[12th Fleet] Z-6 Energy Cell";
+        SCOPE_PUBLIC;
+        displayName = "[KC] Z-6 Energy Cell";
         displayNameShort = "Standard Energy";
         descriptionShort = "Energy Cell Pack<br/>Rounds: 300<br/>Used In: Z-6";
-        ammo = "Aux12thFleet_Ammo_MachineGun_Blue";
 
-        JLTS_hasEMPProtection = 1;
+        model = "\MRC\JLTS\weapons\z6\z6_mag.p3d";
+        picture = "\MRC\JLTS\weapons\z6\data\ui\z6_mag_ui_ca.paa";
+
+        ammo = QCLASS(Bullet_PlasmaMachineGun_Blue);
+        count = 300;
+        initSpeed = 920;
+        mass = 9;
+    };
+
+    class Aux12thFleet_Mag_Z6: CLASS(Mag_300rnd_Z6)
+    {
+        SCOPE_HIDDEN;
+        descriptionShort = "Energy Cell Pack<br/>Rounds: 300<br/>Used In: Z-6<br/>LEGACY CLASS<br/>This class has been deprecated and will be removed in the future.";
     };
 };
