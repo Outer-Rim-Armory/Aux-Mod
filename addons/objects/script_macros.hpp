d3bbec06960f07411a1881ab0ff8a8e89311050f
DartRuffian <skulldude327@gmail.com>
2024-01-03 12:08:21 -0600

    Loadout macros

--- addons/objects/script_macros.hpp
+++ addons/objects/script_macros.hpp
@@ -8,3 +8,10 @@
 
 #define LOADOUTMENU_GETPAGE ace_player getVariable [QQGVAR(LoadoutMenu_Page), MENU_PAGE_HOME]
 #define LOADOUTMENU_SETPAGE(var1) ace_player getVariable [QQGVAR(LoadoutMenu_Page), var1]
+
+
+#define LOADOUTS_RANKS ["CR", "CT", "SCT", "VCT", "CLC", "CPL", "CS", "CSS", "CSFC", "CMS", "CSM", "WO", "WO2", "WO3", "LT", "1LT", "CAP", "MAJ", "COM"]
+#define LOADOUTS_RANKS_NCO ["CS", "CSS", "CSFC", "CMS", "CSM"]
+#define LOADOUTS_RANKS_OFFICER ["WO", "WO2", "WO3", "LT", "1LT", "CAP", "MAJ", "COM"]
+#define LOADOUTS_WEAPONS_LIST ["DC15A", "DC15S", "DC15C"]
+#define LOADOUTS_KITS ["Rifleman", "Squad Leader"]
