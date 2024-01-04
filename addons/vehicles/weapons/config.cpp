f24c1b20cd7e11612b03cac90fc8fa52a01cc94a
DartRuffian <skulldude327@gmail.com>
2024-01-03 22:31:40 -0600

    Base vehicle weapons sub-addon

---
+++ addons/vehicles/weapons/config.cpp
@@ -0,0 +1,26 @@
+#include "script_component.hpp"
+#include "CfgWeapons.hpp"
+#include "CfgMagazines.hpp"
+#include "CfgSoundShaders.hpp"
+#include "CfgSoundSets.hpp"
+
+
+class CfgPatches
+{
+    class SUBADDON
+    {
+        author = "Keeli Company Aux Team";
+        name = COMPONENT_NAME;
+        addonRootClass = QUOTE(ADDON);
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] =
+        {
+            QUOTE(ADDON)
+        };
+        units[] = {};
+        weapons[] =
+        {
+        };
+        VERSION_CONFIG;
+    };
+};
