665a8a919636036141bca223b0e6c408dddfb8df
DartRuffian <skulldude327@gmail.com>
2024-01-06 10:59:24 -0600

    Base config

---
+++ addons/vehicles/air/ogre/config.cpp
@@ -0,0 +1,28 @@
+#include "script_component.hpp"
+#include "CfgVehicles.hpp"
+
+
+class CfgPatches
+{
+    class SUBADDON
+    {
+        author = "Keeli Company Aux Team";
+        name = COMPONENT_NAME;
+        addonRootClass = QUOTE(ADDON);
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] =
+        {
+            QUOTE(ADDON),
+            QGVAR(weapons),
+            QGVAR(sounds),
+            "OPTRE_Vehicles_Pelican"
+        };
+        units[] =
+        {
+            QCLASS(Ogre_Base),
+            QCLASS(Ogre_Armed_Base)
+        };
+        weapons[] = {};
+        VERSION_CONFIG;
+    };
+};
