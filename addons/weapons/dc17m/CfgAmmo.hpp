65d397717b52f842bffc6ae331615621878374ba
DartRuffian <skulldude327@gmail.com>
2024-01-05 23:22:01 -0600

    Fixed missing ammo

--- addons/weapons/dc17m/CfgAmmo.hpp
+++ addons/weapons/dc17m/CfgAmmo.hpp
@@ -10,4 +10,15 @@ class CfgAmmo
         timeToLive = 6;
         typicalSpeed = 300;
     };
+
+    class CLASS(Bullet_PlasmaSniper_Blue);
+    class CLASS(Bullet_PlasmaDC17M_Sniper): CLASS(Bullet_PlasmaSniper_Blue)
+    {
+        hit = 95;
+        caliber = 3;
+        suppressionRadiusHit = 10;
+        suppressionRadiusBulletClose = 8;
+        timetolive = 20;
+        typicalspeed = 1250;
+    };
 };
