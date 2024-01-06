89b0af42a9f9321416f933399c7e70d81fecb4cc
DartRuffian <skulldude327@gmail.com>
2024-01-06 11:21:19 -0600

    Rathian

--- addons/vehicles/air/rathian/CfgVehicles.hpp
+++ addons/vehicles/air/rathian/CfgVehicles.hpp
@@ -1,4 +1,71 @@
 class CfgVehicles
 {
+    class Helicopter_Base_H;
+    class OPTRE_Hornet_base: Helicopter_Base_H
+    {
+        class ACE_SelfActions;
+    };
+    class CLASS(Rathian_Base): OPTRE_Hornet_base
+    {
+        SCOPE_PRIVATE;
+        author = "Keeli Company Aux Team";
 
+        editorSubcategory = QCLASS(EdSubCat_Helicopters);
+        editorPreview = QPATHTOF(air\rathian\data\previews\CLASS(Rathian_Tan).jpg);
+
+        displayName = "Rathian";
+
+        hiddenSelectionsTextures[] = {"\OPTRE_Vehicles_Air\hornet\data\hornet_hull_desert_co.paa"};
+
+        textureList[] = {"TechnoUnion", 1, "EPF", 0};
+        class TextureSources
+        {
+            class TechnoUnion
+            {
+                author = "Article 2 Studios";
+                displayName = "Techno Union";
+                factions[] = {QCLASS(Faction_TU)};
+                textures[] = {"\OPTRE_Vehicles_Air\hornet\data\hornet_hull_desert_co.paa"};
+            };
+            class EPF: TechnoUnion
+            {
+                displayName = "El President Forces";
+                factions[] = {QCLASS(Faction_EPF)};
+                textures[] = {"\OPTRE_Vehicles_Air\hornet\data\hornet_hull_standard_co.paa"};
+            };
+        };
+
+        class ACE_SelfActions: ACE_SelfActions
+        {
+            SELFINTERACTION_HUD_CHANGER;
+        };
+    };
+
+    class CLASS(Rathian_CAP_Base): CLASS(Rathian_Base)
+    {
+        displayName = "Rathian (CAP)";
+        weapons[] = {"OPTRE_GUA23A", "OPTRE_missiles_C2GMLS", "CMFlareLauncher", "Laserdesignator_pilotCamera"};
+        magazines[] =
+        {
+            "OPTRE_2000Rnd_20mm_HEIAP",
+            "OPTRE_8Rnd_C2GMLS_missiles",
+            "OPTRE_8Rnd_C2GMLS_missiles",
+            "168Rnd_CMFlare_Chaff_Magazine",
+            "Laserbatteries"
+        };
+    };
+
+    class CLASS(Rathian_CAS_Base): CLASS(Rathian_Base)
+    {
+        displayName = "Rathian (CAS)";
+        weapons[] = {"OPTRE_GUA23A", "OPTRE_missiles_C2GMLS", "CMFlareLauncher", "Laserdesignator_pilotCamera"};
+        magazines[] =
+        {
+            "OPTRE_2000Rnd_20mm_HEIAP",
+            "OPTRE_8Rnd_C2GMLS_missiles",
+            "OPTRE_8Rnd_C2GMLS_missiles",
+            "168Rnd_CMFlare_Chaff_Magazine",
+            "Laserbatteries"
+        };
+    };
 };
