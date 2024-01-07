e013a207ea61fdd526e3613d291b9224c9adfaba
DartRuffian <skulldude327@gmail.com>
2024-01-07 17:49:28 -0600

    Fixed component name

--- addons/particles/script_component.hpp
+++ addons/particles/script_component.hpp
@@ -1,5 +1,5 @@
-#define COMPONENT component
-#define COMPONENT_BEAUTIFIED Component
+#define COMPONENT particles
+#define COMPONENT_BEAUTIFIED Particles
 
 #include "\ORA\BNA_KC\addons\core\script_mod.hpp"
 #include "\ORA\BNA_KC\addons\core\script_macros.hpp"
