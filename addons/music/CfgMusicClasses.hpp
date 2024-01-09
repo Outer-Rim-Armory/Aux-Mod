0e7b43e11b0555d3579d5d90e78a2b996c6c97cc
DartRuffian <skulldude327@gmail.com>
2024-01-09 03:28:52 -0600

    Configs

---
+++ addons/music/CfgMusicClasses.hpp
@@ -0,0 +1,23 @@
+class CfgMusicClasses
+{
+    class BNA_KC_SWMusic
+    {
+        displayName = "[KC] Star Wars";
+    };
+    class BNA_KC_VGMusic
+    {
+        displayName = "[KC] Video Games";
+    };
+    class BNA_KC_AnimeMusic
+    {
+        displayName = "[KC] Anime";
+    };
+    class BNA_KC_OtherMusic
+    {
+        displayName = "[KC] Other";
+    };
+    class BNA_KC_SFX
+    {
+        displayName = "[KC] Sound Effects"
+    };
+};
---
+++ addons/music/CfgSFX.hpp
@@ -0,0 +1,187 @@
+class CfgSFX
+{
+    class BNA_KC_Ambient_Restaurant_SFX
+    {
+        name = "[KC] Restaurant Ambience";
+        sounds[] = {BNA_KC_Ambient_Restaurant};
+        BNA_KC_Ambient_Restaurant[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\Restaurant_Ambience.ogg",db+10,1,200,1,0,0,0};
+        empty[] = {"",0,0,0,0,0,0,0};
+    };
+    class BNA_KC_Ambient_Incoming_SFX
+    {
+        name = "[KC] Incoming Alarm";
+        sounds[] = {BNA_KC_Ambient_Incoming};
+        BNA_KC_Ambient_Incoming[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\incoming_incoming.ogg",db+50,1,400,1,2,3,4};
+        empty[] = {"",0,0,0,0,0,0,0};
+    };
+    class BNA_KC_Ambient_Disturbance_SFX
+    {
+        name = "[KC] Disturbance in the force";
+        sounds[] = {BNA_KC_Ambient_Disturbance};
+        BNA_KC_Ambient_Disturbance[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\Disturbance.ogg",db+10,1,200,1,2,3,4};
+        empty[] = {"",0,0,0,0,0,0,0};
+    };
+    class BNA_KC_Ambient_JediWarning_SFX
+    {
+        name = "[KC] Jedi Warning";
+        sounds[] = {BNA_KC_Ambient_JediWarning};
+        BNA_KC_Ambient_JediWarning[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\Jedi_Warning.ogg",db+10,1,200,1,2,3,4};
+        empty[] = {"",0,0,0,0,0,0,0};
+    };
+    class BNA_KC_Ambient_ATTE_Alarm_SFX
+    {
+        name = "[KC] ATTE Alarm";
+        sounds[] = {BNA_KC_Ambient_ATTE_Alarm};
+        BNA_KC_Ambient_ATTE_Alarm[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\ATTE_Alarm.ogg",db+50,1,500,1,2,3,4};
+        empty[] = {"",0,0,0,0,0,0,0};
+    };
+    class BNA_KC_Ambient_DK_Radio_SFX
+    {
+        name = "[KC] DK Radio Chatter";
+        sounds[] = {BNA_KC_Ambient_DK_Radio};
+        BNA_KC_Ambient_DK_Radio[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\DK_Radio_Chatter.ogg",db+50,1,100,1,2,3,4};
+        empty[] = {"",0,0,0,0,0,0,0};
+    };
+    class BNA_KC_Ambient_Carnage_Roar_SFX
+    {
+        name = "[KC] Carnage Roar";
+        sounds[] = {BNA_KC_Ambient_Carnage_Roar};
+        BNA_KC_Ambient_Carnage_Roar[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\Carnage_Roar.ogg",db+50,1,500,1,2,3,4};
+        empty[] = {"",0,0,0,0,0,0,0};
+    };
+    class BNA_KC_Ambient_Carnage_Roar_2_SFX
+    {
+        name = "[KC] Carnage Roar 2";
+        sounds[] = {BNA_KC_Ambient_Carnage_Roar_2};
+        BNA_KC_Ambient_Carnage_Roar_2[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\Carnage_Roar_2.ogg",db+50,1,500,1,2,3,4};
+        empty[] = {"",0,0,0,0,0,0,0};
+    };
+    class BNA_KC_Ambient_Carnage_Roar_3_SFX
+    {
+        name = "[KC] Carnage Roar 3";
+        sounds[] = {BNA_KC_Ambient_Carnage_Roar_3};
+        BNA_KC_Ambient_Carnage_Roar_3[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\Carnage_Roar_3.ogg",db+50,1,500,1,2,3,4};
+        empty[] = {"",0,0,0,0,0,0,0};
+    };
+    class BNA_KC_Ambient_Carnage_Roar_4_SFX
+    {
+        name = "[KC] Carnage Roar 4";
+        sounds[] = {BNA_KC_Ambient_Carnage_Roar_4};
+        BNA_KC_Ambient_Carnage_Roar_4[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\Carnage_Roar_4.ogg",db+50,1,500,1,2,3,4};
+        empty[] = {"",0,0,0,0,0,0,0};
+    };
+    class BNA_KC_Ambient_Let_There_Be_Carnage_SFX
+    {
+        name = "[KC] Let There Be Carnage";
+        sounds[] = {BNA_KC_Ambient_Let_There_Be_Carnage};
+        BNA_KC_Ambient_Let_There_Be_Carnage[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\Let_There_Be_Carnage.ogg",db+50,1,500,1,2,3,4};
+        empty[] = {"",0,0,0,0,0,0,0};
+    };
+    class BNA_KC_Ambient_EPF_Radio_1_SFX
+    {
+        name = "[KC] Holonet News";
+        sounds[] = {BNA_KC_Ambient_EPF_Radio_1};
+        BNA_KC_Ambient_EPF_Radio_1[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\EPF_Radio_1.ogg",db+80,1,100,1,2,3,4};
+        empty[] = {"",0,0,0,0,0,0,0};
+    };
+    class BNA_KC_Ambient_EPF_Radio_2_SFX
+    {
+        name = "[KC] Holonet News 2";
+        sounds[] = {BNA_KC_Ambient_EPF_Radio_2};
+        BNA_KC_Ambient_EPF_Radio_2[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\EPF_Radio_2.ogg",db+80,1,100,1,2,3,4};
+        empty[] = {"",0,0,0,0,0,0,0};
+    };
+    class BNA_KC_Ambient_EPF_Radio_3_SFX
+    {
+        name = "[KC] Holonet News 3";
+        sounds[] = {BNA_KC_Ambient_EPF_Radio_3};
+        BNA_KC_Ambient_EPF_Radio_3[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\EPF_Radio_3.ogg",db+80,1,100,1,2,3,4};
+        empty[] = {"",0,0,0,0,0,0,0};
+    };
+};
+
+
+class CfgSounds
+{
+    class BNA_KC_Ambient_Restaurant
+    {
+        name = "Restaurant Ambience";
+        sound[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\Restaurant_Ambience.ogg", db+10, 1};
+        titles[] = {2, "Restaurant"};
+    };
+    class BNA_KC_Ambient_Incoming
+    {
+        name = "Incoming Alarm";
+        sound[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\incoming_incoming.ogg", db+50, 1};
+        titles[] = {2, "Incoming Alarm"};
+    };
+    class BNA_KC_Ambient_Disturbance
+    {
+        name = "Disturbance in the force";
+        sound[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\Disturbance.ogg", db+10, 1};
+        titles[] = {2, "Disturbance in the force"};
+    };
+    class BNA_KC_Ambient_JediWarning
+    {
+        name = "Jedi Warning";
+        sound[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\Jedi_Warning.ogg", db+10, 1};
+        titles[] = {2, "Jedi Warning"};
+    };
+    class BNA_KC_Ambient_ATTE_Alarm
+    {
+        name = "ATTE Alarm";
+        sound[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\ATTE_Alarm.ogg", db+10, 1};
+        titles[] = {2, "ATTE Alarm"};
+    };
+    class BNA_KC_Ambient_Radio_DK
+    {
+        name = "DK Radio Chatter";
+        sound[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\DK_Radio_Radio_SFX.ogg", db+10, 1};
+        titles[] = {2, "DK Radio Chatter"};
+    };
+    class BNA_KC_Ambient_Carnage_Roar
+    {
+        name = "Carnage Roar";
+        sound[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\Carnage_Roar.ogg", db+50, 1, 500, 1, 2, 3, 4};
+        titles[] = {2, "Roaring"};
+    };
+    class BNA_KC_Ambient_Carnage_Roar_2: BNA_KC_Ambient_Carnage_Roar
+    {
+        name = "Carnage Roar 2";
+        sound[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\Carnage_Roar_2.ogg", db+50, 1, 500, 1, 2, 3, 4};
+    };
+    class BNA_KC_Ambient_Carnage_Roar_3: BNA_KC_Ambient_Carnage_Roar
+    {
+        name = "Carnage Roar 3";
+        sound[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\Carnage_Roar_3.ogg", db+50, 1, 500, 1, 2, 3, 4};
+    };
+    class BNA_KC_Ambient_Carnage_Roar_4: BNA_KC_Ambient_Carnage_Roar
+    {
+        name = "Carnage Roar 4";
+        sound[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\Carnage_Roar_4.ogg", db+50, 1, 500, 1, 2, 3, 4};
+    };
+    class BNA_KC_Ambient_Let_There_Be_Carnage
+    {
+        name = "Let There Be Carnage";
+        sound[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\Let_There_Be_Carnage.ogg", db+50, 1, 500, 1, 2, 3, 4};
+        titles[] = {2, "'Let there be carnage!'"};
+    };
+    class BNA_KC_Ambient_Holonet
+    {
+        name = "Holonet News";
+        sound[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\EPF_Radio_1.ogg", db+20, 1, 500, 1};
+        titles[] = {2, "'Holonet News'"};
+    };
+    class BNA_KC_Ambient_Holonet_2
+    {
+        name = "Holonet News 2";
+        sound[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\EPF_Radio_2.ogg", db+20, 1, 500, 1};
+        titles[] = {2, "'Holonet News 2'"};
+    };
+    class BNA_KC_Ambient_Holonet_3
+    {
+        name = "Holonet News 3";
+        sound[] = {"BNA_KC_ZeusAudioModule\Data\Audio\Ambience\EPF_Radio_3.ogg", db+20, 1, 500, 1};
+        titles[] = {2, "'Holonet News 3'"};
+    };
+};
