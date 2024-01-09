f10a7d73d1a3ca6e1d49cf77bb057c1575ee43ad
DartRuffian <skulldude327@gmail.com>
2024-01-09 13:47:08 -0600

    Sounds macro

--- addons/music/script_macros.hpp
+++ addons/music/script_macros.hpp
@@ -1,3 +1,10 @@
+#define SOUND(var1,var2,var3,var4) class TRIPLES(Prefix,Sound,var1) \
+{ \
+    name = var2; \
+    sound[] = {QPATHTOF(data\ambient\var3##), "db+10", 1}; \
+    titles[] = {2, var4}; \
+}
+
 #define SOUND_FX(var1,var2,var3) class TRIPLES(DOUBLES(PREFIX,Sound),var1,SFX) \
 { \
     name = var2; \
