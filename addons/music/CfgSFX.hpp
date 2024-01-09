c3082f433d69ea3cd4f41af86ad2e3124f8e6194
DartRuffian <skulldude327@gmail.com>
2024-01-09 11:51:24 -0600

    Put quotes on db values

--- addons/music/CfgSFX.hpp
+++ addons/music/CfgSFX.hpp
@@ -4,98 +4,98 @@ class CfgSFX
     {
         name = "[KC] Restaurant Ambience";
         sounds[] = {BNA_KC_Ambient_Restaurant};
-        BNA_KC_Ambient_Restaurant[] = {QPATHTOF(data\ambient\Restaurant_Ambience.ogg), db+10, 1, 200, 1, 0, 0, 0};
+        BNA_KC_Ambient_Restaurant[] = {QPATHTOF(data\ambient\Restaurant_Ambience.ogg), "db+10", 1, 200, 1, 0, 0, 0};
         empty[] = {"", 0, 0, 0, 0, 0, 0, 0};
     };
     class BNA_KC_Ambient_Incoming_SFX
     {
         name = "[KC] Incoming Alarm";
         sounds[] = {BNA_KC_Ambient_Incoming};
-        BNA_KC_Ambient_Incoming[] = {QPATHTOF(data\ambient\incoming_incoming.ogg), db+50, 1, 400, 1, 2, 3, 4};
+        BNA_KC_Ambient_Incoming[] = {QPATHTOF(data\ambient\incoming_incoming.ogg), "db+50", 1, 400, 1, 2, 3, 4};
         empty[] = {"", 0, 0, 0, 0, 0, 0, 0};
     };
     class BNA_KC_Ambient_Disturbance_SFX
     {
         name = "[KC] Disturbance in the force";
         sounds[] = {BNA_KC_Ambient_Disturbance};
-        BNA_KC_Ambient_Disturbance[] = {QPATHTOF(data\ambient\Disturbance.ogg), db+10, 1, 200, 1, 2, 3, 4};
+        BNA_KC_Ambient_Disturbance[] = {QPATHTOF(data\ambient\Disturbance.ogg), "db+10", 1, 200, 1, 2, 3, 4};
         empty[] = {"", 0, 0, 0, 0, 0, 0, 0};
     };
     class BNA_KC_Ambient_JediWarning_SFX
     {
         name = "[KC] Jedi Warning";
         sounds[] = {BNA_KC_Ambient_JediWarning};
-        BNA_KC_Ambient_JediWarning[] = {QPATHTOF(data\ambient\Jedi_Warning.ogg), db+10, 1, 200, 1, 2, 3, 4};
+        BNA_KC_Ambient_JediWarning[] = {QPATHTOF(data\ambient\Jedi_Warning.ogg), "db+10", 1, 200, 1, 2, 3, 4};
         empty[] = {"", 0, 0, 0, 0, 0, 0, 0};
     };
     class BNA_KC_Ambient_ATTE_Alarm_SFX
     {
         name = "[KC] ATTE Alarm";
         sounds[] = {BNA_KC_Ambient_ATTE_Alarm};
-        BNA_KC_Ambient_ATTE_Alarm[] = {QPATHTOF(data\ambient\ATTE_Alarm.ogg), db+50, 1, 500, 1, 2, 3, 4};
+        BNA_KC_Ambient_ATTE_Alarm[] = {QPATHTOF(data\ambient\ATTE_Alarm.ogg), "db+50", 1, 500, 1, 2, 3, 4};
         empty[] = {"", 0, 0, 0, 0, 0, 0, 0};
     };
     class BNA_KC_Ambient_DK_Radio_SFX
     {
         name = "[KC] DK Radio Chatter";
         sounds[] = {BNA_KC_Ambient_DK_Radio};
-        BNA_KC_Ambient_DK_Radio[] = {QPATHTOF(data\ambient\DK_Radio_Chatter.ogg), db+50, 1, 100, 1, 2, 3, 4};
+        BNA_KC_Ambient_DK_Radio[] = {QPATHTOF(data\ambient\DK_Radio_Chatter.ogg), "db+50", 1, 100, 1, 2, 3, 4};
         empty[] = {"", 0, 0, 0, 0, 0, 0, 0};
     };
     class BNA_KC_Ambient_Carnage_Roar_SFX
     {
         name = "[KC] Carnage Roar";
         sounds[] = {BNA_KC_Ambient_Carnage_Roar};
-        BNA_KC_Ambient_Carnage_Roar[] = {QPATHTOF(data\ambient\Carnage_Roar.ogg), db+50, 1, 500, 1, 2, 3, 4};
+        BNA_KC_Ambient_Carnage_Roar[] = {QPATHTOF(data\ambient\Carnage_Roar.ogg), "db+50", 1, 500, 1, 2, 3, 4};
         empty[] = {"", 0, 0, 0, 0, 0, 0, 0};
     };
     class BNA_KC_Ambient_Carnage_Roar_2_SFX
     {
         name = "[KC] Carnage Roar 2";
         sounds[] = {BNA_KC_Ambient_Carnage_Roar_2};
-        BNA_KC_Ambient_Carnage_Roar_2[] = {QPATHTOF(data\ambient\Carnage_Roar_2.ogg), db+50, 1, 500, 1, 2, 3, 4};
+        BNA_KC_Ambient_Carnage_Roar_2[] = {QPATHTOF(data\ambient\Carnage_Roar_2.ogg), "db+50", 1, 500, 1, 2, 3, 4};
         empty[] = {"", 0, 0, 0, 0, 0, 0, 0};
     };
     class BNA_KC_Ambient_Carnage_Roar_3_SFX
     {
         name = "[KC] Carnage Roar 3";
         sounds[] = {BNA_KC_Ambient_Carnage_Roar_3};
-        BNA_KC_Ambient_Carnage_Roar_3[] = {QPATHTOF(data\ambient\Carnage_Roar_3.ogg), db+50, 1, 500, 1, 2, 3, 4};
+        BNA_KC_Ambient_Carnage_Roar_3[] = {QPATHTOF(data\ambient\Carnage_Roar_3.ogg), "db+50", 1, 500, 1, 2, 3, 4};
         empty[] = {"", 0, 0, 0, 0, 0, 0, 0};
     };
     class BNA_KC_Ambient_Carnage_Roar_4_SFX
     {
         name = "[KC] Carnage Roar 4";
         sounds[] = {BNA_KC_Ambient_Carnage_Roar_4};
-        BNA_KC_Ambient_Carnage_Roar_4[] = {QPATHTOF(data\ambient\Carnage_Roar_4.ogg), db+50, 1, 500, 1, 2, 3, 4};
+        BNA_KC_Ambient_Carnage_Roar_4[] = {QPATHTOF(data\ambient\Carnage_Roar_4.ogg), "db+50", 1, 500, 1, 2, 3, 4};
         empty[] = {"", 0, 0, 0, 0, 0, 0, 0};
     };
     class BNA_KC_Ambient_Let_There_Be_Carnage_SFX
     {
         name = "[KC] Let There Be Carnage";
         sounds[] = {BNA_KC_Ambient_Let_There_Be_Carnage};
-        BNA_KC_Ambient_Let_There_Be_Carnage[] = {QPATHTOF(data\ambient\Let_There_Be_Carnage.ogg), db+50, 1, 500, 1, 2, 3, 4};
+        BNA_KC_Ambient_Let_There_Be_Carnage[] = {QPATHTOF(data\ambient\Let_There_Be_Carnage.ogg), "db+50", 1, 500, 1, 2, 3, 4};
         empty[] = {"", 0, 0, 0, 0, 0, 0, 0};
     };
     class BNA_KC_Ambient_EPF_Radio_1_SFX
     {
         name = "[KC] Holonet News";
         sounds[] = {BNA_KC_Ambient_EPF_Radio_1};
-        BNA_KC_Ambient_EPF_Radio_1[] = {QPATHTOF(data\ambient\EPF_Radio_1.ogg), db+80, 1, 100, 1, 2, 3, 4};
+        BNA_KC_Ambient_EPF_Radio_1[] = {QPATHTOF(data\ambient\EPF_Radio_1.ogg), "db+80", 1, 100, 1, 2, 3, 4};
         empty[] = {"", 0, 0, 0, 0, 0, 0, 0};
     };
     class BNA_KC_Ambient_EPF_Radio_2_SFX
     {
         name = "[KC] Holonet News 2";
         sounds[] = {BNA_KC_Ambient_EPF_Radio_2};
-        BNA_KC_Ambient_EPF_Radio_2[] = {QPATHTOF(data\ambient\EPF_Radio_2.ogg), db+80, 1, 100, 1, 2, 3, 4};
+        BNA_KC_Ambient_EPF_Radio_2[] = {QPATHTOF(data\ambient\EPF_Radio_2.ogg), "db+80", 1, 100, 1, 2, 3, 4};
         empty[] = {"", 0, 0, 0, 0, 0, 0, 0};
     };
     class BNA_KC_Ambient_EPF_Radio_3_SFX
     {
         name = "[KC] Holonet News 3";
         sounds[] = {BNA_KC_Ambient_EPF_Radio_3};
-        BNA_KC_Ambient_EPF_Radio_3[] = {QPATHTOF(data\ambient\EPF_Radio_3.ogg), db+80, 1, 100, 1, 2, 3, 4};
+        BNA_KC_Ambient_EPF_Radio_3[] = {QPATHTOF(data\ambient\EPF_Radio_3.ogg), "db+80", 1, 100, 1, 2, 3, 4};
         empty[] = {"", 0, 0, 0, 0, 0, 0, 0};
     };
 };
