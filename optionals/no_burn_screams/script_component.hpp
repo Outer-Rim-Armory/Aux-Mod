f7670248e9260cec034380c2056e98f0cf7fe6e8
DartRuffian <skulldude327@gmail.com>
2024-01-09 02:16:17 -0600

    Renamed no_burn_screams addon

--- optionals/no_burn_screams/script_component.hpp
+++ optionals/no_burn_screams/script_component.hpp
@@ -1,6 +1,6 @@
-#define COMPONENT noBurnScreams
+#define COMPONENT no_burn_screams
 #define COMPONENT_BEAUTIFIED No Burn 'Em Screams
 
 #include "\ORA\BNA_KC\addons\core\script_mod.hpp"
 #include "\ORA\BNA_KC\addons\core\script_macros.hpp"
-#include "\ORA\BNA_KC\addons\noBurnScreams\script_macros.hpp"
+#include "\ORA\BNA_KC\addons\no_burn_screams\script_macros.hpp"
