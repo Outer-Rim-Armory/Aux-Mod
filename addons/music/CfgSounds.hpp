c1260e8fd61da8f8024c0b613a3a891cf18a4934
DartRuffian <skulldude327@gmail.com>
2024-01-09 13:47:08 -0600

    Sounds macro

--- addons/music/CfgSounds.hpp
+++ addons/music/CfgSounds.hpp
@@ -1,84 +1,17 @@
 class CfgSounds
 {
-    class CLASS(Sound_Restaurant)
-    {
-        name = "Restaurant Ambience";
-        sound[] = {QPATHTOF(data\ambient\Restaurant_Ambience.ogg), "db+10", 1};
-        titles[] = {2, "Restaurant"};
-    };
-    class CLASS(Sound_Incoming)
-    {
-        name = "Incoming Alarm";
-        sound[] = {QPATHTOF(data\ambient\incoming_incoming.ogg), "db+50", 1};
-        titles[] = {2, "Incoming Alarm"};
-    };
-    class CLASS(Sound_Disturbance)
-    {
-        name = "Disturbance in the force";
-        sound[] = {QPATHTOF(data\ambient\Disturbance.ogg), "db+10", 1};
-        titles[] = {2, "Disturbance in the force"};
-    };
-    class CLASS(Sound_JediWarning)
-    {
-        name = "Jedi Warning";
-        sound[] = {QPATHTOF(data\ambient\Jedi_Warning.ogg), "db+10", 1};
-        titles[] = {2, "Jedi Warning"};
-    };
-    class CLASS(Sound_ATTE_Alarm)
-    {
-        name = "ATTE Alarm";
-        sound[] = {QPATHTOF(data\ambient\ATTE_Alarm.ogg), "db+10", 1};
-        titles[] = {2, "ATTE Alarm"};
-    };
-    class CLASS(Sound_Radio_DK)
-    {
-        name = "DK Radio Chatter";
-        sound[] = {QPATHTOF(data\ambient\DK_Radio_Radio_SFX.ogg), "db+10", 1};
-        titles[] = {2, "DK Radio Chatter"};
-    };
-    class CLASS(Sound_Carnage_Roar)
-    {
-        name = "Carnage Roar";
-        sound[] = {QPATHTOF(data\ambient\Carnage_Roar.ogg), "db+50", 1, 500, 1, 2, 3, 4};
-        titles[] = {2, "Roaring"};
-    };
-    class CLASS(Sound_Carnage_Roar_2): CLASS(Sound_Carnage_Roar)
-    {
-        name = "Carnage Roar 2";
-        sound[] = {QPATHTOF(data\ambient\Carnage_Roar_2.ogg), "db+50", 1, 500, 1, 2, 3, 4};
-    };
-    class CLASS(Sound_Carnage_Roar_3): CLASS(Sound_Carnage_Roar)
-    {
-        name = "Carnage Roar 3";
-        sound[] = {QPATHTOF(data\ambient\Carnage_Roar_3.ogg), "db+50", 1, 500, 1, 2, 3, 4};
-    };
-    class CLASS(Sound_Carnage_Roar_4): CLASS(Sound_Carnage_Roar)
-    {
-        name = "Carnage Roar 4";
-        sound[] = {QPATHTOF(data\ambient\Carnage_Roar_4.ogg), "db+50", 1, 500, 1, 2, 3, 4};
-    };
-    class CLASS(Sound_Let_There_Be_Carnage)
-    {
-        name = "Let There Be Carnage";
-        sound[] = {QPATHTOF(data\ambient\Let_There_Be_Carnage.ogg), "db+50", 1, 500, 1, 2, 3, 4};
-        titles[] = {2, "'Let there be carnage!'"};
-    };
-    class CLASS(Sound_Holonet)
-    {
-        name = "Holonet News";
-        sound[] = {QPATHTOF(data\ambient\EPF_Radio_1.ogg), "db+20", 1, 500, 1};
-        titles[] = {2, "'Holonet News'"};
-    };
-    class CLASS(Sound_Holonet_2)
-    {
-        name = "Holonet News 2";
-        sound[] = {QPATHTOF(data\ambient\EPF_Radio_2.ogg), "db+20", 1, 500, 1};
-        titles[] = {2, "'Holonet News 2'"};
-    };
-    class CLASS(Sound_Holonet_3)
-    {
-        name = "Holonet News 3";
-        sound[] = {QPATHTOF(data\ambient\EPF_Radio_3.ogg), "db+20", 1, 500, 1};
-        titles[] = {2, "'Holonet News 3'"};
-    };
+    SOUND(Restaurant,"Restaurant Ambience",Restaurant_Ambience.ogg,"Restaurant chatter");
+    SOUND(Incoming,"Incoming Alarm",incoming_incoming.ogg,"Incoming! Incoming!");
+    SOUND(Disturbance,"Disturbance in the force",Disturbance.ogg,"You sense a disturbance in the force...");
+    SOUND(JediWarning,"Jedi Warning",Jedi_Warning.ogg,"A warning to all Jedi...");
+    SOUND(ATTE_Alarm,"AT-TE Alarm",ATTE_Alarm.ogg,"AT-TE Alarm");
+    SOUND(DK_Radio,"DK Radio Chatter",DK_Radio_Chatter.ogg,"Static, cries for help, and blaster fire");
+    SOUND(CarnageRoar,"Carnage Roar",Carnage_Roar.ogg,"Roaring");
+    SOUND(CarnageRoar2,"Carnage Roar 2",Carnage_Roar2.ogg,"Roaring");
+    SOUND(CarnageRoar3,"Carnage Roar 3",Carnage_Roar3.ogg,"Roaring");
+    SOUND(CarnageRoar4,"Carnage Roar 4",Carnage_Roar4.ogg,"Roaring");
+    SOUND(LetThereBeCarnage,"Let There Be Carnage",Let_There_Be_Carnage.ogg,"Let there be carnage!");
+    SOUND(EPF_Radio,"Holownet News",EPF_Radio.ogg,"News broadcast");
+    SOUND(EPF_Radio2,"Holownet News 2",EPF_Radio2.ogg,"News broadcast");
+    SOUND(EPF_Radio3,"Holownet News 3",EPF_Radio3.ogg,"News broadcast");
 };
