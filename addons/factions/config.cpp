01d6b2c167dd6759fc2ff552d2f9ff9bee3b7752
DartRuffian <skulldude327@gmail.com>
2024-01-10 22:45:52 -0600

    Base classes

--- addons/factions/config.cpp
+++ addons/factions/config.cpp
@@ -9,10 +9,25 @@ class CfgPatches
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] =
         {
-            QCLASS(core)
+            QCLASS(core),
+            "ls_armor_redfor",
+            "ls_armor_greenfor"
         };
-        units[] = {};
-        weapons[] = {};
+        units[] =
+        {
+            QCLASS(OPFOR_Unit_Base),
+            QCLASS(INDEP_Unit_Base),
+            QCLASS(OtherSide_Backpack_Base)
+        };
+        weapons[] =
+        {
+            QCLASS(OPFOR_Helmet_Base),
+            QCLASS(OPFOR_Uniform_Base),
+            QCLASS(OPFOR_Vest_Base),
+            QCLASS(INDEP_Helmet_Base),
+            QCLASS(INDEP_Uniform_Base),
+            QCLASS(INDEP_Vest_Base)
+        };
         VERSION_CONFIG;
     };
 };
