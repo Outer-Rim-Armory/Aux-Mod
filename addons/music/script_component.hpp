e709a43a88a0242c0e346393d40ccbc803221de0
DartRuffian <skulldude327@gmail.com>
2024-01-10 22:22:41 -0600

    Updated to new format

--- addons/music/script_component.hpp
+++ addons/music/script_component.hpp
@@ -1,6 +1,9 @@
 #define COMPONENT music
 #define COMPONENT_BEAUTIFIED Music
-
 #include "\ORA\BNA_KC\addons\core\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
 #include "\ORA\BNA_KC\addons\core\script_macros.hpp"
 #include "script_macros.hpp"
