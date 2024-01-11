1fa759441612002dac204d2615cd2d0a5d1ed7d9
DartRuffian <skulldude327@gmail.com>
2024-01-10 23:43:14 -0600

    BX Droid

--- addons/factions/cis/CfgVehicles.hpp
+++ addons/factions/cis/CfgVehicles.hpp
@@ -18,4 +18,61 @@ class CfgVehicles
         impactEffectsBlood = "ImpactMetal";
         impactEffectsNoBlood = "ImpactMetal";
     };
+    class CLASS(CIS_Unit_Droid_BX): CLASS(CIS_Unit_DroidBase)
+    {
+        SCOPE_PUBLIC;
+
+        editorSubcategory = QCLASS(EdSubCat_SpecialForces);
+        editorPreview = QPATHTOF(cis\data\previews\CLASS(CIS_Unit_Droid_BX).jpg);
+
+        displayName = "BX Commando Droid";
+        uniformClass = QCLASS(CIS_Uniform_Droid_BX);
+
+        weapons[] =
+        {
+            QCLASS(E5),
+            "",
+            "Throw",
+            "Put"
+        };
+        respawnWeapons[] =
+        {
+            QCLASS(E5),
+            "",
+            "Throw",
+            "Put"
+        };
+
+        magazines[] =
+        {
+            ITEM_11(QCLASS(Mag_100rnd_E5)),
+            ITEM_2("ls_mag_classC_thermalDet"),
+            ITEM_3("3AS_SmokeWhite")
+        };
+        respawnMagazines[] =
+        {
+            ITEM_11(QCLASS(Mag_100rnd_E5)),
+            ITEM_2("ls_mag_classC_thermalDet"),
+            ITEM_3("3AS_SmokeWhite")
+        };
+        items[] =
+        {
+            // Other
+            ITEM_5("ACE_CableTie")
+        };
+        respawnItems[] =
+        {
+            // Other
+            ITEM_5("ACE_CableTie")
+        };
+
+        linkedItems[] =
+        {
+            QCLASS(CIS_Vest_Droid_BX), "JLTS_NVG_droid_chip_2", "SWLB_comlink_droid", LINKED_ITEMS
+        };
+        respawnLinkedItems[] =
+        {
+            QCLASS(CIS_Vest_Droid_BX), "JLTS_NVG_droid_chip_2", "SWLB_comlink_droid", LINKED_ITEMS
+        };
+    };
 };
