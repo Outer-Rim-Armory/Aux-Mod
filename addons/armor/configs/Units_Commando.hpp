5fa32558a3a8c89dce4926508bd373a6eaab2f4a
DartRuffian <skulldude327@gmail.com>
2024-01-13 14:32:22 -0600

    Gave dc-17m to commando unit

--- addons/armor/configs/Units_Commando.hpp
+++ addons/armor/configs/Units_Commando.hpp
@@ -1,7 +1,7 @@
 class CLASS(Unit_Commando_Base): CLASS(Unit_ARC_CT)
 {
     displayName = "SF Commando - Base";
-    editorPreview = "\SWLB_clones_spec\data\ui\editorPreviews\SWLB_clone_commando_base.jpg";
+    editorPreview = EDITOR_PREVIEW(Unit_Commando_Base);
 
     uniformClass = QCLASS(Uniform_Commando_Base);
     model = "\ls_armor_bluefor\uniform\sob\commando\ls_sob_commando_uniform.p3d";
@@ -12,6 +12,61 @@ class CLASS(Unit_Commando_Base): CLASS(Unit_ARC_CT)
         "\ls_armor_bluefor\uniform\sob\commando\data\camo2_co.paa"
     };
 
+    weapons[] =
+        {
+            QCLASS(DC17M),
+            QCLASS(DC15SA),
+            "SWLB_clone_binocular",
+            "Throw",
+            "Put"
+        };
+        respawnWeapons[] =
+        {
+            QCLASS(DC17M),
+            QCLASS(DC15SA),
+            "SWLB_clone_binocular",
+            "Throw",
+            "Put"
+        };
+        magazines[] =
+        {
+            // Ammo
+            ITEM_11(QCLASS(Mag_60rnd_DC17M)),
+            ITEM_2(QCLASS(Mag_7rnd_DC15SA)),
+            // Grenades
+            ITEM_2("ls_mag_classC_thermalDet"),
+            ITEM_3("ShieldGrenade_Mag"),
+            // Smokes
+            ITEM_3("3AS_SmokeWhite"),
+            ITEM_3("3AS_SmokeBlue"),
+            ITEM_3("3AS_SmokeGreen")
+        };
+        respawnMagazines[] =
+        {
+            // Ammo
+            ITEM_11(QCLASS(Mag_60rnd_DC17M)),
+            ITEM_2(QCLASS(Mag_7rnd_DC15SA)),
+            // Grenades
+            ITEM_2("ls_mag_classC_thermalDet"),
+            ITEM_3("ShieldGrenade_Mag"),
+            // Smokes
+            ITEM_3("3AS_SmokeWhite"),
+            ITEM_3("3AS_SmokeBlue"),
+            ITEM_3("3AS_SmokeGreen")
+        };
+        items[] =
+        {
+            // Medical
+            ITEM_10("ACE_elasticBandage"),
+            ITEM_4("ACE_tourniquet"),
+        };
+        respawnItems[] =
+        {
+            // Medical
+            ITEM_10("ACE_elasticBandage"),
+            ITEM_4("ACE_tourniquet")
+        };
+
     linkedItems[] =
     {
         QCLASS(Helmet_Commando_Base), QCLASS(Vest_Commando_Basic), QCLASS(NVG_Chip), "lsd_gar_republicCommando_hud", CLONE_LINKED_ITEMS_RADIO
@@ -20,5 +75,5 @@ class CLASS(Unit_Commando_Base): CLASS(Unit_ARC_CT)
     {
         QCLASS(Helmet_Commando_Base), QCLASS(Vest_Commando_Basic), QCLASS(NVG_Chip), "lsd_gar_republicCommando_hud", CLONE_LINKED_ITEMS_RADIO
     };
-    backpack = QCLASS(Backack_Commando_Base);
+    backpack = QCLASS(Backack_Commando);
 };
