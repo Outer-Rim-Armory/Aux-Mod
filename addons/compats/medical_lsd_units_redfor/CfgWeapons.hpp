f1ea9a18fb9dbc6c6ee688a42ab55baa87193719
DartRuffian <skulldude327@gmail.com>
2024-01-13 21:50:43 -0600

    LS Droids compat

---
+++ addons/compats/medical_lsd_units_redfor/CfgWeapons.hpp
@@ -0,0 +1,8 @@
+class CfgWeapons
+{
+    class ls_redforUniform_base;
+    class ls_cis_b1Droid_uniform: ls_redforUniform_base
+    {
+        EGVAR(medical,canBleed) = FALSE;
+    };
+};
---
+++ addons/compats/medical_lsd_units_redfor/script_component.hpp
@@ -0,0 +1,4 @@
+#define SUBCOMPONENT medical_lsd_units_redfor
+#define SUBCOMPONENT_BEAUTIFIED Medical - LS Droids
+
+#include "..\script_component.hpp"
