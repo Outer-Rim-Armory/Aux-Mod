57ab6000b2a6248d608be9c0cd30cd53822f73f3
DartRuffian <skulldude327@gmail.com>
2024-01-13 20:51:28 -0600

    Backpack on Chest compat

---
+++ addons/compats/jetpacks_boc/CfgVehicles.hpp
@@ -0,0 +1,25 @@
+class CfgVehicles
+{
+    class Man;
+    class CAManBase: Man
+    {
+        class ACE_SelfActions
+        {
+            class ACE_Equipment
+            {
+                class zade_boc_onChest
+                {
+                    condition = QUOTE(!(missionNamespace getVariable [ARR_2('zade_boc_disabled',false)]) && !(backpack _player isEqualTo '') && ([_player] call zade_boc_fnc_chestpack isEqualTo '') && !(_player call BNAKC_Jetpacks_fnc_hasJetpack));
+                };
+                class zade_boc_onBack: zade_boc_onChest
+                {
+                    condition = QUOTE(!(missionNamespace getVariable [ARR_2('zade_boc_disabled',false)]) && (backpack _player isEqualTo '') && !([_player] call zade_boc_fnc_chestpack isEqualTo '') && !(_player call BNAKC_Jetpacks_fnc_hasJetpack));
+                };
+                class zade_boc_swap: zade_boc_onChest
+                {
+                    condition = QUOTE(!(missionNamespace getVariable [ARR_2('zade_boc_disabled',false)]) && !(backpack _player isEqualTo '') && !([_player] call zade_boc_fnc_chestpack isEqualTo '') && !(_player call BNAKC_Jetpacks_fnc_hasJetpack));
+                };
+            };
+        };
+    };
+};
---
+++ addons/compats/jetpacks_boc/script_component.hpp
@@ -0,0 +1,4 @@
+#define SUBCOMPONENT jetpacks_boc
+#define SUBCOMPONENT_BEAUTIFIED Jetpacks - Backpack On Chest
+
+#include "..\script_component.hpp"
