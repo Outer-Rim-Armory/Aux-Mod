c503d2ae476977484e2fcf2c508ed4f8c7488606
DartRuffian <skulldude327@gmail.com>
2024-01-15 11:32:17 -0600

    Ignored legacy addons
    Legacy addons aren't necessary to keep on github

--- addons_old/BNA_KC_Vehicles/Static/Mortar/ArtySubmunition.hpp
+++
@@ -1,9 +0,0 @@
-class SmokeShellArty;
-class BNA_KC_SmokeShellArty_Blue: SmokeShellArty
-{
-    effectsSmoke = "BNA_KC_ComplexEffects_SmokeShellBlueSmall";
-};
-class BNA_KC_SmokeShellArty_Red: SmokeShellArty
-{
-    effectsSmoke = "BNA_KC_ComplexEffects_SmokeShellRedSmall";
-};
