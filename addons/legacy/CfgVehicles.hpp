30b9a4d7e1708cfa39408924cd0f6a157ad3ccf4
DartRuffian <skulldude327@gmail.com>
2024-01-29 09:29:10 -0600

    Fixed missing colons/semicolons

--- addons/legacy/CfgVehicles.hpp
+++ addons/legacy/CfgVehicles.hpp
@@ -1,19 +1,19 @@
 class CfgVehicles
 {
-    class CLASS(Venator_Resurgence_ClosedFullyHollowed)
-    class CLASS(Ven_Resurgence_ClosedFullyHollowed) CLASS(Venator_Resurgence_ClosedFullyHollowed)
+    class CLASS(Venator_Resurgence_ClosedFullyHollowed);
+    class CLASS(Ven_Resurgence_ClosedFullyHollowed): CLASS(Venator_Resurgence_ClosedFullyHollowed)
     {
         SCOPE_HIDDEN;
     };
 
-    class CLASS(Venator_Resurgence_OpenFullyHollowed)
-    class CLASS(Ven_Resurgence_OpenFullyHollowed) CLASS(Venator_Resurgence_OpenFullyHollowed)
+    class CLASS(Venator_Resurgence_OpenFullyHollowed);
+    class CLASS(Ven_Resurgence_OpenFullyHollowed): CLASS(Venator_Resurgence_OpenFullyHollowed)
     {
         SCOPE_HIDDEN;
     };
 
-    class CLASS(Venator_Resurgence_Zeus)
-    class CLASS(Ven_Resurgence_Zeus) CLASS(Venator_Resurgence_Zeus)
+    class CLASS(Venator_Resurgence_Zeus);
+    class CLASS(Ven_Resurgence_Zeus): CLASS(Venator_Resurgence_Zeus)
     {
         SCOPE_HIDDEN;
     };
