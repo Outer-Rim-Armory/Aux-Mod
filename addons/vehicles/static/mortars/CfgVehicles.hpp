ecd5603ce573be69319a3c0875a80d34ce678b8c
DartRuffian <skulldude327@gmail.com>
2024-02-02 12:19:22 -0600

    Increased mortar magazine rounds

--- addons/vehicles/static/mortars/CfgVehicles.hpp
+++ addons/vehicles/static/mortars/CfgVehicles.hpp
@@ -142,10 +142,10 @@ class CfgVehicles
             {
                 magazines[] =
                 {
-                    QCLASS(Mag_3Rnd_Mortar_82mm_HE),
-                    QCLASS(Mag_3Rnd_Mortar_SmokeWhite),
-                    QCLASS(Mag_3Rnd_Mortar_SmokeBlue),
-                    QCLASS(Mag_3Rnd_Mortar_SmokeRed)
+                    QCLASS(Mag_6Rnd_Mortar_82mm_HE),
+                    QCLASS(Mag_6Rnd_Mortar_SmokeWhite),
+                    QCLASS(Mag_6Rnd_Mortar_SmokeBlue),
+                    QCLASS(Mag_6Rnd_Mortar_SmokeRed)
                 };
                 weapons[] = {QCLASS(Mortar_M190_Turret)};
             };
