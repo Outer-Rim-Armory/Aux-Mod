de0443fba44c36412c54f6865131c96094293080
DartRuffian <skulldude327@gmail.com>
2024-02-10 19:57:14 -0600

    Moved ground holders

--- addons/factions/gar/CfgVehicles.hpp
+++ addons/factions/gar/CfgVehicles.hpp
@@ -1,17 +1,5 @@
 class CfgVehicles
 {
-    class DOUBLES(Ground,CLASS(Holder_Base));
-    GROUND_HOLDER(Helmet_Phase1_HowzerBlue,"INF P1 Helm ('Howzer') [Blue]");
-    GROUND_HOLDER(Helmet_Phase1_HowzerBrown,"INF P1 Helm ('Howzer') [Brown]");
-    GROUND_HOLDER(Helmet_Phase1_91st_Ponds,"INF P1 Helm ('Ponds')");
-    GROUND_HOLDER(Helmet_Phase2_Fil,"INF P2 Helm ('Fil')");
-    GROUND_HOLDER(Helmet_Phase2_FilSquad,"INF P2 Helm ('Fil's Squad')");
-    GROUND_HOLDER(Helmet_Phase2_HowzerBlue,"INF P2 Helm ('Howzer') [Blue]");
-    GROUND_HOLDER(Helmet_Phase2_HowzerBrown,"INF P2 Helm ('Howzer') [Brown]");
-    GROUND_HOLDER(Helmet_Phase2_91st_Ponds,"INF P2 Helm ('Ponds')");
-    GROUND_HOLDER(Helmet_Phase2_CG_Stone,"INF P2 Helm ('Stone')");
-    GROUND_HOLDER(Helmet_Phase2_187th,"INF P2 Helm (187th Trooper)");
-
     class CLASS(Unit_Phase2_Base);
     class CLASS(Unit_Fil): CLASS(Unit_Phase2_Base)
     {
@@ -184,4 +172,16 @@ class CfgVehicles
             QCLASS(Helmet_Phase2_187th), QCLASS(Vest_Basic), QCLASS(NVG_Chip), "lsd_gar_p2Interior_hud", CLONE_LINKED_ITEMS_RADIO
         };
     };
+
+    class DOUBLES(Ground,CLASS(Holder_Base));
+    GROUND_HOLDER(Helmet_Phase1_HowzerBlue,"INF P1 Helm ('Howzer') [Blue]");
+    GROUND_HOLDER(Helmet_Phase1_HowzerBrown,"INF P1 Helm ('Howzer') [Brown]");
+    GROUND_HOLDER(Helmet_Phase1_91st_Ponds,"INF P1 Helm ('Ponds')");
+    GROUND_HOLDER(Helmet_Phase2_Fil,"INF P2 Helm ('Fil')");
+    GROUND_HOLDER(Helmet_Phase2_FilSquad,"INF P2 Helm ('Fil's Squad')");
+    GROUND_HOLDER(Helmet_Phase2_HowzerBlue,"INF P2 Helm ('Howzer') [Blue]");
+    GROUND_HOLDER(Helmet_Phase2_HowzerBrown,"INF P2 Helm ('Howzer') [Brown]");
+    GROUND_HOLDER(Helmet_Phase2_91st_Ponds,"INF P2 Helm ('Ponds')");
+    GROUND_HOLDER(Helmet_Phase2_CG_Stone,"INF P2 Helm ('Stone')");
+    GROUND_HOLDER(Helmet_Phase2_187th,"INF P2 Helm (187th Trooper)");
 };
