021c212f93d33bb9c66285c15dea5857673c63c8
DartRuffian <skulldude327@gmail.com>
2024-02-10 20:09:50 -0600

    CG Helmets

--- addons/factions/gar/CfgWeapons.hpp
+++ addons/factions/gar/CfgWeapons.hpp
@@ -99,7 +99,31 @@ class CfgWeapons
         EGVAR(custom_armor,isCustom) = TRUE;
     };
 
-    class CLASS(Helmet_Phase2_CG_Stone): CLASS(Helmet_Phase2_Base)
+    class CLASS(Helmet_Phase1_CG): CLASS(Helmet_Phase1_Base)
+    {
+        displayName = "[CG] INF P1 Helm";
+        hiddenSelectionsTextures[] =
+        {
+            QPATHTOF(SUBCOMPONENT\data\helmets\phase1\CG_camo1_co.paa),
+            "\ls_armor_bluefor\helmet\gar\phase1\data\visor_co.paa"
+        };
+
+        EGVAR(custom_armor,isCustom) = TRUE;
+    };
+
+    class CLASS(Helmet_Phase2_CG): CLASS(Helmet_Phase2_Base)
+    {
+        displayName = "[CG] INF P2 Helm";
+        hiddenSelectionsTextures[] =
+        {
+            QPATHTOF(SUBCOMPONENT\data\helmets\phase2\CG_camo1_co.paa),
+            "\ls_armor_bluefor\helmet\gar\phase2\data\visor_co.paa"
+        };
+
+        EGVAR(custom_armor,isCustom) = TRUE;
+    };
+
+    class CLASS(Helmet_Phase2_CG_Stone): CLASS(Helmet_Phase2_CG)
     {
         displayName = "[CG] INF P2 Helm ('Stone')";
         hiddenSelectionsTextures[] =
@@ -107,8 +131,6 @@ class CfgWeapons
             QPATHTOF(SUBCOMPONENT\data\helmets\phase2\Stone_camo1_co.paa),
             "\ls_armor_bluefor\helmet\gar\phase2\data\visor_co.paa"
         };
-
-        EGVAR(custom_armor,isCustom) = TRUE;
     };
 
     class CLASS(Helmet_Phase2_187th): CLASS(Helmet_Phase2_Base)
