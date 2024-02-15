bb5b393ebba250e5b202d1be0f1be60e9ee3aa87
DartRuffian <skulldude327@gmail.com>
2024-02-14 23:32:22 -0600

    Weapon optic tweaks

--- addons/weapons/accessories/CfgWeapons.hpp
+++ addons/weapons/accessories/CfgWeapons.hpp
@@ -33,7 +33,8 @@ class CfgWeapons
                     distanceZoomMax = 50;
                     distanceZoomMin = 50;
 
-                    visionMode[] = {};
+                    visionMode[] = {"Normal"};
+                    thermalMode[] = {WHOT, BHOT};
                     opticsPPEffects[] = {"OpticsBlur1"};
                 };
             };
@@ -45,11 +46,17 @@ class CfgWeapons
         SCOPE_PUBLIC;
 
         displayName = "[KC] Pistol Holosight";
+        descriptionShort = "Pistol scope<br/>Magnification: 1x";
 
         model = QPATHTOF(SUBCOMPONENT\data\optics\Pistol_Holosight.p3d);
         picture = QPATHTOF(SUBCOMPONENT\data\ui\Pistol_Holosight_ca.paa);
 
         ace_scopeHeightAboveRail = 2.8;
+
+        class ItemInfo: ItemInfo
+        {
+            mass = 2;
+        };
     };
 
     class CLASS(Optic_Pistol2): CLASS(Optic_Pistol)
@@ -64,6 +71,7 @@ class CfgWeapons
         SCOPE_PUBLIC;
 
         displayName = "[KC] Short Range Holosight";
+        descriptionShort = "Short range rifle scope<br/>Magnification: 2-4x";
 
         model = QPATHTOF(SUBCOMPONENT\data\optics\SR_Holosight.p3d);
         picture = QPATHTOF(SUBCOMPONENT\data\ui\SR_Holosight_ca.paa);
@@ -72,9 +80,16 @@ class CfgWeapons
 
         class ItemInfo: ItemInfo
         {
+            modelOptics = QPATHTOF(SUBCOMPONENT\data\optics\SR_Holosight.p3d);
+            mass = 8;
+
             class OpticsModes: OpticsModes
             {
-                class Sight: Sight {};
+                class Sight: Sight
+                {
+                    distanceZoomMax = 200;
+                    distanceZoomMin = 200;
+                };
 
                 class Scope: Sight
                 {
@@ -99,6 +114,11 @@ class CfgWeapons
         displayName = "[KC] Short Range Holosight 2";
         model = QPATHTOF(SUBCOMPONENT\data\optics\SR_Holosight2.p3d);
         picture = QPATHTOF(SUBCOMPONENT\data\ui\SR_Holosight2_ca.paa);
+
+        class ItemInfo: ItemInfo
+        {
+            modelOptics = QPATHTOF(SUBCOMPONENT\data\optics\SR_Holosight2.p3d);
+        };
     };
 
     class CLASS(Optic_SR_Holosight3): CLASS(Optic_SR_Holosight)
@@ -106,6 +126,11 @@ class CfgWeapons
         displayName = "[KC] Short Range Holosight 3";
         model = QPATHTOF(SUBCOMPONENT\data\optics\SR_Holosight3.p3d);
         picture = QPATHTOF(SUBCOMPONENT\data\ui\SR_Holosight3_ca.paa);
+
+        class ItemInfo: ItemInfo
+        {
+            modelOptics = QPATHTOF(SUBCOMPONENT\data\optics\SR_Holosight3.p3d);
+        };
     };
 
     class CLASS(Optic_MR_Holosight): CLASS(Optic_Base)
@@ -113,6 +138,7 @@ class CfgWeapons
         SCOPE_PUBLIC;
 
         displayName = "[KC] Medium Range Holosight";
+        descriptionShort = "Medium range rifle scope<br/>Magnification: 2-6x";
 
         model = QPATHTOF(SUBCOMPONENT\data\optics\MR_Holosight.p3d);
         picture = QPATHTOF(SUBCOMPONENT\data\ui\MR_Holosight_ca.paa);
@@ -121,21 +147,27 @@ class CfgWeapons
 
         class ItemInfo: ItemInfo
         {
+            modelOptics = QPATHTOF(SUBCOMPONENT\data\optics\MR_Holosight.p3d);
+            opticType = OPTIC_ZOOM_LEVEL_LONG;
+            mass = 8;
+
             class OpticsModes: OpticsModes
             {
-                class Sight: Sight {};
+                class Sight: Sight
+                {
+                    distanceZoomMax = 300;
+                    distanceZoomMin = 300;
+                };
 
                 class Scope: Sight
                 {
                     opticsID = 2;
                     memoryPointCamera = "opticView";
 
+                    opticsFlare = TRUE;
                     opticsZoomInit = __EVAL(0.25/2);
                     opticsZoomMax = __EVAL(0.25/2);
                     opticsZoomMin = __EVAL(0.25/6);
-
-                    distanceZoomMin = 300;
-                    distanceZoomMax = 300;
 
                     opticsPPEffects[] = {"OpticsCHAbera5", "OpticsBlur5"};
                 };
@@ -148,6 +180,11 @@ class CfgWeapons
         displayName = "[KC] Medium Range Holosight 2";
         model = QPATHTOF(SUBCOMPONENT\data\optics\MR_Holosight2.p3d);
         picture = QPATHTOF(SUBCOMPONENT\data\ui\MR_Holosight2_ca.paa);
+
+        class ItemInfo: ItemInfo
+        {
+            modelOptics = QPATHTOF(SUBCOMPONENT\data\optics\MR_Holosight2.p3d);
+        };
     };
 
     class CLASS(Optic_MR_Holosight3): CLASS(Optic_MR_Holosight)
@@ -155,6 +192,11 @@ class CfgWeapons
         displayName = "[KC] Medium Range Holosight 3";
         model = QPATHTOF(SUBCOMPONENT\data\optics\MR_Holosight3.p3d);
         picture = QPATHTOF(SUBCOMPONENT\data\ui\MR_Holosight3_ca.paa);
+
+        class ItemInfo: ItemInfo
+        {
+            modelOptics = QPATHTOF(SUBCOMPONENT\data\optics\MR_Holosight3.p3d);
+        };
     };
 
     class CLASS(Optic_LR_Holosight): CLASS(Optic_Base)
@@ -162,6 +204,7 @@ class CfgWeapons
         SCOPE_PUBLIC;
 
         displayName = "[KC] Long Range Holosight";
+        descriptionShort = "Long range rifle scope<br/>Magnification: 4-6x";
 
         model = QPATHTOF(SUBCOMPONENT\data\optics\LR_Holosight.p3d);
         picture = QPATHTOF(SUBCOMPONENT\data\ui\LR_Holosight_ca.paa);
@@ -170,9 +213,20 @@ class CfgWeapons
 
         class ItemInfo: ItemInfo
         {
+            modelOptics = QPATHTOF(SUBCOMPONENT\data\optics\LR_Holosight.p3d);
+            opticType = OPTIC_ZOOM_LEVEL_LONG;
+            mass = 12;
+
             class OpticsModes: OpticsModes
             {
-                class Sight: Sight {};
+                class Sight: Sight
+                {
+                    distanceZoomMax = 200;
+                    distanceZoomMin = 200;
+
+                    discreteDistance[] = {200};
+                    discreteDistanceInitIndex = 0;
+                };
 
                 class Scope: Sight
                 {
@@ -183,10 +237,11 @@ class CfgWeapons
                     opticsZoomMax = __EVAL(0.25/2);
                     opticsZoomMin = __EVAL(0.25/6);
 
+                    distanceZoomMax = 1200;
                     distanceZoomMin = 300;
-                    distanceZoomMax = 300;
 
                     opticsPPEffects[] = {"OpticsCHAbera5", "OpticsBlur5"};
+                    visionMode[] = {"Normal", "NVG"};
                 };
             };
         };
@@ -197,6 +252,11 @@ class CfgWeapons
         displayName = "[KC] Long Range Holosight 2";
         model = QPATHTOF(SUBCOMPONENT\data\optics\LR_Holosight2.p3d);
         picture = QPATHTOF(SUBCOMPONENT\data\ui\LR_Holosight2_ca.paa);
+
+        class ItemInfo: ItemInfo
+        {
+            modelOptics = QPATHTOF(SUBCOMPONENT\data\optics\LR_Holosight2.p3d);
+        };
     };
 
     class CLASS(Optic_LR_Holosight3): CLASS(Optic_LR_Holosight)
@@ -204,6 +264,11 @@ class CfgWeapons
         displayName = "[KC] Long Range Holosight 3";
         model = QPATHTOF(SUBCOMPONENT\data\optics\LR_Holosight3.p3d);
         picture = QPATHTOF(SUBCOMPONENT\data\ui\LR_Holosight3_ca.paa);
+
+        class ItemInfo: ItemInfo
+        {
+            modelOptics = QPATHTOF(SUBCOMPONENT\data\optics\LR_Holosight3.p3d);
+        };
     };
 
     class CLASS(Optic_LR_Holosight4): CLASS(Optic_LR_Holosight)
@@ -211,6 +276,11 @@ class CfgWeapons
         displayName = "[KC] Long Range Holosight 4";
         model = QPATHTOF(SUBCOMPONENT\data\optics\LR_Holosight4.p3d);
         picture = QPATHTOF(SUBCOMPONENT\data\ui\LR_Holosight3_ca.paa);
+
+        class ItemInfo: ItemInfo
+        {
+            modelOptics = QPATHTOF(SUBCOMPONENT\data\optics\LR_Holosight4.p3d);
+        };
     };
 
     class CLASS(Optic_Holosight): CLASS(Optic_Base)
@@ -218,6 +288,7 @@ class CfgWeapons
         SCOPE_PUBLIC;
 
         displayName = "[KC] Holo Sight";
+        descriptionShort = "Holo sight<br/>Magnification: 1x";
 
         model = QPATHTOF(SUBCOMPONENT\data\optics\Holosight.p3d);
         picture = QPATHTOF(SUBCOMPONENT\data\ui\Holosight_ca.paa);
@@ -226,6 +297,8 @@ class CfgWeapons
 
         class ItemInfo: ItemInfo
         {
+            mass = 6;
+
             class OpticsModes: OpticsModes
             {
                 class Sight: Sight {};
@@ -250,24 +323,37 @@ class CfgWeapons
     class CLASS(Optic_Holoscope): CLASS(Optic_Holosight)
     {
         displayName = "[KC] Holo Scope";
+        descriptionShort = "Holo scope<br/>Magnification: 2x";
 
         class ItemInfo: ItemInfo
         {
+            modelOptics = "\3AS\3AS_Weapons\Data\3AS_2D_Optic.p3d";
+            mass = 6;
+
             class OpticsModes: OpticsModes
             {
-                class Sight: Sight {};
+                class Sight: Sight
+                {
+                    opticsZoomInit = 0.75;
+                    opticsZoomMax = 1.25;
+                    opticsZoomMin = 0.25;
+
+                    distanceZoomMin = 200;
+                    distanceZoomMax = 200;
+                };
 
                 class Scope: Sight
                 {
                     opticsID = 2;
                     memoryPointCamera = "opticView";
+                    useModelOptics = TRUE;
 
                     opticsZoomInit = __EVAL(0.25/2);
                     opticsZoomMax = __EVAL(0.25/2);
-                    opticsZoomMin = __EVAL(0.25/6);
+                    opticsZoomMin = __EVAL(0.25/2);
 
-                    distanceZoomMin = 300;
-                    distanceZoomMax = 300;
+                    distanceZoomMin = 100;
+                    distanceZoomMax = 100;
 
                     opticsPPEffects[] = {"OpticsCHAbera5", "OpticsBlur5"};
                 };
@@ -305,14 +391,22 @@ class CfgWeapons
 
             class OpticsModes: OpticsModes
             {
-                class Sight: Sight {};
+                class Sight: Sight
+                {
+                    opticsZoomInit = 0.75;
+                    opticsZoomMax = 1.25;
+                    opticsZoomMin = 0.25;
+
+                    distanceZoomMin = 200;
+                    distanceZoomMax = 200;
+                };
 
                 class Scope: Sight
                 {
                     opticsID = 2;
                     memoryPointCamera = "opticView";
-
                     useModelOptics = TRUE;
+
                     opticsZoomInit = __EVAL(0.25/2);
                     opticsZoomMax = __EVAL(0.25/2);
                     opticsZoomMin = __EVAL(0.25/2);
@@ -332,16 +426,17 @@ class CfgWeapons
         SCOPE_PUBLIC;
 
         displayName = "[KC] LRPS Scope";
+        descriptionShort = "Long range sniper scope<br/>Magnification: 6x-25x";
 
         model = "\A3\Weapons_F_EPA\Acc\acco_marksman_F.p3d";
         picture = "\A3\Weapons_F\acc\Data\UI\icon_optic_DMS_ca.paa";
 
         inertia = 0.2;
 
-        ace_scopeAdjust_Horizontal[] = {-8, 8};
-        ace_scopeAdjust_HorizontalIncrement = 0.1;
-        ace_scopeAdjust_Vertical[] = {0, 27};
-        ace_scopeAdjust_VerticalIncrement = 0.1;
+        ace_scopeAdjust_horizontal[] = {-8, 8};
+        ace_scopeAdjust_horizontalIncrement = 0.1;
+        ace_scopeAdjust_vertical[] = {0, 27};
+        ace_scopeAdjust_verticalIncrement = 0.1;
         ace_scopeHeightAboveRail = 4.2098;
 
         class ItemInfo: ItemInfo
@@ -369,11 +464,12 @@ class CfgWeapons
                 {
                     opticsID = 2;
                     memoryPointCamera = "opticView";
-
                     useModelOptics = TRUE;
+
                     opticsZoomInit = 0.042;
                     opticsZoomMax = 0.042;
                     opticsZoomMin = 0.01;
+
                     opticsDisablePeripherialVision = TRUE;
                     opticsFlare = TRUE;
                     opticsPPEffects[] = {"OpticsCHAbera5", "OpticsBlur5"};
@@ -386,7 +482,6 @@ class CfgWeapons
                     distanceZoomMin = 300;
 
                     visionMode[] = {"Normal", "NVG", "TI"};
-                    thermalMode[] = {WHOT, BHOT};
                 };
             };
         };
@@ -395,19 +490,10 @@ class CfgWeapons
     class CLASS(Optic_DC15X): CLASS(Optic_LRPS)
     {
         displayName = "[KC] DC-15X Scope";
-        descriptionShort = "Long range scope<br/>Magnification: 6x-25x";
 
         model = "\MRC\JLTS\weapons\DC15X\DC15X_scope.p3d";
         hiddenSelections[] = {"camo1"};
         hiddenSelectionsTextures[] = {"\MRC\JLTS\weapons\DC15X\data\DC15X_scope_co.paa"};
         picture = "\MRC\JLTS\weapons\DC15X\data\ui\DC15X_scope_ui_ca.paa";
-
-        class ItemInfo: ItemInfo
-        {
-            class OpticsModes: OpticsModes
-            {
-                class Scope: Scope {};
-            };
-        };
     };
 };
