713e8be0e74e13e27df01ebda0c16b42f06484ee
DartRuffian <skulldude327@gmail.com>
2024-02-16 16:02:53 -0600

    ARC Helmets

--- addons/compats/armor_arsenal_extended/XtdGearInfos.hpp
+++ addons/compats/armor_arsenal_extended/XtdGearInfos.hpp
@@ -4,20 +4,62 @@ class XtdGearInfos
     {
         class CLASS(Helmet_Airborne_Base)
         {
-            model = QCLASS(Helmet_Airborne);
+            model = QCLASS(Helmets_Airborne);
             camo = "Base";
         };
 
         class CLASS(Helmet_Airborne_CT)
         {
-            model = QCLASS(Helmet_Airborne);
+            model = QCLASS(Helmets_Airborne);
             camo = "CT";
         };
 
         class CLASS(Helmet_Airborne_CT_v2)
         {
-            model = QCLASS(Helmet_Airborne);
+            model = QCLASS(Helmets_Airborne);
             camo = "CT_v2";
+        };
+
+        class CLASS(Helmet_ARC_Base)
+        {
+            model = QCLASS(Helmets_ARC);
+            camo = "Base";
+        };
+
+        class CLASS(Helmet_ARC_CT)
+        {
+            model = QCLASS(Helmets_ARC);
+            camo = "CT";
+        };
+
+        class CLASS(Helmet_ARC_CT_Legacy)
+        {
+            model = QCLASS(Helmets_ARC);
+            camo = "CT_Legacy";
+        };
+
+        class CLASS(Helmet_ARC_NightShade)
+        {
+            model = QCLASS(Helmets_ARC);
+            camo = "NightShade";
+        };
+
+        class CLASS(Helmet_ARC_Enlisted_CamoBrown)
+        {
+            model = QCLASS(Helmets_ARC);
+            camo = "Enlisted_CamoBrown";
+        };
+
+        class CLASS(Helmet_ARC_Enlisted_CamoGrey)
+        {
+            model = QCLASS(Helmets_ARC);
+            camo = "Enlisted_CamoGrey";
+        };
+
+        class CLASS(Helmet_ARC_Enlisted_CamoNight)
+        {
+            model = QCLASS(Helmets_ARC);
+            camo = "Enlisted_CamoNight";
         };
     };
 };
--- addons/compats/armor_arsenal_extended/XtdGearModels.hpp
+++ addons/compats/armor_arsenal_extended/XtdGearModels.hpp
@@ -2,7 +2,7 @@ class XtdGearModels
 {
     class CfgWeapons
     {
-        class CLASS(Helmet_Airborne)
+        class CLASS(Helmets_Airborne)
         {
             label = "Airborne Helmets";
             author = "Keeli Company Aux Team";
@@ -22,6 +22,36 @@ class XtdGearModels
             class Base { label = "Base"; };
             class CT { label = "CT"; };
             class CT_v2 { label = "CT v2"; };
+        };
+
+        class CLASS(Helmets_ARC)
+        {
+            label = "ARC Helmets";
+            author = "Keeli Company Aux Team";
+            options[] = {"camo", "Base", "CT", "CT_Legacy", "NightShade", "Enlisted_CamoBrown", "Enlisted_CamoGrey", "Enlisted_CamoNight"};
+
+            class camo
+            {
+                changeInGame = FALSE;
+                values[] =
+                {
+                    "Base",
+                    "CT",
+                    "CT_Legacy",
+                    "NightShade",
+                    "Enlisted_CamoBrown",
+                    "Enlisted_CamoGrey",
+                    "Enlisted_CamoNight"
+                };
+            };
+
+            class Base { label = "Base"; };
+            class CT { label = "CT"; };
+            class CT_Legacy { label = "CT (Legacy)"; };
+            class NightShade { label = "NightShade"; };
+            class Enlisted_CamoBrown { label = "Enlisted (Brown Camo)"; };
+            class Enlisted_CamoGrey { label = "Enlisted (Grey Camo)"; };
+            class Enlisted_CamoNight { label = "Enlisted (Night Camo)"; };
         };
     };
 };
