a7f3a0aff8c76034a87d02026bce4c53b5cd64fc
DartRuffian <skulldude327@gmail.com>
2024-02-17 11:45:24 -0600

    Fixed vest names

--- addons/armor/configs/Vests_Airborne.hpp
+++ addons/armor/configs/Vests_Airborne.hpp
@@ -78,7 +78,7 @@ class CLASS(Vest_Airborne_CT_Light): CLASS(Vest_Basic)
 
 class CLASS(Vest_Airborne_CS_Light): CLASS(Vest_Airborne_CT_Light)
 {
-    displayName = "[KC] AB Vest 06 (Sergeant, Light)";
+    displayName = "[KC] AB Vest 06 (NCO, Light)";
     hiddenSelectionsTextures[] =
     {
         QPATHTOF(data\vests\infantry\heavy\Accessories_camo1_co.paa), // Holster
@@ -90,7 +90,7 @@ class CLASS(Vest_Airborne_CS_Light): CLASS(Vest_Airborne_CT_Light)
 
 class CLASS(Vest_Airborne_LifeSaver): CLASS(Vest_Medic)
 {
-    displayName = "[KC] AB MED Vest 01+ (Life Saver)";
+    displayName = "[KC] AB MED Vest 01 (Life Saver)";
     model = "\SWLB_CEE\data\SWLB_CEE_Airborne_CFR.p3d";
     hiddenSelections[] = {"camo1", "camo2", "ammo", "pauldron"};
     hiddenSelectionsTextures[] =
@@ -111,7 +111,7 @@ class CLASS(Vest_Airborne_LifeSaver): CLASS(Vest_Medic)
 
 class CLASS(Vest_Airborne_LifeSaverNCO): CLASS(Vest_Airborne_LifeSaver)
 {
-    displayName = "[KC] AB MED Vest 04+ (NCO Life Saver)";
+    displayName = "[KC] AB MED Vest 04 (NCO Life Saver)";
     hiddenSelectionsTextures[] =
     {
         QPATHTOF(data\vests\infantry\heavy\Medic_v2_camo1_co.paa),
