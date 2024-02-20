d74ad16485a29c453c08153d211f101db98729b0
DartRuffian <skulldude327@gmail.com>
2024-02-20 14:23:16 -0600

    Base custom files

--- addons/custom_armor/XtdGearInfos.hpp
+++ addons/custom_armor/XtdGearInfos.hpp
@@ -2,6 +2,23 @@ class XtdGearInfos
 {
     class CfgWeapons
     {
+        #include "XtdGear\Helmets_P1.hpp"
+        #include "XtdGear\Helmets_P1_Geonosis.hpp"
+        #include "XtdGear\Helmets_P1_Pilot.hpp"
+        #include "XtdGear\Helmets_P1_Tanker.hpp"
+
+        #include "XtdGear\Helmets_P2.hpp"
+        #include "XtdGear\Helmets_P2_Insulated.hpp"
+        #include "XtdGear\Helmets_P2_Pilot.hpp"
+        #include "XtdGear\Helmets_P2_Tanker.hpp"
+
+        #include "XtdGear\Helmets_Airborne.hpp"
+        #include "XtdGear\Helmets_Engineer.hpp"
+
+        #include "XtdGear\Helmets_ARC.hpp"
+        #include "XtdGear\Helmets_ARF.hpp"
+        #include "XtdGear\Helmets_BARC.hpp"
+
         #include "XtdGear\Uniforms.hpp"
         #include "XtdGear\Uniforms_P2_Insulated.hpp"
     };
