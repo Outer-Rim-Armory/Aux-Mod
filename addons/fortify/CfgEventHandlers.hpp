f63c56d43411cf2e186ec7252c9958d85b4dbfff
DartRuffian <skulldude327@gmail.com>
2024-02-28 16:44:48 -0600

    Moved fortify EH to server init

--- addons/fortify/CfgEventHandlers.hpp
+++ addons/fortify/CfgEventHandlers.hpp
@@ -18,6 +18,6 @@ class Extended_PostInit_EventHandlers
 {
     class ADDON
     {
-        init = QUOTE(call COMPILE_SCRIPT(XEH_postInit));
+        serverInit = QUOTE(call COMPILE_SCRIPT(XEH_postInitServer));
     };
 };
