6910ccabf75276d020187e9c7eeff00ec672d7e8
DartRuffian <skulldude327@gmail.com>
2024-02-29 17:41:20 -0600

    SOUND/SOUND_FX don't take file paths

--- addons/music/CfgSFX.hpp
+++ addons/music/CfgSFX.hpp
@@ -5,23 +5,21 @@ class CfgSFX
       - This gets other text added to it, Restaurant becomes BNA_KC_Sound_Restaurant_SFX
     * Second value is the name of the sound that gets displayed
       - Should be prefixed with [KC]
-    * Third value is the file name and extension.
-      - Macro goes looking in the data/ambience folder
     */
-    SOUND_FX(Restaurant,"[KC] Restaurant Ambience",Restaurant_Ambience.ogg);
-    SOUND_FX(Incoming,"[KC] Incoming Alarm",incoming_incoming.ogg);
-    SOUND_FX(Disturbance,"[KC] Disturbance in the force",Disturbance.ogg);
-    SOUND_FX(JediWarning,"[KC] Jedi Warning",Jedi_Warning.ogg);
-    SOUND_FX(ATTE_Alarm,"[KC] AT-TE Alarm",ATTE_Alarm.ogg);
-    SOUND_FX(DK_Radio,"[KC] DK Radio Chatter",DK_Radio_Chatter.ogg);
-    SOUND_FX(CarnageRoar,"[KC] Carnage Roar",Carnage_Roar.ogg);
-    SOUND_FX(CarnageRoar2,"[KC] Carnage Roar 2",Carnage_Roar2.ogg);
-    SOUND_FX(CarnageRoar3,"[KC] Carnage Roar 3",Carnage_Roar3.ogg);
-    SOUND_FX(CarnageRoar4,"[KC] Carnage Roar 4",Carnage_Roar4.ogg);
-    SOUND_FX(LetThereBeCarnage,"[KC] Let There Be Carnage",Let_There_Be_Carnage.ogg);
-    SOUND_FX(EPF_Radio,"[KC] Holonet News",EPF_Radio.ogg);
-    SOUND_FX(EPF_Radio2,"[KC] Holonet News 2",EPF_Radio2.ogg);
-    SOUND_FX(EPF_Radio3,"[KC] Holonet News 3",EPF_Radio3.ogg);
-    SOUND_FX(EPF_PresidenteSpeech,"[KC] El Presidente Speech",EPF_PresidenteSpeech.ogg);
-    SOUND_FX(TDI_Theme,"[KC] Total Drama Island Theme",TDI_Theme.ogg);
+    SOUND_FX(Restaurant,"[KC] Restaurant Ambience");
+    SOUND_FX(Incoming,"[KC] Incoming Alarm");
+    SOUND_FX(Disturbance,"[KC] Disturbance in the forcee");
+    SOUND_FX(JediWarning,"[KC] Jedi Warning");
+    SOUND_FX(ATTE_Alarm,"[KC] AT-TE Alarm");
+    SOUND_FX(DK_Radio,"[KC] DK Radio Chatter");
+    SOUND_FX(CarnageRoar,"[KC] Carnage Roar");
+    SOUND_FX(CarnageRoar2,"[KC] Carnage Roar 2");
+    SOUND_FX(CarnageRoar3,"[KC] Carnage Roar 3");
+    SOUND_FX(CarnageRoar4,"[KC] Carnage Roar 4");
+    SOUND_FX(LetThereBeCarnage,"[KC] Let There Be Carnage");
+    SOUND_FX(EPF_Radio,"[KC] Holonet News");
+    SOUND_FX(EPF_Radio2,"[KC] Holonet News 2");
+    SOUND_FX(EPF_Radio3,"[KC] Holonet News 3");
+    SOUND_FX(EPF_PresidenteSpeech,"[KC] El Presidente Speech");
+    SOUND_FX(TDI_Theme,"[KC] Total Drama Island Theme");
 };
--- addons/music/script_macros.hpp
+++ addons/music/script_macros.hpp
@@ -3,18 +3,18 @@
 #define SOUND_FX_NAME(var1) DOUBLES(SOUND_NAME(var1),SFX)
 #define MUSIC_NAME(var1) TRIPLES(Prefix,Music,var1)
 
-#define SOUND(var1,var2,var3,var4) class SOUND_NAME(var1) \
+#define SOUND(var1,var2,var3) class SOUND_NAME(var1) \
 { \
     name = var2; \
-    sound[] = {QPATHTOF(data\ambience\var3), "db+10", 1}; \
-    titles[] = {2, var4}; \
+    sound[] = {QPATHTOF(data\ambience\var1##.ogg), "db+10", 1}; \
+    titles[] = {2, var3}; \
 }
 
-#define SOUND_FX(var1,var2,var3) class SOUND_FX_NAME(var1) \
+#define SOUND_FX(var1,var2) class SOUND_FX_NAME(var1) \
 { \
     name = var2; \
     sounds[] = {"sound0"}; \
-    sound0[] = {QPATHTOF(data\ambience\var3), "db+10", 1, 500, 1, 2, 3, 4}; \
+    sound0[] = {QPATHTOF(data\ambience\var1##.ogg), "db+10", 1, 500, 1, 2, 3, 4}; \
     empty[] = {"", 0, 0, 0, 0, 0, 0, 0}; \
 }
 
