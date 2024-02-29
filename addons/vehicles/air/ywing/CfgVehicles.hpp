988bd97de53a173545cc04367dc3c6559583e2ce
DartRuffian <skulldude327@gmail.com>
2024-02-29 13:00:38 -0600

    Gave Y-Wing most pylon weapons
    Gave Y-Wing most pylon weapons for testing purposes.

--- addons/vehicles/air/ywing/CfgVehicles.hpp
+++ addons/vehicles/air/ywing/CfgVehicles.hpp
@@ -6,6 +6,13 @@ class CfgVehicles {
         class Turrets {
             class BubbleGun;
         };
+        class Components: Components {
+            class TransportPylonsComponent {
+                class Pylons {
+                    class pylons1;
+                };
+            };
+        };
         class PilotCamera;
     };
     class CLASS(BTLB_YWing): BTL_Base {
@@ -69,6 +76,57 @@ class CfgVehicles {
         class Turrets: Turrets {
             class BubbleGun: BubbleGun {
                 gunnerName = "Gunner";
+            };
+        };
+
+        class Components: Components
+        {
+            class TransportPylonsComponent: TransportPylonsComponent
+            {
+                uiPicture = QPATHTOF(air\ywing\data\ui\BTLB_YWing_pylons_ca.paa);
+
+                class Pylons: Pylons
+                {
+                    class pylons1: pylons1
+                    {
+                        // attachment = "PylonRack_Missile_AMRAAM_D_x1";
+                        hardpoints[] = {"SCALPEL_1RND", "B_ASRAAM", "B_AMRAAM_D", "B_AMRAAM_D_RAIL", "DAR", "DAGR", "B_GBU12", "B_AGM65_RAIL", "20MM_TWIN_CANNON", "B_MISSILE_PYLON", "B_BOMB_PYLON", "ARC_EMP_RAIL", "B_SDB_QUAD_RAIL", "B_BIM9X_RAIL", "B_AGM65_DUAL_RAIL", "B_HARM_RAIL", "ARC_AGM_PYLON"};
+                        maxweight = 5000;
+                        uiPosition[] = {0.15, 0.25};
+                    };
+                    class pylons2: pylons1
+                    {
+                        mirroredMissilePos = 1;
+                        uiPosition[] = {0.5, 0.25};
+                    };
+                    class pylons3: pylons1
+                    {
+                        uiPosition[] = {0.1, 0.35};
+                    };
+                    class pylons4: pylons3
+                    {
+                        mirroredMissilePos = 3;
+                        uiPosition[] = {0.55, 0.35};
+                    };
+                    class pylons5: pylons1
+                    {
+                        uiPosition[] = {0.05, 0.45};
+                    };
+                    class pylons6: pylons5
+                    {
+                        mirroredMissilePos = 3;
+                        uiPosition[] = {0.6, 0.45};
+                    };
+                    class pylons7: pylons1
+                    {
+                        uiPosition[] = {0.2, 0.4};
+                    };
+                    class pylons8: pylons7
+                    {
+                        mirroredMissilePos = 7;
+                        uiPosition[] = {0.45, 0.4};
+                    };
+                };
             };
         };
 
