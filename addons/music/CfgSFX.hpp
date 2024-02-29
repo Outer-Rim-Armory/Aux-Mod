603f4952b7912d7e6a8cb48e789569cdffd5c179
DartRuffian <skulldude327@gmail.com>
2024-02-29 17:43:14 -0600

    Can give radius of sound

--- addons/music/CfgSFX.hpp
+++ addons/music/CfgSFX.hpp
@@ -5,21 +5,22 @@ class CfgSFX
       - This gets other text added to it, Restaurant becomes BNA_KC_Sound_Restaurant_SFX
     * Second value is the name of the sound that gets displayed
       - Should be prefixed with [KC]
+    * Third value is radius that the sound can be heard in meters
     */
-    SOUND_FX(Restaurant,"[KC] Restaurant Ambience");
-    SOUND_FX(Incoming,"[KC] Incoming Alarm");
-    SOUND_FX(Disturbance,"[KC] Disturbance in the forcee");
-    SOUND_FX(JediWarning,"[KC] Jedi Warning");
-    SOUND_FX(ATTE_Alarm,"[KC] AT-TE Alarm");
-    SOUND_FX(DK_Radio,"[KC] DK Radio Chatter");
-    SOUND_FX(CarnageRoar,"[KC] Carnage Roar");
-    SOUND_FX(CarnageRoar2,"[KC] Carnage Roar 2");
-    SOUND_FX(CarnageRoar3,"[KC] Carnage Roar 3");
-    SOUND_FX(CarnageRoar4,"[KC] Carnage Roar 4");
-    SOUND_FX(LetThereBeCarnage,"[KC] Let There Be Carnage");
-    SOUND_FX(EPF_Radio,"[KC] Holonet News");
-    SOUND_FX(EPF_Radio2,"[KC] Holonet News 2");
-    SOUND_FX(EPF_Radio3,"[KC] Holonet News 3");
-    SOUND_FX(EPF_PresidenteSpeech,"[KC] El Presidente Speech");
-    SOUND_FX(TDI_Theme,"[KC] Total Drama Island Theme");
+    SOUND_FX(Restaurant,"[KC] Restaurant Ambience",100);
+    SOUND_FX(Incoming,"[KC] Incoming Alarm",100);
+    SOUND_FX(Disturbance,"[KC] Disturbance in the forcee",100);
+    SOUND_FX(JediWarning,"[KC] Jedi Warning",100);
+    SOUND_FX(ATTE_Alarm,"[KC] AT-TE Alarm",100);
+    SOUND_FX(DK_Radio,"[KC] DK Radio Chatter",100);
+    SOUND_FX(CarnageRoar,"[KC] Carnage Roar",100);
+    SOUND_FX(CarnageRoar2,"[KC] Carnage Roar 2",100);
+    SOUND_FX(CarnageRoar3,"[KC] Carnage Roar 3",100);
+    SOUND_FX(CarnageRoar4,"[KC] Carnage Roar 4",100);
+    SOUND_FX(LetThereBeCarnage,"[KC] Let There Be Carnage",100);
+    SOUND_FX(EPF_Radio,"[KC] Holonet News",100);
+    SOUND_FX(EPF_Radio2,"[KC] Holonet News 2",100);
+    SOUND_FX(EPF_Radio3,"[KC] Holonet News 3",100);
+    SOUND_FX(EPF_PresidenteSpeech,"[KC] El Presidente Speech",100);
+    SOUND_FX(TDI_Theme,"[KC] Total Drama Island Theme",100);
 };
--- addons/music/script_macros.hpp
+++ addons/music/script_macros.hpp
@@ -10,11 +10,11 @@
     titles[] = {2, var3}; \
 }
 
-#define SOUND_FX(var1,var2) class SOUND_FX_NAME(var1) \
+#define SOUND_FX(var1,var2,var3) class SOUND_FX_NAME(var1) \
 { \
     name = var2; \
     sounds[] = {"sound0"}; \
-    sound0[] = {QPATHTOF(data\ambience\var1##.ogg), "db+10", 1, 500, 1, 2, 3, 4}; \
+    sound0[] = {QPATHTOF(data\ambience\var1##.ogg), "db+10", 1, var3, 1, 2, 3, 4}; \
     empty[] = {"", 0, 0, 0, 0, 0, 0, 0}; \
 }
 
