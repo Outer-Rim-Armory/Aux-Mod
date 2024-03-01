46df4a1ea613a799acca6f9bfca99dcb5606d49b
DartRuffian <skulldude327@gmail.com>
2024-02-29 20:51:10 -0600

    Speeder sounds

--- addons/vehicles/land/barc/CfgVehicles.hpp
+++ addons/vehicles/land/barc/CfgVehicles.hpp
@@ -1,7 +1,27 @@
 class CfgVehicles
 {
-    class 3AS_BARC;
-    class CLASS(BARC): 3AS_BARC
+    class BARC_Base;
+    class 3AS_BARC_Base: BARC_Base
+    {
+        // class Sounds
+        // {
+        //     class Engine;
+        //     class EngineThrust_Int;
+        //     class Engine_Int;
+        //     class Engine1_Thrust_ext;
+        //     class Engine2_Thrust_ext;
+        //     class Engine3_Thrust_ext;
+        //     class Engine4_Thrust_ext;
+        //     class Engine1_Thrust_int;
+        //     class Engine2_Thrust_int;
+        //     class Engine3_Thrust_int;
+        //     class Engine4_Thrust_int;
+        //     class Idle_Int;
+        //     class Idle_Ext;
+        //     class IdleThrust_Int;
+        // };
+    };
+    class CLASS(BARC): 3AS_BARC_Base
     {
         SCOPE_PUBLIC;
         author = "Keeli Company Aux Team";
@@ -45,5 +65,80 @@ class CfgVehicles
                 textures[] = {"\3AS\3AS_LightVics\3AS_BARC\data\barc_212_co.paa"};
             };
         };
+
+        soundEngineOnInt[] = {QPATHTOF(sounds\data\audio\barc\Engine_On.wss), 1, 1, 200};
+        soundEngineOnExt[] = {QPATHTOF(sounds\data\audio\barc\Engine_On.wss), 1, 1, 200};
+        soundEngineOffInt[] = {QPATHTOF(sounds\data\audio\barc\Engine_Off.wss), 1, 1, 200};
+        soundEngineOffExt[] = {QPATHTOF(sounds\data\audio\barc\Engine_Off.wss), 1, 1, 200};
+
+        // TODO: WIP
+        /*
+        class Sounds: Sounds
+        {
+            class Engine: Engine
+            {
+                sound[] = {QPATHTOF(sounds\data\audio\barc\Engine_Idle.wss), 0.65, 1, 200};
+            };
+
+            class EngineThrust_Int: EngineThrust_Int
+            {
+                sound[] = {QPATHTOF(sounds\data\audio\barc\Thrust_Medium.wss), 0.75, 1, 200};
+            };
+
+            class Engine_Int: Engine_Int
+            {
+                sound[] = {QPATHTOF(sounds\data\audio\barc\Thrust_Medium.wss), 0.65, 1, 200};
+            };
+
+            class Engine1_Thrust_Ext: Engine1_Thrust_Ext
+            {
+                sound[] = {QPATHTOF(sounds\data\audio\barc\Thrust_Medium.wss), 0.85, 1, 250};
+            };
+            class Engine2_Thrust_Ext: Engine2_Thrust_Ext
+            {
+                sound[] = {QPATHTOF(sounds\data\audio\barc\Thrust_Medium.wss), 1, 1, 300};
+            };
+            class Engine3_Thrust_Ext: Engine3_Thrust_Ext
+            {
+                sound[] = {QPATHTOF(sounds\data\audio\barc\Thrust_Medium.wss), 1, 1, 350};
+            };
+            class Engine4_Thrust_Ext: Engine4_Thrust_Ext
+            {
+                sound[] = {QPATHTOF(sounds\data\audio\barc\Thrust_High.wss), 1, 1, 350};
+            };
+
+            class Engine1_Thrust_Int: Engine1_Thrust_Int
+            {
+                sound[] = {QPATHTOF(sounds\data\audio\barc\Thrust_Medium.wss), 1, 100};
+            };
+            class Engine2_Thrust_Int: Engine2_Thrust_Int
+            {
+                sound[] = {QPATHTOF(sounds\data\audio\barc\Thrust_Medium.wss), 1, 100};
+            };
+            class Engine3_Thrust_Int: Engine3_Thrust_Int
+            {
+                sound[] = {QPATHTOF(sounds\data\audio\barc\Thrust_Medium.wss), 1, 100};
+            };
+            class Engine4_Thrust_Int: Engine4_Thrust_Int
+            {
+                sound[] = {QPATHTOF(sounds\data\audio\barc\Thrust_High.wss), 1, 1, 100};
+            };
+
+            class Idle_Int: Idle_Int
+            {
+                sound[] = {QPATHTOF(sounds\data\audio\barc\Thrust_Medium.wss), 1, 1, 100};
+            };
+
+            class Idle_Ext: Idle_Ext
+            {
+                sound[] = {QPATHTOF(sounds\data\audio\barc\Thrust_Medium.wss), 1, 1, 100};
+            };
+
+            class IdleThrust_Int: IdleThrust_Int
+            {
+                sound[] = {QPATHTOF(sounds\data\audio\barc\Thrust_Medium.wss), 0.64999998, 1};
+            };
+        };
+        */
     };
 };
