211a95e7674b289f9ea18a29db58225f2e26addd
DartRuffian <skulldude327@gmail.com>
2024-03-09 00:13:42 -0600

    Fixed helmets

--- addons/custom_armor/configs/Helmets_P2_Pilot.hpp
+++ addons/custom_armor/configs/Helmets_P2_Pilot.hpp
@@ -1,4 +1,19 @@
 class CLASS(Helmet_Phase2_Pilot_Base);
 P2_PILOT_HELMET_CUSTOM(Bailout);
 P2_PILOT_HELMET_VISOR_CUSTOM(Blue,Bacta);
-P2_PILOT_HELMET_CUSTOM(Mischief);
+
+class CLASS(Helmet_Phase2_Pilot_Mischief): CLASS(Helmet_Phase2_Pilot_Base)
+{
+    displayName = "[KC] AVI P2 Helm ('Mischief')";
+    hiddenSelectionsTextures[] =
+    {
+        QPATHTOF(data\helmets\phase2Pilot\Mischief_camo1_co.paa),
+        QPATHTOF(data\helmets\phase2Pilot\Mischief_camo2_co.paa)
+    };
+    GVAR(isCustom) = TRUE;
+    class XtdGearInfo
+    {
+        model = QCLASS(Helmets_Pilot_Phase2_Custom);
+        camo = "Mischief";
+    };
+};
