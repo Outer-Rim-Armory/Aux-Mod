2e86c7ff97b2dcef962ca0025d32f42890736672
DartRuffian <skulldude327@gmail.com>
2024-03-10 13:17:30 -0500

    Updated Unit P1 helmets

---
+++ addons/factions/gar/script_macros.hpp
@@ -0,0 +1,33 @@
+#define P1_HELMET_UNIT(var1) class CLASS(DOUBLES(Helmet_Phase1,var1)): CLASS(Helmet_Phase1_Base) \
+{ \
+    displayName = QUOTE([##var1##] INF P1 Helm); \
+    hiddenSelectionsTextures[] = \
+    { \
+        QPATHTOF(SUBCOMPONENT\data\helmets\phase1\DOUBLES(var1,camo1_co.paa)), \
+        "\ls_armor_bluefor\helmet\gar\phase1\data\visor_co.paa", \
+        "\ls_armor_bluefor\helmet\gar\phase1\data\illum_co.paa" \
+    }; \
+    EGVAR(custom_armor,isCustom) = TRUE; \
+    class XtdGearInfo \
+    { \
+        model = QCLASS(Helmets_Phase1_Units); \
+        camo = QUOTE(DOUBLES(KC,var1)); \
+    }; \
+}
+
+#define P1_HELMET_UNIT_NAME(var1,var2) class CLASS(TRIPLES(Helmet_Phase1,var1,var2)): CLASS(Helmet_Phase1_Base) \
+{ \
+    displayName = QUOTE([##var1##] INF P1 Helm ('##var2##')); \
+    hiddenSelectionsTextures[] = \
+    { \
+        QPATHTOF(SUBCOMPONENT\data\helmets\phase1\TRIPLES(var1,var2,camo1_co.paa)), \
+        "\ls_armor_bluefor\helmet\gar\phase1\data\visor_co.paa", \
+        "\ls_armor_bluefor\helmet\gar\phase1\data\illum_co.paa" \
+    }; \
+    EGVAR(custom_armor,isCustom) = TRUE; \
+    class XtdGearInfo \
+    { \
+        model = QCLASS(Helmets_Phase1_Units); \
+        camo = QUOTE(TRIPLES(KC,var1,var2)); \
+    }; \
+}
