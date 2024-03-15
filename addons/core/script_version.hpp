6b6738bf765315149e227f07dc19e03eb1a0298b
Keeli <ShakalakaTribe@gmail.com>
2024-03-14 21:45:04 -0700

    .

--- addons/core/script_version.hpp
+++ addons/core/script_version.hpp
@@ -1,4 +1,4 @@
 #define MAJOR 2
-#define MINOR 5
+#define MINOR 6
 #define PATCH 4
 #define BUILD 0
--- addons/music/CfgSFX.hpp
+++ addons/music/CfgSFX.hpp
@@ -7,21 +7,21 @@ class CfgSFX
       - Should be prefixed with [KC]
     * Third value is radius that the sound can be heard in meters
     */
-    SOUND_FX(Restaurant,"[KC] Restaurant Ambience",100);
-    SOUND_FX(Incoming,"[KC] Incoming Alarm",100);
-    SOUND_FX(Disturbance,"[KC] Disturbance in the forcee",100);
+    SOUND_FX(Restaurant,"[KC] Restaurant Ambience",300);
+    SOUND_FX(Incoming,"[KC] Incoming Alarm",400);
+    SOUND_FX(Disturbance,"[KC] Disturbance in the forcee",150);
     SOUND_FX(JediWarning,"[KC] Jedi Warning",100);
-    SOUND_FX(ATTE_Alarm,"[KC] AT-TE Alarm",100);
+    SOUND_FX(ATTE_Alarm,"[KC] AT-TE Alarm",400);
     SOUND_FX(DK_Radio,"[KC] DK Radio Chatter",100);
-    SOUND_FX(CarnageRoar,"[KC] Carnage Roar",100);
-    SOUND_FX(CarnageRoar2,"[KC] Carnage Roar 2",100);
-    SOUND_FX(CarnageRoar3,"[KC] Carnage Roar 3",100);
-    SOUND_FX(CarnageRoar4,"[KC] Carnage Roar 4",100);
-    SOUND_FX(LetThereBeCarnage,"[KC] Let There Be Carnage",100);
+    SOUND_FX(CarnageRoar,"[KC] Carnage Roar",300);
+    SOUND_FX(CarnageRoar2,"[KC] Carnage Roar 2",300);
+    SOUND_FX(CarnageRoar3,"[KC] Carnage Roar 3",300);
+    SOUND_FX(CarnageRoar4,"[KC] Carnage Roar 4",300);
+    SOUND_FX(LetThereBeCarnage,"[KC] Let There Be Carnage",300);
     SOUND_FX(EPF_Radio,"[KC] Holonet News",100);
     SOUND_FX(EPF_Radio2,"[KC] Holonet News 2",100);
     SOUND_FX(EPF_Radio3,"[KC] Holonet News 3",100);
     SOUND_FX(EPF_PresidenteSpeech,"[KC] El Presidente Speech",100);
     SOUND_FX(TDI_Theme,"[KC] Total Drama Island Theme",100);
-    SOUND_FX(PokeVillage,"[KC] Poke Village Theme",150);
+    SOUND_FX(PokeVillage,"[KC] Poke Village Theme",300);
 };
