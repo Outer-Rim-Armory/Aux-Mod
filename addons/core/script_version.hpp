9da1e27360ecf065f51d17d5c3901f60457e3571
Keeli <ShakalakaTribe@gmail.com>
2024-03-14 22:19:09 -0700

    Keeli fixing mod :D

--- addons/core/script_version.hpp
+++ addons/core/script_version.hpp
@@ -1,10 +1,4 @@
 #define MAJOR 2
-<<<<<<< HEAD
-<<<<<<< HEAD
-#define MINOR 8
-=======
-=======
->>>>>>> 1c08be284be9e2f36ee956403f95ccfba8cef6f8
 #define MINOR 2
 #define PATCH 4
-#define BUILD 0
+#define BUILD 2
