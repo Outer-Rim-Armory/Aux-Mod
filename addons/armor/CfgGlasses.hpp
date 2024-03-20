bb72d4b978464148426ebc9e10781423811abce3
DartRuffian <skulldude327@gmail.com>
2024-03-19 21:58:20 -0500

    Pauldron facewear

--- addons/armor/CfgGlasses.hpp
+++ addons/armor/CfgGlasses.hpp
@@ -1,8 +1,6 @@
-class CfgGlasses
-{
+class CfgGlasses {
     class ls_combatGoggles_base;
-    class CLASS(Facewear_ARF_Flaps): ls_combatGoggles_base
-    {
+    class CLASS(Facewear_ARF_Flaps): ls_combatGoggles_base {
         SCOPE_PUBLIC;
         author = AUTHOR;
 
@@ -18,9 +16,19 @@ class CfgGlasses
         };
         picture = QPATHTOF(data\ui\Facewear_ARF_Flaps_ca.paa);
 
-        ACE_DustPath = "\z\ace\addons\goggles\textures\fx\dust\%1.paa";
         ACE_Overlay = "\SWLB_equipment\facewears\data\P2_HUD_ca.paa";
         ACE_OverlayCracked = "\SWLB_equipment\facewears\data\P2_HUD_cracked_ca.paa";
         ACE_OverlayDirt = "\A3\Ui_f\data\IGUI\RscTitles\HealthTextures\dust_upper_ca.paa";
+        ACE_DustPath = "\z\ace\addons\goggles\textures\fx\dust\%1.paa";
+    };
+
+    class CLASS(Facewear_phase2_Pauldron): CLASS(Facewear_ARF_Flaps) {
+        displayName = "[KC] Pauldron";
+        model = "\SWLB_clones\SWLB_clone_lieutenant_armor.p3d";
+        hiddenSelections[] = {"camo1"};
+        hiddenSelectionsTextures[] = {
+            QPATHTOF(data\vests\infantry\officer\Officer_camo1_co.paa) // Pauldron
+        };
+        picture = "\SWLB_clones\data\ui\icon_SWLB_clone_officer_armor_ca.paa";
     };
 };
