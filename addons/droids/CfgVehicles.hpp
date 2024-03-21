98785c6fc7c85838f0e939aef1d289d977467b6c
DartRuffian <skulldude327@gmail.com>
2024-03-20 23:54:51 -0500

    B2 Revival

--- addons/droids/CfgVehicles.hpp
+++ addons/droids/CfgVehicles.hpp
@@ -1,5 +1,31 @@
 class CfgVehicles {
     class CLASS(CIS_Unit_Droid_Base);
     class CLASS(CIS_Unit_Droid_B2): CLASS(CIS_Unit_Droid_Base) {
+        GVAR(reviveUnit) = QCLASS(CIS_Unit_Droid_B2_Revived);
+        GVAR(reviveChance) = 1;
+    };
+
+    class CLASS(CIS_Unit_Droid_B2_Revived): CLASS(CIS_Unit_Droid_B2) {
+        SCOPE_HIDDEN;
+        GVAR(reviveUnit) = "";
+        GVAR(reviveChance) = 0;
+
+        armor = 1;
+        armorStructural = 1;
+        // Textures of original alive unit will be applied
+    };
+
+    class CLASS(CIS_Unit_Droid_BX): CLASS(CIS_Unit_Droid_Base) {
+        GVAR(reviveUnit) = QCLASS(CIS_Unit_Droid_BX_Revived);
+        GVAR(reviveChance) = 1;
+    };
+
+    class CLASS(CIS_Unit_Droid_BX_Revived): CLASS(CIS_Unit_Droid_BX) {
+        SCOPE_HIDDEN;
+        GVAR(reviveUnit) = "";
+        GVAR(reviveChance) = 0;
+
+        armor = 1;
+        armorStructural = 1;
     };
 };
