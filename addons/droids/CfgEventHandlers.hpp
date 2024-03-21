0140eb461b632bcd631fc9080eaf9835671bdb25
DartRuffian <skulldude327@gmail.com>
2024-03-20 23:54:51 -0500

    B2 Revival

--- addons/droids/CfgEventHandlers.hpp
+++ addons/droids/CfgEventHandlers.hpp
@@ -16,9 +16,28 @@ class Extended_InitPost_EventHandlers {
             init = QUOTE(_this#0 call FUNC(initB2));
         };
     };
+    class CLASS(CIS_Unit_Droid_B2_Revived) {
+        class GVAR(init) {
+            init = "";
+        };
+    };
+
     class CLASS(CIS_Unit_Droid_B1_Prototype) {
         class GVAR(init) {
             init = QUOTE(_this#0 call FUNC(initB1Prototype));
+        };
+    };
+};
+
+class Extended_Killed_EventHandlers {
+    class CLASS(CIS_Unit_Droid_Base) {
+        class GVAR(revive) {
+            killed = QUOTE(_this call FUNC(revive));
+        };
+    };
+    class CLASS(CIS_Unit_Droid_B2) {
+        class GVAR(revive) {
+            killed = QUOTE([ARR_2(_this#0,'b2')] call FUNC(revive));
         };
     };
 };
--- addons/droids/CfgVehicles.hpp
+++ addons/droids/CfgVehicles.hpp
@@ -5,5 +5,32 @@ class CfgVehicles {
         respawnWeapons[] = {"WBK_B2_NormalMod"};
         magazines[] = {ITEM_10("SWLW_ZH73_Mag")};
         respawnMagazines[] = {ITEM_10("SWLW_ZH73_Mag")};
+
+        GVAR(reviveUnit) = QCLASS(CIS_Unit_Droid_B2_Revived);
+        GVAR(reviveChance) = 1;
+    };
+
+    class CLASS(CIS_Unit_Droid_B2_Revived): CLASS(CIS_Unit_Droid_B2) {
+        SCOPE_HIDDEN;
+        GVAR(reviveUnit) = "";
+        GVAR(reviveChance) = 0;
+
+        armor = 1;
+        armorStructural = 1;
+        // Textures of original alive unit will be applied
+    };
+
+    class CLASS(CIS_Unit_Droid_BX): CLASS(CIS_Unit_Droid_Base) {
+        GVAR(reviveUnit) = QCLASS(CIS_Unit_Droid_BX_Revived);
+        GVAR(reviveChance) = 1;
+    };
+
+    class CLASS(CIS_Unit_Droid_BX_Revived): CLASS(CIS_Unit_Droid_BX) {
+        SCOPE_HIDDEN;
+        GVAR(reviveUnit) = "";
+        GVAR(reviveChance) = 0;
+
+        armor = 1;
+        armorStructural = 1;
     };
 };
