a9452a9488ff4abcf5dd4e8e07ca7f4c4c518149
DartRuffian <skulldude327@gmail.com>
2024-03-21 22:29:11 -0500

    Re-added bench seats to Mk2 LAAT/i

--- addons/vehicles/air/laati/CfgVehicles.hpp
+++ addons/vehicles/air/laati/CfgVehicles.hpp
@@ -303,6 +303,10 @@ class CfgVehicles {
             class CoPilot: CoPilot {};
             class CargoTurret_01: CargoTurret_01 {};
             class CargoTurret_02: CargoTurret_02 {};
+            class CargoTurret_03: CargoTurret_03 {};
+            class CargoTurret_04: CargoTurret_04 {};
+            class CargoTurret_05: CargoTurret_05 {};
+            class CargoTurret_06: CargoTurret_06 {};
         };
     };
     class CLASS(LAATi_MK2_Lamps): CLASS(LAATi_MK2) {
