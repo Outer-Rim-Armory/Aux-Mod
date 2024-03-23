e90a993df822cab6c18dc3f6feb8abaea9076ab8
DartRuffian <skulldude327@gmail.com>
2024-03-23 13:55:40 -0500

    Custom sound

--- addons/weapons/b2blaster/CfgSoundShaders.hpp
+++ addons/weapons/b2blaster/CfgSoundShaders.hpp
@@ -2,7 +2,7 @@ class CfgSoundShaders {
     class CLASS(SoundShader_WeaponShot_Base);
     class CLASS(SoundShader_B2Blaster_Shot): CLASS(SoundShader_WeaponShot_Base) {
         samples[] = {
-            {"\SWLW_droids\smgs\e5\sounds\e5.wss", 1, 1}
+            {QPATHTOF(b2Blaster\data\audio\Fire1.wss), 1, 1}
         };
     };
 
