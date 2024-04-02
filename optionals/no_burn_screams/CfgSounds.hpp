4999dde01396ef4eb1b12c49f0f136aafa648de2
DartRuffian <skulldude327@gmail.com>
2024-04-02 10:07:25 -0500

    Formatting

--- optionals/no_burn_screams/CfgSounds.hpp
+++ optionals/no_burn_screams/CfgSounds.hpp
@@ -1,19 +1,18 @@
-class CfgSounds
-{
-    class hit_by_flame_1 {sound[] = {BLANK_SOUND};};
-    class hit_by_flame_2 {sound[] = {BLANK_SOUND};};
-    class hit_by_flame_3 {sound[] = {BLANK_SOUND};};
-    class hit_by_flame_4 {sound[] = {BLANK_SOUND};};
-    class burnttodeath_distant1 {sound[] = {BLANK_SOUND};};
-    class burnttodeath_distant2 {sound[] = {BLANK_SOUND};};
-    class burnttodeath_distant3 {sound[] = {BLANK_SOUND};};
-    class burnttodeath_distant4 {sound[] = {BLANK_SOUND};};
-    class burnttodeath_distant5 {sound[] = {BLANK_SOUND};};
-    class burnttodeath_distant6 {sound[] = {BLANK_SOUND};};
-    class burnttodeath1 {sound[] = {BLANK_SOUND};};
-    class burnttodeath2 {sound[] = {BLANK_SOUND};};
-    class burnttodeath3 {sound[] = {BLANK_SOUND};};
-    class burnttodeath4 {sound[] = {BLANK_SOUND};};
-    class burnttodeath5 {sound[] = {BLANK_SOUND};};
-    class burnttodeath6 {sound[] = {BLANK_SOUND};};
+class CfgSounds {
+    class hit_by_flame_1 { sound[] = {BLANK_SOUND}; };
+    class hit_by_flame_2 { sound[] = {BLANK_SOUND}; };
+    class hit_by_flame_3 { sound[] = {BLANK_SOUND}; };
+    class hit_by_flame_4 { sound[] = {BLANK_SOUND}; };
+    class burnttodeath_distant1 { sound[] = {BLANK_SOUND}; };
+    class burnttodeath_distant2 { sound[] = {BLANK_SOUND}; };
+    class burnttodeath_distant3 { sound[] = {BLANK_SOUND}; };
+    class burnttodeath_distant4 { sound[] = {BLANK_SOUND}; };
+    class burnttodeath_distant5 { sound[] = {BLANK_SOUND}; };
+    class burnttodeath_distant6 { sound[] = {BLANK_SOUND}; };
+    class burnttodeath1 { sound[] = {BLANK_SOUND}; };
+    class burnttodeath2 { sound[] = {BLANK_SOUND}; };
+    class burnttodeath3 { sound[] = {BLANK_SOUND}; };
+    class burnttodeath4 { sound[] = {BLANK_SOUND}; };
+    class burnttodeath5 { sound[] = {BLANK_SOUND}; };
+    class burnttodeath6 { sound[] = {BLANK_SOUND}; };
 };
--- optionals/no_burn_screams/script_component.hpp
+++ optionals/no_burn_screams/script_component.hpp
@@ -1,6 +1,9 @@
 #define COMPONENT no_burn_screams
 #define COMPONENT_BEAUTIFIED No Burn 'Em Screams
-
 #include "\ORA\BNA_KC\addons\core\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
 #include "\ORA\BNA_KC\addons\core\script_macros.hpp"
-#include "\ORA\BNA_KC\addons\no_burn_screams\script_macros.hpp"
+#include "script_macros.hpp"
