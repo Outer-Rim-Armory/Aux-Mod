d906f5ad6c6a6717e48c98fe85c5cb42e74a333d
DartRuffian <skulldude327@gmail.com>
2024-04-03 19:21:19 -0500

    Memes addon

---
+++ addons/memes/script_component.hpp
@@ -0,0 +1,8 @@
+#define COMPONENT memes
+#define COMPONENT_BEAUTIFIED Memes
+#include "\ORA\BNA_KC\addons\core\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+
+#include "\ORA\BNA_KC\addons\core\script_macros.hpp"
