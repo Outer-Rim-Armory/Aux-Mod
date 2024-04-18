500df385c87185abc403a94d917045de2b1805fc
DartRuffian <skulldude327@gmail.com>
2024-04-18 00:38:45 -0500

    Renamed Keeli backpack

--- addons/custom_armor/CfgVehicles.hpp
+++ addons/custom_armor/CfgVehicles.hpp
@@ -2,7 +2,7 @@ class CfgVehicles {
     #include "configs\Units.hpp"
 
     class CLASS(cloneBackpack_standard);
-    class CLASS(Backpack_Keeli): CLASS(cloneBackpack_standard) {
+    class CLASS(cloneBackpack_Keeli): CLASS(cloneBackpack_standard) {
         displayName = "[KC] Clone Trooper Backpack ('Keeli')";
         hiddenSelectionsTextures[] = {
             QPATHTOF(data\backpacks\Keeli_camo1_co.paa),
