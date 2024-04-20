adf993bfc269fca70a66f3692659da4f13de438b
DartRuffian <skulldude327@gmail.com>
2024-04-19 20:41:33 -0500

    Increased Westar-M5 magazine capacity

--- addons/weapons/westar_m5/CfgWeapons.hpp
+++ addons/weapons/westar_m5/CfgWeapons.hpp
@@ -35,7 +35,7 @@ class CfgWeapons {
 
         modes[] = {"Single", "Burst", "FullAuto"};
         muzzles[] = {"this"};
-        magazines[] = {QCLASS(Mag_60Rnd_WestarM5), "Aux12thFleet_Mag_WestarM5"};
+        magazines[] = {QCLASS(Mag_99Rnd_WestarM5), "Aux12thFleet_Mag_WestarM5"};
         magazineWell[] = {};
 
         canShootInWater = TRUE;
