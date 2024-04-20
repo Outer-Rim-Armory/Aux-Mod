e4d6f8ffac4183917a83534fc6d2e4fe6380362b
DartRuffian <skulldude327@gmail.com>
2024-04-19 22:45:48 -0500

    Added comment

--- addons/weapons/dc17/CfgAmmo.hpp
+++ addons/weapons/dc17/CfgAmmo.hpp
@@ -3,7 +3,7 @@ class CfgAmmo {
     class CLASS(Bullet_Plasma_DC17Dual_Blue): CLASS(Bullet_PlasmaPistol_Blue) {
         caliber = 2.5;
         cost = 3;
-        hit = 40;
+        hit = 40; // ! NEEDS CHANGING, 2/3 as strong as highest damage vanilla round
         typicalspeed = 800;
 
         ACE_ammoTempMuzzleVelocityShifts[] = {-26.55, -25.47, -22.85, -20.12, -16.98, -12.8, -7.64, -1.53, 5.96, 15.17, 26.19};
