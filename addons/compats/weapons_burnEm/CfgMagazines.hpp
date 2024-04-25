ad64a814e8d94b416b698b91e6cb2e3dd4792b37
DartRuffian <skulldude327@gmail.com>
2024-04-25 12:35:57 -0500

    Moved B2 flamer to main B2 blaster addon
    Updated inheritance for B2Blaster_flamer, uses base B2 blaster.
    Burn 'Em compat

--- addons/compats/weapons_burnEm/CfgMagazines.hpp
+++ addons/compats/weapons_burnEm/CfgMagazines.hpp
@@ -3,4 +3,8 @@ class CfgMagazines {
     class CLASS(Mag_150Rnd_X42): CLASS(Mag_Base) {
         ammo = "Flamethrower_Fuel";
     };
+
+    class CLASS(Mag_150rnd_B2Blaster_flamer): CLASS(Mag_Base) {
+        ammo = "Flamethrower_Fuel";
+    };
 };
--- addons/weapons/b2blaster_flamer/script_component.hpp
+++
@@ -1,4 +0,0 @@
-#define SUBCOMPONENT b2blaster_flamer
-#define SUBCOMPONENT_BEAUTIFIED B2 Blaster (Flamer)
-
-#include "..\script_component.hpp"
