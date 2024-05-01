b87805efd875afc2bcfcfb9d856ebbac7cc458cf
DartRuffian <skulldude327@gmail.com>
2024-05-01 00:04:01 -0500

    Updated textures

--- addons/vehicles/land/utat/CfgVehicles.hpp
+++ addons/vehicles/land/utat/CfgVehicles.hpp
@@ -3,10 +3,9 @@ class CfgVehicles {
     class CLASS(UTAT): 3AS_UTAT {
         SCOPE_PUBLIC;
 
-
-        faction = QCLASS(Faction_KC);
+        faction = QFACTION(KC);
         editorSubcategory = QEDSUBCAT(Tanks);
-        // editorPreview = EEDITOR_PREVIEW(vehicles\land\utat,UTAT);
+        editorPreview = EEDITOR_PREVIEW(vehicles\land\utat,UTAT);
 
         displayName = "UT-AT";
         crew = QCLASS(Unit_Phase2_Tanker_CT);
@@ -14,8 +13,29 @@ class CfgVehicles {
 
         hiddenSelections[] = {"camo", "camo1"}; // 3AS bug, missing comma in original config
         hiddenSelectionsTextures[] = {
-            "\3AS\3AS_UTAT\data\utat_primary_co.paa",
-            "\3AS\3AS_UTAT\data\utat_primary_co.paa"
+            QPATHTOF(land\utat\data\textures\KeeliCompany\Primary_co.paa),
+            QPATHTOF(land\utat\data\textures\KeeliCompany\Secondary_co.paa)
+        };
+
+        textureList[] = {"Standard", 0, "KeeliCompany", 1};
+        class TextureSources {
+            class Standard {
+                author = "3rd Army Studios";
+                displayName = "Standard";
+                factions[] = {};
+                textures[] = {
+                    "\3AS\3AS_UTAT\data\utat_primary_co.paa",
+                    "\3AS\3AS_UTAT\data\utat_primary_co.paa"
+                };
+            };
+            class KeeliCompany: Standard {
+                author = AUTHOR;
+                displayName = "Keeli Company";
+                textures[] = {
+                    QPATHTOF(land\utat\data\textures\KeeliCompany\Primary_co.paa),
+                    QPATHTOF(land\utat\data\textures\KeeliCompany\Secondary_co.paa)
+                };
+            };
         };
     };
 };
