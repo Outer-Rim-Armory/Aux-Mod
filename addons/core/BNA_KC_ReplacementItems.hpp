afa664e95217ae2fce544c2b0923359e38191c60
DartRuffian <skulldude327@gmail.com>
2024-05-10 03:08:12 -0500

    Updated NVGs

--- addons/core/BNA_KC_ReplacementItems.hpp
+++ addons/core/BNA_KC_ReplacementItems.hpp
@@ -1,2 +1,13 @@
 class GVARMAIN(replacementItems) {
+    CLASS(NVG_Chip) = QCLASS(cloneNvg_chip);
+    CLASS(NVG_phase1_Visor) = QCLASS(cloneNvg_phase1_visor);
+    CLASS(NVG_phase2_Visor) = QCLASS(cloneNvg_phase2_visor);
+    CLASS(NVG_phase2_Visor_v2) = QCLASS(cloneNvg_phase2_visor_v2);
+    CLASS(NVG_phase1_Rangefinder) = QCLASS(cloneNvg_phase1_rangefinder);
+    CLASS(NVG_phase2_Rangefinder) = QCLASS(cloneNvg_phase2_rangefinder);
+    CLASS(NVG_phase1_Officer) = QCLASS(cloneNvg_phase1_officerVisor);
+    CLASS(NVG_phase2_Officer) = QCLASS(cloneNvg_phase2_officerVisor);
+    CLASS(NVG_Engineer) = QCLASS(cloneNvg_engineer);
+    CLASS(NVG_Commando_Visor) = QCLASS(cloneNvg_commandoVisor);
+    CLASS(NVG_Commando_Antenna) = QCLASS(cloneNvg_commandoAntenna);
 };
