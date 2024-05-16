78c86970e1ac325a7de95e8ed6086b17853361ec
DartRuffian <skulldude327@gmail.com>
2024-05-15 23:36:10 -0500

    Removed old CfgFunctions

--- addons/jetpacks/CfgFunctions.hpp
+++ addons/jetpacks/CfgFunctions.hpp
@@ -1,15 +1,6 @@
 // TODO: Move to PREP
 class CfgFunctions {
     class BNA_KC_Jetpacks {
-        class Main {
-            file = "\ORA\BNA_KC\addons\jetpacks\Data\Functions\Main";
-            class jetpack {};
-            class frameHandler {};
-            class effectHandler {};
-            class soundHandler {};
-            class fuelHandler {};
-        };
-
         class Fuel {
             file = "\ORA\BNA_KC\addons\jetpacks\Data\Functions\Refuel";
             class refuelFromBody {};
