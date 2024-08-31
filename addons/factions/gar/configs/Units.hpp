992fcf02e4b82e86a39f06e05320324f24597d0a
Dexus-Omega <118584541+Dexus-Omega@users.noreply.github.com>
2024-08-30 23:51:17 -0400

    testing 2

--- addons/factions/gar/configs/Units.hpp
+++ addons/factions/gar/configs/Units.hpp
@@ -250,7 +250,7 @@ class CLASS(Unit_91st_Ponds): CLASS(Unit_Phase2_Base) {
     displayName = "Commander Ponds";
     faction = QFACTION(GAR);
     editorSubcategory = QEDSUBCAT(91st);
-    editorPreview = EDITOR_PREVIEW(Unit_91st_Ponds);
+    editorPreview = EDITOR_PREVIEW();
 
     uniformClass = QCLASS(Uniform_91st_Ponds);
     hiddenSelectionsTextures[] = {
@@ -271,7 +271,7 @@ class CLASS(Unit_91st): CLASS(Unit_Phase2_Base) {
     displayName = "(Clone Trooper)";
     faction = QFACTION(GAR);
     editorSubcategory = QEDSUBCAT(91st);
-    editorPreview = EDITOR_PREVIEW(Unit_91st_Ponds);
+    editorPreview = EDITOR_PREVIEW();
 
     uniformClass = QCLASS(Uniform_91st);
     hiddenSelectionsTextures[] = {
@@ -292,7 +292,7 @@ class CLASS(Unit_91st): CLASS(Unit_Phase2_Base) {
     displayName = "(Clone Trooper)";
     faction = QFACTION(GAR);
     editorSubcategory = QEDSUBCAT(327th);
-    editorPreview = EDITOR_PREVIEW(Unit_91st_Ponds);
+    editorPreview = EDITOR_PREVIEW();
 
     uniformClass = QCLASS(Uniform_327th);
     hiddenSelectionsTextures[] = {
