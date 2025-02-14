8bbf9fd8bb70c885d5c55d08d17cacb90454a65d
Dexus-Omega <118584541+Dexus-Omega@users.noreply.github.com>
2025-02-13 19:17:58 -0500

    Aux Patch Update 2.44.7.0

--- addons/vehicles/land/hermitaur/CfgVehicles.hpp
+++ addons/vehicles/land/hermitaur/CfgVehicles.hpp
@@ -40,10 +40,65 @@ class CfgVehicles {
 
         hiddenSelections[] = {"Camo","camo2","camo3"};
         hiddenSelectionsTextures[] = {
-            "3as\3AS_Rebel_Armor\PX10_Cav\Textures\PX10CAV_Republic_co.paa",
+            QPATHTOF(land\hermitaur\data\textures\KeeliCompany\PX10CAV_KC_co.paa),
             "3as\3as_rebel_armor\px10_cav\textures\px10cavintback_co.paa",
             "3as\3as_rebel_armor\px10_cav\textures\px10cavintfront_co.paa"
         };
+
+        textureList[] = {"Standard", 0, "KeeliCompany", 1, "BrownCamo", 0, "GreyCamo", 0, "Medic", 0, "Imperial", 0};
+        class TextureSources {
+            class Standard {
+                author = AUTHOR;
+                displayName = "Standard";
+                factions[] = {};
+                textures[] = {
+                    "3as\3AS_Rebel_Armor\PX10_Cav\Textures\PX10CAV_Republic_co.paa",
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintback_co.paa",
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintfront_co.paa"
+                };
+            };
+            class KeeliCompany: Standard {
+                displayName = "Keeli Company";
+                textures[] = {
+                    QPATHTOF(land\hermitaur\data\textures\KeeliCompany\PX10CAV_KC_co.paa),
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintback_co.paa",
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintfront_co.paa"
+                };
+            };
+            class BrownCamo: KeeliCompany {
+                displayName = "Brown Camo";
+                textures[] = {
+                    QPATHTOF(land\hermitaur\data\textures\BrownCamo\PX10CAV_KCBROWNCAMO_CO.paa),
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintback_co.paa",
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintfront_co.paa"
+                };
+            };
+            class GreyCamo: KeeliCompany {
+                displayName = "Grey Camo";
+                textures[] = {
+                    QPATHTOF(land\hermitaur\data\textures\GreyCamo\PX10CAV_KCGREYCAMO_CO.paa),
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintback_co.paa",
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintfront_co.paa"
+                };
+            };
+            class Medic: KeeliCompany {
+                displayName = "Medic";
+                textures[] = {
+                    QPATHTOF(land\hermitaur\data\textures\Medic\PX10CAV_KCMEDIC_CO.paa),
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintback_co.paa",
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintfront_co.paa"
+                };
+            };
+            class Imperial: KeeliCompany {
+                displayName = "Imperial";
+                textures[] = {
+                    QPATHTOF(land\hermitaur\data\textures\Imperial\PX10CAV_KCImp_CO.paa),
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintback_co.paa",
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintfront_co.paa"
+                };
+            };
+        };
+
         model = "3as\3AS_Rebel_Armor\PX10_Cav\model\3AS_PX10.p3d";
         picture = "3as\3AS_Rebel_Armor\PX10_Cav\data\ui\px10cav_side_ca.paa";
         icon = "3as\3AS_Rebel_Armor\PX10_Cav\data\ui\px10cav_top_ca.paa";
@@ -109,6 +164,59 @@ class CfgVehicles {
             "3as\3as_rebel_armor\px10_cav\textures\px10fuel_Republic_co.paa",
             "3as\3as_rebel_armor\px10_cav\textures\px10crane_Republic_co.paa"
         };
+        textureList[] = {"Standard", 0, "KeeliCompany", 0, "BrownCamo", 0, "GreyCamo", 1, "Medic", 0, "Imperial", 0};
+        class TextureSources {
+            class Standard {
+                author = AUTHOR;
+                displayName = "Standard";
+                factions[] = {};
+                textures[] = {
+                    "3as\3AS_Rebel_Armor\PX10_Cav\Textures\PX10CAV_Republic_co.paa",
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintback_co.paa",
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintfront_co.paa"
+                };
+            };
+            class KeeliCompany: Standard {
+                displayName = "Keeli Company";
+                textures[] = {
+                    QPATHTOF(land\hermitaur\data\textures\KeeliCompany\PX10CAV_KC_co.paa),
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintback_co.paa",
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintfront_co.paa"
+                };
+            };
+            class BrownCamo: KeeliCompany {
+                displayName = "Brown Camo";
+                textures[] = {
+                    QPATHTOF(land\hermitaur\data\textures\BrownCamo\PX10CAV_KCBROWNCAMO_CO.paa),
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintback_co.paa",
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintfront_co.paa"
+                };
+            };
+            class GreyCamo: KeeliCompany {
+                displayName = "Grey Camo";
+                textures[] = {
+                    QPATHTOF(land\hermitaur\data\textures\GreyCamo\PX10CAV_KCGREYCAMO_CO.paa),
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintback_co.paa",
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintfront_co.paa"
+                };
+            };
+            class Medic: KeeliCompany {
+                displayName = "Medic";
+                textures[] = {
+                    QPATHTOF(land\hermitaur\data\textures\Medic\PX10CAV_KCMEDIC_CO.paa),
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintback_co.paa",
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintfront_co.paa"
+                };
+            };
+            class Imperial: KeeliCompany {
+                displayName = "Imperial";
+                textures[] = {
+                    QPATHTOF(land\hermitaur\data\textures\Imperial\PX10CAV_KCImp_CO.paa),
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintback_co.paa",
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintfront_co.paa"
+                };
+            };
+        };
         model = "3as\3AS_Rebel_Armor\PX10_Cav\model\3AS_PX10.p3d";
         picture = "3as\3AS_Rebel_Armor\PX10_Cav\data\ui\px10cav_side_ca.paa";
         icon = "3as\3AS_Rebel_Armor\PX10_Cav\data\ui\px10cav_top_ca.paa";
@@ -142,6 +250,59 @@ class CfgVehicles {
             "3as\3as_rebel_armor\px10_cav\textures\px10cavintfront_co.paa",
             "3as\3as_rebel_armor\px10_cav\textures\px10fuel_Republic_co.paa",
             "3as\3as_rebel_armor\px10_cav\textures\px10crane_Republic_co.paa"
+        };
+        textureList[] = {"Standard", 0, "KeeliCompany", 0, "BrownCamo", 0, "GreyCamo", 0, "Medic", 1, "Imperial", 0};
+        class TextureSources {
+            class Standard {
+                author = AUTHOR;
+                displayName = "Standard";
+                factions[] = {};
+                textures[] = {
+                    "3as\3AS_Rebel_Armor\PX10_Cav\Textures\PX10CAV_Republic_co.paa",
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintback_co.paa",
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintfront_co.paa"
+                };
+            };
+            class KeeliCompany: Standard {
+                displayName = "Keeli Company";
+                textures[] = {
+                    QPATHTOF(land\hermitaur\data\textures\KeeliCompany\PX10CAV_KC_co.paa),
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintback_co.paa",
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintfront_co.paa"
+                };
+            };
+            class BrownCamo: KeeliCompany {
+                displayName = "Brown Camo";
+                textures[] = {
+                    QPATHTOF(land\hermitaur\data\textures\BrownCamo\PX10CAV_KCBROWNCAMO_CO.paa),
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintback_co.paa",
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintfront_co.paa"
+                };
+            };
+            class GreyCamo: KeeliCompany {
+                displayName = "Grey Camo";
+                textures[] = {
+                    QPATHTOF(land\hermitaur\data\textures\GreyCamo\PX10CAV_KCGREYCAMO_CO.paa),
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintback_co.paa",
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintfront_co.paa"
+                };
+            };
+            class Medic: KeeliCompany {
+                displayName = "Medic";
+                textures[] = {
+                    QPATHTOF(land\hermitaur\data\textures\Medic\PX10CAV_KCMEDIC_CO.paa),
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintback_co.paa",
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintfront_co.paa"
+                };
+            };
+            class Imperial: KeeliCompany {
+                displayName = "Imperial";
+                textures[] = {
+                    QPATHTOF(land\hermitaur\data\textures\Imperial\PX10CAV_KCImp_CO.paa),
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintback_co.paa",
+                    "3as\3as_rebel_armor\px10_cav\textures\px10cavintfront_co.paa"
+                };
+            };
         };
         model = "3as\3AS_Rebel_Armor\PX10_Cav\model\3AS_PX10.p3d";
         picture = "3as\3AS_Rebel_Armor\PX10_Cav\data\ui\px10cav_side_ca.paa";
