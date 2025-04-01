a67b391735d3a62c7927c02510e965a2bb0418b7
Dexus-Omega <118584541+Dexus-Omega@users.noreply.github.com>
2025-03-31 20:48:49 -0400

    Finally got the Helmets to work

--- addons/armor/configs/Helmets_Scout_CT.hpp
+++ addons/armor/configs/Helmets_Scout_CT.hpp
@@ -1,13 +1,13 @@
-class CLASS(Helmet_Scout_Base): CLASS(cloneHelmet_base) {
+class CLASS(Helmet_Scout_CT): CLASS(cloneHelmet_base) {
     SCOPE_PUBLIC;
 
-    displayName = "[KC] SF Scout Helm (Base)";
+    displayName = "[KC] SF Scout Helm (CT)";
 
     model = "\ls_armor_bluefor\helmet\gar\scout\ls_gar_scout_helmet.p3d";
     hiddenSelections[] = {"camo1","camo2","visor"};
     hiddenSelectionsTextures[] = {
-        "ls_armor_bluefor\helmet\gar\scout\Data\helmet_co.paa",
-        "ls_armor_bluefor\helmet\gar\scout\Data\helmet_co.paa",
+        QPATHTOF(data\helmets\Scout\Scout_CT_Helmet_co.paa),
+        QPATHTOF(data\helmets\Scout\Scout_CT_Helmet_co.paa),
         "ls_armor_bluefor\helmet\gar\scout\Data\visor_co.paa"};
 
     class ItemInfo: ItemInfo {
