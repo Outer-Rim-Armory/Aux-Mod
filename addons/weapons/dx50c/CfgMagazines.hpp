b30248905ebdb86b9347b3221c4d81268238336b
CTAxel187 <crosenau1@gmail.com>
2025-08-08 18:55:21 -0400

    Update CfgMagazines.hpp

--- addons/weapons/dx50c/CfgMagazines.hpp
+++ addons/weapons/dx50c/CfgMagazines.hpp
@@ -1,16 +1,16 @@
 class CfgMagazines {
     class CLASS(Mag_Base);
-    class CLASS(Mag_15Rnd_DX50C): CLASS(Mag_Base) {
+    class CLASS(Mag_20Rnd_DX50C): CLASS(Mag_Base) {
         SCOPE_PUBLIC;
         displayName = "[KC] DX-50C Energy Cell";
         displayNameShort = "Standard Energy";
-        descriptionShort = "Energy Cell Pack<br/>Rounds: 15<br/>Used In: DX-50C";
+        descriptionShort = "Energy Cell Pack<br/>Rounds: 20<br/>Used In: DX-50C";
 
         model = "\MRC\JLTS\weapons\DP23\EPL2_mag.p3d";
         picture = "\ls\core\addons\weapons_dcSeries\data\ui\dc15sa_mag_ui_ca.paa";
 
         ammo = QCLASS(Bullet_PlasmaRifle_Blue2);
-        count = 15;
+        count = 20;
         mass = 20;
     };
 };
