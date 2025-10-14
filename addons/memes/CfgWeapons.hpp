7a078c6c2882dc80c0ac6d5c82461d43de15602e
Dogo1610 <164506107+Dogo1610@users.noreply.github.com>
2025-10-14 18:19:21 +0100

    dart fixed yay
    dart fixed it all

--- addons/memes/CfgWeapons.hpp
+++ addons/memes/CfgWeapons.hpp
@@ -1,14 +1,13 @@
 #define FLAG_ITEM(type,name,color) \
     class TRIPLES(BNA_KC_carrierFlag,type,item): ace_flags_white { \
         author = AUTHOR; \
-        displayName = QUOTE(Flag (##name##)); \
-        picture = QUOTE(\z\ace\addons\flags\data\pictures\##color##_item.paa); \
+        displayName = QUOTE(Flag (name)); \
+        picture = QUOTE(\z\ace\addons\flags\data\pictures\DOUBLES(color,item.paa)); \
         ace_flags_texture = QPATHTOF(data\flags\TRIPLES(flag,type,ca.paa)); \
         ace_flags_carrier = QUOTE(DOUBLES(BNA_KC_carrierFlag,type)); \
-        ace_flags_actionIconPlace = QUOTE(\z\ace\addons\flags\data\icons\place\##color##_place_icon.paa); \
-        ace_flags_actionIconCarry = QUOTE(\z\ace\addons\flags\data\icons\carry\##color##_carry_icon.paa); \
+        ace_flags_actionIconPlace = QUOTE(\z\ace\addons\flags\data\icons\place\DOUBLES(color,place_icon.paa)); \
+        ace_flags_actionIconCarry = QUOTE(\z\ace\addons\flags\data\icons\carry\DOUBLES(color,carry_icon.paa)); \
     }
-
 
 class CfgWeapons {
 
