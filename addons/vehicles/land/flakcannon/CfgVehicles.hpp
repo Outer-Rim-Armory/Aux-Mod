e91c5291d0efe90bd8f3533f939749df1665ce35
Dexus-Omega <118584541+Dexus-Omega@users.noreply.github.com>
2026-07-13 23:11:44 -0400

    Flak Cannon Fix

--- addons/vehicles/land/flakcannon/CfgVehicles.hpp
+++ addons/vehicles/land/flakcannon/CfgVehicles.hpp
@@ -1,8 +1,8 @@
 class CfgVehicles {
-    class 3AS_Static_FlakCannon;
-    class CLASS(Flak_Cannon_Base): 3AS_Static_FlakCannon {
+    class 3as_FlakCannon;
+    class CLASS(Flak_Cannon_Base): 3as_FlakCannon {
         SCOPE_HIDDEN;
-        displayName = "Flak Cannon (Base)";
+        displayName = "Flak Cannon";
         editorSubcategory = QEDSUBCAT(Turrets);
         faction = QFACTION(CIS);
         crew = QCLASS(CIS_Unit_B1_Crew);
