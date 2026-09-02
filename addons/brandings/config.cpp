1ce0eeb178725b4c23b93a29a85a16c74d50413b
Dexus-Omega <118584541+Dexus-Omega@users.noreply.github.com>
2026-09-02 17:09:49 -0400

    CBA Setting Removed

--- addons/brandings/config.cpp
+++ addons/brandings/config.cpp
@@ -6,15 +6,9 @@ class CfgPatches {
     };
 };
 
-class Extended_PreInit_EventHandlers {
-    class KC_Brandings_Settings {
-        init = "['KC_Brandings_EnableBackgroundMusic','CHECKBOX',['Enable Background Image & Music','Controls whether the KC background image and main menu music are enabled.'],['KC Brandings','Background Image & Music'],true,0] call CBA_fnc_addSetting;";
-    };
-};
-
 class Extended_DisplayLoad_EventHandlers {
     class RscDisplayMain {
-        BNA_KC_Brandings_MenuMusic = "[] spawn {uiSleep 0.5; if (!(missionNamespace getVariable ['KC_Brandings_EnableBackgroundMusic',true])) exitWith {}; if (isNil 'BNA_KC_Brandings_menuMusicLoopEH') then {BNA_KC_Brandings_menuMusicLoopEH = addMusicEventHandler ['MusicStop',{params ['_music']; if (_music == 'BNA_KC_Music_fooBAR_Menu_Version' && {missionNamespace getVariable ['KC_Brandings_EnableBackgroundMusic',true]}) then {uiSleep 0.1; playMusic 'BNA_KC_Music_fooBAR_Menu_Version';};}];}; playMusic 'BNA_KC_Music_fooBAR_Menu_Version';};";
+        BNA_KC_Brandings_MenuMusic = "[] spawn {uiSleep 0.5; if (isNil 'BNA_KC_Brandings_menuMusicLoopEH') then {BNA_KC_Brandings_menuMusicLoopEH = addMusicEventHandler ['MusicStop', {params ['_music']; if (_music == 'BNA_KC_Music_fooBAR_Menu_Version') then {uiSleep 0.1; playMusic 'BNA_KC_Music_fooBAR_Menu_Version';};}];}; playMusic 'BNA_KC_Music_fooBAR_Menu_Version';};";
     };
 };
 
@@ -58,11 +52,7 @@ class RscDisplayStart: RscStandardDisplay {
 
 class RscDisplayMain: RscStandardDisplay {
     enableDisplay = 0;
-
     delete Spotlight;
-
-    onLoad = "params ['_display']; [_display] spawn {params ['_display']; uiSleep 0.1; if (!(missionNamespace getVariable ['KC_Brandings_EnableBackgroundMusic',true])) then {private _background = _display displayCtrl 1; if (!isNull _background) then {_background ctrlShow false;}; stopMusic;};};";
-
     class Controls {
         delete Spotlight1;
         delete Spotlight2;
@@ -73,8 +63,8 @@ class RscDisplayMain: RscStandardDisplay {
 
         class Logo: RscPicture {
             idc = -1;
-            x = 0.375;
-            y = -0.375;
+            x = 0.375;    // centered (0.5 - 0.25/2)
+            y = -0.375;     // near top, below menu bar
             w = 0.25;
             h = 0.35;
             text = "\ORA\BNA_KC\addons\brandings\images\KCLogo2.paa";
@@ -93,7 +83,6 @@ class RscDisplayMain: RscStandardDisplay {
             tooltip = "Join KC Campaign Server";
             action = "connectToServer ['217.217.25.9', 2342, 'KC123'];";
         };
-
         class LogoButton2: RscButton {
             idc = -1;
             x = 0.500;
@@ -108,7 +97,6 @@ class RscDisplayMain: RscStandardDisplay {
             action = "connectToServer ['216.173.119.203', 2487, 'KC123'];";
         };
     };
-
     class controlsBackground {
         class LoadingPic: RscPicture {
             idc = 1;
